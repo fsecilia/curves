49eb7880e613a76d8c17b319bbf4ae9ae784d93d
Frank Secilia <frank.secilia@gmail.com>
2025-11-07 17:50:35 -0500

    build: delete real, add fixed
    Originally, I was going to pursue building with optionally either fixed
    point or floating point, but between the hurdles necessary to support
    both as a configuration option, the difference between the approximation
    coefficients, and the pressure I expect from upstream for even
    mentioning the FPU, it's not worth it. This is all fixed point.

---
+++ src/curves/kernel/driver/fixed.h
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: GPL-2.0+ OR MIT
+/*
+ * Fixed-point number type and supporting math functions.
+ *
+ * Copyright (C) 2025 Frank Secilia
+ * Author: Frank Secilia <frank.secilia@gmail.com>
+ */
+
+#ifndef _CURVES_FIXED_H
+#define _CURVES_FIXED_H
+
+typedef int fixed_t;
+
+#endif /* _CURVES_FIXED_h */
