35c9739e9dc48b13f0b4d1d0c41b11b17f3eb5b2
Frank Secilia <frank.secilia@gmail.com>
2025-11-07 18:16:03 -0500

    doc: update description of lib.hpp
    It is not the userland library. It is in userland, but it is the c++
    library. The only time the userland distinction is important is the
    common c library that contains the same files as the kernel module.

--- src/curves/lib/lib.hpp
+++ src/curves/lib/lib.hpp
@@ -1,9 +1,9 @@
 // SPDX-License-Identifier: MIT
 /*
   \file
-  \brief Main userland library header.
+  \brief Main c++ library header.
 
-  This file is included by every file in the library.
+  This file is included by every file in the c++ library.
 
   Copyright (C) 2025 Frank Secilia \n
   Author: Frank Secilia <frank.secilia@gmail.com>
