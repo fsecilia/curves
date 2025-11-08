d2f37e7b872d19825974e41f1dcb808a021537ad
Frank Secilia <frank.secilia@gmail.com>
2025-11-07 19:07:14 -0500

    build: shell int128 module

---
+++ src/curves/kernel/driver/int128.c
@@ -0,0 +1,7 @@
+// SPDX-License-Identifier: GPL-2.0+ OR MIT
+/*
+ * Copyright (C) 2025 Frank Secilia
+ * Author: Frank Secilia <frank.secilia@gmail.com>
+ */
+
+#include "int128.h"
---
+++ src/curves/kernel/int128_test.cpp
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+/*!
+  \file
+  \copyright Copyright (C) 2025 Frank Secilia
+*/
+
+#include <curves/kernel/common_test.hpp>
+
+namespace curves
+{
+
+//
+
+} // namespace curves
