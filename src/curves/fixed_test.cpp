26bdfca953a83ef464af210db7159299e7e740d5
Frank Secilia <frank.secilia@gmail.com>
2025-11-09 17:15:25 -0500

    test(fixed): make constants test a table

--- src/curves/fixed_test.cpp
+++ src/curves/fixed_test.cpp
@@ -6,6 +6,7 @@
 
 #include <cmath>
 #include <curves/test.hpp>
+#include <string>
 
 extern "C" {
 #include <curves/driver/fixed.h>
@@ -13,6 +14,10 @@ extern "C" {
 
 namespace curves {
 namespace {
+
+// ----------------------------------------------------------------------------
+// Fixed Test
+// ----------------------------------------------------------------------------
 
 struct fixed_test_t : Test {};
 
@@ -34,35 +39,59 @@ TEST_F(fixed_test_t, one_lowest_precision) {
   ASSERT_EQ(expected, actual);
 }
 
-TEST_F(fixed_test_t, ln2_highest_precision) {
-  const auto decimal_place = CURVES_LN_2_DECIMAL_PLACE;
-  const auto expected = std::log(2.0);
+// ----------------------------------------------------------------------------
+// Constant Test
+// ----------------------------------------------------------------------------
 
-  const auto actual = double(curves_const_ln2(decimal_place)) /
-                      double(curves_const_one(decimal_place));
+struct constant_test_param_t {
+  std::string name;
+  curves_fixed_t (*constant_func)(unsigned int);
+  unsigned int decimal_place;
+  double expected_value;
+  double tolerance;
 
-  ASSERT_EQ(expected, actual);
+  friend auto operator<<(std::ostream& out, const constant_test_param_t& src)
+      -> std::ostream& {
+    return out << "{" << src.decimal_place << ", " << src.expected_value << ", "
+               << src.tolerance << "}";
+  }
+};
+
+struct fixed_constant_test_t : public TestWithParam<constant_test_param_t> {
+  auto get_one(unsigned int decimal_place) const {
+    return curves_const_one(decimal_place);
+  }
+};
+
+TEST_P(fixed_constant_test_t, verify_constants) {
+  const auto param = GetParam();
+
+  const auto actual_fixed = param.constant_func(param.decimal_place);
+  const auto one_fixed = get_one(param.decimal_place);
+
+  const auto actual_double = double(actual_fixed) / double(one_fixed);
+
+  if (param.tolerance == 0.0) {
+    ASSERT_EQ(param.expected_value, actual_double);
+  } else {
+    ASSERT_NEAR(param.expected_value, actual_double, param.tolerance);
+  }
 }
 
-TEST_F(fixed_test_t, ln2_medium_precision) {
-  const auto decimal_place = CURVES_LN_2_DECIMAL_PLACE / 2;
-  const auto expected = std::log(2.0);
+constant_test_param_t constant_test_params[] = {
+    // ln(2)
+    {"ln2_high", curves_const_ln2, CURVES_LN_2_DECIMAL_PLACE, std::log(2.0),
+     0.0},
+    {"ln2_medium", curves_const_ln2, CURVES_LN_2_DECIMAL_PLACE / 2,
+     std::log(2.0), 5.0e-10},
+    {"ln2_low", curves_const_ln2, 1, std::log(2.0), 2.0e-1},
+};
 
-  const auto actual = double(curves_const_ln2(decimal_place)) /
-                      double(curves_const_one(decimal_place));
-
-  ASSERT_NEAR(expected, actual, 5.0e-10);
-}
-
-TEST_F(fixed_test_t, ln2_lowest_precision) {
-  const auto decimal_place = 1;
-  const auto expected = std::log(2.0);
-
-  const auto actual = double(curves_const_ln2(decimal_place)) /
-                      double(curves_const_one(decimal_place));
-
-  ASSERT_NEAR(expected, actual, 2.0e-1);
-}
+INSTANTIATE_TEST_SUITE_P(all_constants, fixed_constant_test_t,
+                         ValuesIn(constant_test_params),
+                         [](const TestParamInfo<constant_test_param_t>& info) {
+                           return info.param.name;
+                         });
 
 }  // namespace
 }  // namespace curves
