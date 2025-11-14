27988639c3f6fbc981f3ec0003167e5663bae144
Frank Secilia <frank.secilia@gmail.com>
2025-11-14 09:26:06 -0500

    fix(math): add externs to .c

--- src/curves/driver/math64.c
+++ src/curves/driver/math64.c
@@ -8,3 +8,10 @@
 
 extern s64 curves_s64_saturate(bool result_positive);
 extern s64 curves_s128_to_s64_truncate(s128 value);
+
+extern s64 curves_s64_shr_rtn(s64 value, unsigned int right_shift);
+extern s64 curves_s128_to_s64_shr_rtn(s128 value, unsigned int right_shift);
+
+extern struct curves_div_result curves_div_s128_by_s64(int128_t dividend,
+						       int64_t divisor);
+extern s64 curves_div_s128_by_s64_rtn(int128_t dividend, s64 divisor);
