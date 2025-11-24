e59e9d3418aa83122edc7cea2bfe30f7eaaa321f
Frank Secilia <frank.secilia@gmail.com>
2025-11-24 13:01:12 -0500

    fix(math): correct name of extern

--- src/curves/driver/math.c
+++ src/curves/driver/math.c
@@ -10,7 +10,7 @@ extern s64 curves_sign_mask(s64 value);
 extern u64 curves_strip_sign(s64 value);
 extern s64 curves_apply_sign(u64 value, s64 mask);
 
-extern s64 curves_fixed_narrow_s128_s64(s128 value);
+extern s64 curves_narrow_s128_s64(s128 value);
 
 extern struct div_u128_u64_result
 curves_div_u128_u64(unsigned __int128 dividend, u64 divisor);
