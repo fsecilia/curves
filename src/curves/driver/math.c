099d4ddfd0ba3e338089c388b7fa47ffe2d4f7a4
Frank Secilia <frank.secilia@gmail.com>
2025-11-26 16:20:22 -0500

    refactor(math): add missing extern

--- src/curves/driver/math.c
+++ src/curves/driver/math.c
@@ -12,6 +12,7 @@ extern s128 curves_saturate_s128(bool result_positive);
 extern s64 curves_sign_mask(s64 value);
 extern u64 curves_strip_sign(s64 value);
 extern s64 curves_apply_sign(u64 value, s64 mask);
+extern u32 curves_max_u32(u32 a, u32 b);
 
 extern s64 curves_narrow_s128_s64(s128 value);
 
