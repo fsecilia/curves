3f843e6ce4b192382066ca90dcb6e1d991eb9c39
Frank Secilia <frank.secilia@gmail.com>
2025-12-25 10:36:40 -0500

    test(spline): add gain accuracy test

--- src/curves/testing/integration/synchronous_curve_approximation_accuracy.cpp
+++ src/curves/testing/integration/synchronous_curve_approximation_accuracy.cpp
@@ -6,18 +6,20 @@
 
 #include <curves/testing/test.hpp>
 #include <curves/curves/synchronous.hpp>
+#include <curves/curves/transfer_function/curve.hpp>
+#include <curves/curves/transfer_function/from_gain.hpp>
+#include <curves/curves/transfer_function/from_sensitivity.hpp>
 #include <curves/math/io.hpp>
 #include <curves/math/spline.hpp>
-#include <curves/curves/transfer_function/curve.hpp>
+#include <curves/testing/error_metrics.hpp>
 
 namespace curves {
 namespace {
 
 TEST(spline_set, synchronous_accuracy_from_sensitivity) {
-  auto sensitivity = SynchronousCurve{10.0L, 10.0L, 27.55L, 0.5L};
+  auto sensitivity = FromSensitivity{SynchronousCurve{8.0L, 0.5L, 10.5L, 0.5L}};
 
-  const auto spline =
-      spline::create_spline(TransferFunction{sensitivity}, 1.0L);
+  const auto spline = spline::create_spline(sensitivity, 1.0L);
   const auto v_to_x = Fixed::literal(spline.v_to_x);
 
   std::cout << "spline.v_to_x ~= " << v_to_x.to_real() << " (" << v_to_x.value
@@ -42,7 +44,75 @@ TEST(spline_set, synchronous_accuracy_from_sensitivity) {
   while (x_fixed < x_max) {
     const auto x_float = x_fixed.to_real();
 
-    const auto y_curve = x_float * sensitivity(x_float).f;
+    const auto y_curve = sensitivity(x_float).f;
+    const auto y_table = spline::eval(&spline, x_fixed.value);
+    x_fixed += dx;
+
+    const auto expected = y_curve;
+    const auto actual = Fixed::literal(y_table).to_real();
+
+    ++num_samples;
+    if (!expected) continue;  // skip near 0 so rel doesn't explode
+
+    const auto abs_err = std::abs(actual - expected);
+    if (max_abs_err < abs_err) {
+      max_abs_err = abs_err;
+      max_abs_err_x = x_float;
+    }
+    sse_abs += abs_err * abs_err;
+
+    const auto rel_err = std::abs(abs_err / expected);
+    if (max_rel_err < rel_err) {
+      max_rel_err = rel_err;
+      max_rel_err_x = x_float;
+    }
+    sse_rel += rel_err * rel_err;
+  }
+  std::cout << "x1: " << x_fixed << " (" << x_fixed.value << " fixed)"
+            << std::endl;
+
+  const auto mse_abs = sse_abs / num_samples;
+  const auto mse_rel = sse_rel / num_samples;
+  std::cout << "Max Abs Error: " << max_abs_err << " (x = " << max_abs_err_x
+            << ")"
+            << "\nSSE Abs: " << sse_abs << "\nMSE Abs: " << mse_abs
+            << "\nRMSE Abs: " << std::sqrt(mse_abs)
+            << "\nMax Rel Error: " << max_rel_err << " (x = " << max_rel_err_x
+            << ")"
+            << "\nSSE Rel: " << sse_rel << "\nMSE Rel: " << mse_rel
+            << "\nRMSE Rel: " << std::sqrt(mse_rel) << std::endl;
+}
+
+TEST(spline_set, synchronous_accuracy_from_gain) {
+  auto spline_gain = FromGain{SynchronousCurve{8.0L, 0.5L, 10.5L, 0.5L}};
+  auto test_gain = spline_gain;  // make copy
+
+  const auto spline = spline::create_spline(spline_gain, 1.0L);
+  const auto v_to_x = Fixed::literal(spline.v_to_x);
+
+  std::cout << "spline.v_to_x ~= " << v_to_x.to_real() << " (" << v_to_x.value
+            << " fixed)" << std::endl;
+
+  const auto x_max = Fixed::literal(spline.x_geometric_limit);
+
+  const auto dx = Fixed{1.0e-3L};
+  std::cout << "dx: " << dx << " (" << dx.value << " fixed)" << std::endl;
+
+  auto x_fixed = Fixed{0};
+  std::cout << "x0: " << x_fixed << " (" << x_fixed.value << " fixed)"
+            << std::endl;
+
+  auto max_abs_err = 0.0L;
+  auto max_rel_err = 0.0L;
+  auto max_abs_err_x = 0.0L;
+  auto max_rel_err_x = 0.0L;
+  auto sse_abs = 0.0L;
+  auto sse_rel = 0.0L;
+  auto num_samples = 0;
+  while (x_fixed < x_max) {
+    const auto x_float = x_fixed.to_real();
+
+    const auto y_curve = test_gain(x_float).f;
     const auto y_table = spline::eval(&spline, x_fixed.value);
     x_fixed += dx;
 
