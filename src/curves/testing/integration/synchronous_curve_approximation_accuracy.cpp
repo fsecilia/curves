e628226a02d45ae4557c4f32fbda62119313be06
Frank Secilia <frank.secilia@gmail.com>
2025-12-26 01:38:26 -0500

    test(spline): quantify equivalent sources

--- src/curves/testing/integration/synchronous_curve_approximation_accuracy.cpp
+++ src/curves/testing/integration/synchronous_curve_approximation_accuracy.cpp
@@ -152,5 +152,61 @@ TEST(spline_set, synchronous_accuracy_from_gain) {
             << "\nRMSE Rel: " << std::sqrt(mse_rel) << std::endl;
 }
 
+TEST(spline, sensitivity_vs_gain) {
+  const auto curve = SynchronousCurve{8.0L, 0.5L, 10.5L, 0.5L};
+  const auto sensitivity_spline =
+      spline::create_spline(FromSensitivity{curve}, 1.0L);
+  const auto gain_spline = spline::create_spline(FromGain{curve}, 1.0L);
+
+  ASSERT_EQ(sensitivity_spline.v_to_x, gain_spline.v_to_x);
+  ASSERT_EQ(sensitivity_spline.x_geometric_limit,
+            gain_spline.x_geometric_limit);
+  ASSERT_EQ(sensitivity_spline.x_runout_limit, gain_spline.x_runout_limit);
+  ASSERT_EQ(sensitivity_spline.runout_width_log2,
+            gain_spline.runout_width_log2);
+
+  const auto x_max = Fixed::literal(sensitivity_spline.x_geometric_limit);
+
+  const auto dv = Fixed{1.0e-3L};
+  std::cout << "dv: " << dv << " (" << dv.value << " fixed)" << std::endl;
+
+  auto v = Fixed(0);
+  auto x =
+      Fixed::literal(spline::transform_v_to_x(&sensitivity_spline, v.value));
+  std::cout << "x0: " << x << " (" << x.value << " fixed)" << std::endl;
+
+  const auto knot_locator = spline::KnotLocator{};
+
+  auto accuracy_metrics = AccuracyMetrics{};
+  while (v < x_max) {
+    const auto xSx_viaTs =
+        Fixed::literal(spline::eval(&sensitivity_spline, x.value));
+
+    s64 segment_index;
+    s64 t;
+    spline::locate_segment(x.value, &segment_index, &t);
+    const auto gain_segment = gain_spline.segments[segment_index];
+    const auto* const c = gain_segment.coeffs;
+    auto dTg = 3 * c[0];
+    dTg = (s64)(((s128)dTg * t + SPLINE_FRAC_HALF) >> SPLINE_FRAC_BITS) +
+          2 * c[1];
+    dTg = (s64)(((s128)dTg * t + SPLINE_FRAC_HALF) >> SPLINE_FRAC_BITS) + c[2];
+
+    const auto segment_width = Fixed::literal(knot_locator(segment_index + 1) -
+                                              knot_locator(segment_index));
+
+    const auto xSx_viaTg = x * Fixed::literal(dTg) / segment_width;
+
+    accuracy_metrics.sample(x.to_real(), xSx_viaTs.to_real(),
+                            xSx_viaTg.to_real());
+
+    v += dv;
+    x = Fixed::literal(spline::transform_v_to_x(&sensitivity_spline, v.value));
+  }
+  std::cout << "x1: " << x << " (" << x.value << " fixed)" << std::endl;
+
+  std::cout << accuracy_metrics << std::endl;
+}
+
 }  // namespace
 }  // namespace curves
