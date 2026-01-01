51aa3d599ab228e8fa416e286d7c678b35d24186
Frank Secilia <frank.secilia@gmail.com>
2026-01-01 05:45:08 -0500

    build(seg): shell segment module

---
+++ src/curves/driver/segment.h
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-2.0+ OR MIT
+/*
+ * Packed and unpacked cubic Hermite spline segments.
+ *
+ * Copyright (C) 2025 Frank Secilia
+ * Author: Frank Secilia <frank.secilia@gmail.com>
+ */
+
+#ifndef _CURVES_SEGMENT_H
+#define _CURVES_SEGMENT_H
+
+#include "kernel_compat.h"
+#include "fixed.h"
+
+#endif /* _CURVES_SEGMENT_H */
---
+++ src/curves/math/segment.hpp
@@ -0,0 +1,17 @@
+// SPDX-License-Identifier: MIT
+/*!
+  \file
+  \brief Floating-point wrapper for kernel spline segments.
+
+  \copyright Copyright (C) 2025 Frank Secilia
+*/
+
+#pragma once
+
+#include <curves/lib.hpp>
+
+namespace curves {
+
+//
+
+}  // namespace curves
