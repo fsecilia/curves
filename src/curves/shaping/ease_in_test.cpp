aa593266bf9420d7e03162ec358ffba950d7851c
Frank Secilia <frank.secilia@gmail.com>
2026-01-08 04:49:24 -0500

    test(shaping): extract common from ease-in

--- src/curves/shaping/ease_in_test.cpp
+++ src/curves/shaping/ease_in_test.cpp
@@ -6,61 +6,19 @@
 
 #include "ease_in.hpp"
 #include <curves/testing/test.hpp>
-#include <curves/math/jet.hpp>
+#include <curves/shaping/ease_testing.hpp>
 #include <gmock/gmock.h>
 
 namespace curves::shaping {
 namespace {
-
-using Parameter = double;
-using Jet = math::Jet<Parameter>;
 
 // ============================================================================
 // operator()
 // ============================================================================
 
 // ----------------------------------------------------------------------------
-// Test Double
-// ----------------------------------------------------------------------------
-
-template <Parameter kX0, Parameter kWidth, Parameter kHeight>
-struct Transition {
-  constexpr auto x0() const noexcept -> Parameter { return kX0; }
-  constexpr auto width() const noexcept -> Parameter { return kWidth; }
-  constexpr auto height() const noexcept -> Parameter { return kHeight; }
-
-  template <typename Value>
-  constexpr auto operator()(const Value& x) const noexcept -> Value {
-    return (x - Value{x0()}) * Value{height() / width()};
-  }
-};
-
-struct DegenerateTransition {
-  constexpr auto width() const noexcept -> Parameter { return 0; }
-  constexpr auto height() const noexcept -> Parameter { return 0; }
-
-  auto fail() const -> void { GTEST_FAIL(); }
-
-  template <typename Value>
-  auto operator()(const Value&) const -> Value {
-    fail();
-    return {};
-  }
-};
-
-// ----------------------------------------------------------------------------
 // Fixture
 // ----------------------------------------------------------------------------
-
-struct CallTestVector {
-  Parameter x;
-  Jet expected;
-
-  friend auto operator<<(std::ostream& out, const CallTestVector& src)
-      -> std::ostream& {
-    return out << "{.x = " << src.x << ", .expected = " << src.expected << "}";
-  }
-};
 
 template <typename Transition>
 struct EaseInCallTest : TestWithParam<CallTestVector> {
@@ -77,8 +35,6 @@ struct EaseInCallTest : TestWithParam<CallTestVector> {
     EXPECT_NEAR(expected.v, actual.v, 1e-10);
   }
 };
-
-constexpr auto kEps = 1e-5;
 
 // ----------------------------------------------------------------------------
 // Nominal Case
@@ -219,33 +175,25 @@ INSTANTIATE_TEST_SUITE_P(TestVectors, EaseInCallTestNullTransition,
 
 }  // namespace null_transition
 
+}  // namespace
+
 // ============================================================================
 // inverse()
 // ============================================================================
+
+namespace inverse {
+namespace {
 
 struct EaseInInverseTest : Test {
   static constexpr auto x0 = Parameter{1};
   static constexpr auto width = Parameter{1};
   static constexpr auto height = Parameter{1};
 
-  struct Inverter {};
   Inverter inverter;
 
-  struct MockTransition {
-    MOCK_METHOD(Parameter, inverse, (Parameter, const Inverter& inverter),
-                (const, noexcept));
-    virtual ~MockTransition() = default;
-  };
   StrictMock<MockTransition> mock_transition;
 
-  struct Transition : shaping::Transition<x0, width, height> {
-    auto inverse(Parameter y, const Inverter& inverter) const noexcept
-        -> Parameter {
-      return mock_transition->inverse(y, inverter);
-    }
-
-    MockTransition* mock_transition = nullptr;
-  };
+  using Transition = inverse::Transition<x0, width, height>;
 
   using Sut = EaseIn<Parameter, Transition>;
   Sut sut{Transition{{}, &mock_transition}};
@@ -280,6 +228,11 @@ TEST_F(EaseInInverseTest, TransitionSegment) {
 
   EXPECT_DOUBLE_EQ(expected, actual);
 }
+
+}  // namespace
+}  // namespace inverse
+
+namespace {
 
 // ============================================================================
 // required_knots()
---
+++ src/curves/shaping/ease_testing.hpp
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: MIT
+/*!
+  \file
+  \brief Common facilities for testing ease functions.
+
+  \copyright Copyright (C) 2026 Frank Secilia
+*/
+
+#pragma once
+
+#include <curves/testing/test.hpp>
+#include <curves/math/jet.hpp>
+#include <gmock/gmock.h>
+
+namespace curves::shaping {
+
+using Parameter = double;
+using Jet = math::Jet<Parameter>;
+
+template <Parameter kX0, Parameter kWidth, Parameter kHeight>
+struct Transition {
+  constexpr auto x0() const noexcept -> Parameter { return kX0; }
+  constexpr auto width() const noexcept -> Parameter { return kWidth; }
+  constexpr auto height() const noexcept -> Parameter { return kHeight; }
+
+  template <typename Value>
+  constexpr auto operator()(const Value& x) const noexcept -> Value {
+    return (x - Value{x0()}) * Value{height() / width()};
+  }
+};
+
+struct CallTestVector {
+  Parameter x;
+  Jet expected;
+
+  friend auto operator<<(std::ostream& out, const CallTestVector& src)
+      -> std::ostream& {
+    return out << "{.x = " << src.x << ", .expected = " << src.expected << "}";
+  }
+};
+
+constexpr auto kEps = 1e-5;
+
+struct DegenerateTransition {
+  constexpr auto width() const noexcept -> Parameter { return 0; }
+  constexpr auto height() const noexcept -> Parameter { return 0; }
+
+  auto fail() const -> void { GTEST_FAIL(); }
+
+  template <typename Value>
+  auto operator()(const Value&) const -> Value {
+    fail();
+    return {};
+  }
+};
+
+namespace inverse {
+
+struct Inverter {};
+
+struct MockTransition {
+  MOCK_METHOD(Parameter, inverse, (Parameter, const Inverter& inverter),
+              (const, noexcept));
+  virtual ~MockTransition() = default;
+};
+
+template <Parameter kX0, Parameter kWidth, Parameter kHeight>
+struct Transition : shaping::Transition<kX0, kWidth, kHeight> {
+  auto inverse(Parameter y, const Inverter& inverter) const noexcept
+      -> Parameter {
+    return mock_transition->inverse(y, inverter);
+  }
+
+  MockTransition* mock_transition = nullptr;
+};
+
+}  // namespace inverse
+}  // namespace curves::shaping
