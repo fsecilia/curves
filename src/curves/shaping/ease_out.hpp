bd23b8173e2fe2e05cb062e01441818afcdd96ab
Frank Secilia <frank.secilia@gmail.com>
2026-01-08 11:07:04 -0500

    feat(shaping): add EaseOut
    This is literally a line-by-line port of EaseIn to reflect it.

---
+++ src/curves/shaping/ease_out.hpp
@@ -0,0 +1,60 @@
+// SPDX-License-Identifier: MIT
+/*!
+  \file
+  \brief Piecewise ease-out function.
+
+  \copyright Copyright (C) 2026 Frank Secilia
+*/
+
+#pragma once
+
+#include <curves/lib.hpp>
+#include <utility>
+
+namespace curves::shaping {
+
+template <typename Parameter, typename Transition>
+class EaseOut {
+ public:
+  explicit constexpr EaseOut(Transition transition) noexcept
+      : transition_{std::move(transition)},
+        ceiling_{transition_.x0() + transition.height()} {}
+
+  template <typename Value>
+  constexpr auto operator()(const Value& x) const noexcept -> Value {
+    // Linear segment through origin.
+    const auto x0 = Value{transition_.x0()};
+    if (x < x0) return x;
+
+    // Flat segment.
+    const auto width = Value{transition_.width()};
+    if (x >= x0 + width) return Value{ceiling_};
+
+    // Transition segment.
+    return transition_(x) + x0;
+  }
+
+  constexpr auto inverse(Parameter y, auto&& inverter) const noexcept
+      -> Parameter {
+    // Linear segment.
+    if (y <= transition_.x0()) return y;
+
+    // Flat segment.
+    if (y >= ceiling_) return transition_.x0() + transition_.width();
+
+    // Transition segment.
+    return transition_.inverse(y - transition_.x0(),
+                               std::forward<decltype(inverter)>(inverter));
+  }
+
+  constexpr auto required_knots() const noexcept -> std::array<Parameter, 2> {
+    const auto x0 = transition_.x0();
+    return {x0, x0 + transition_.width()};
+  }
+
+ private:
+  [[no_unique_address]] Transition transition_;
+  Parameter ceiling_;
+};
+
+}  // namespace curves::shaping
---
+++ src/curves/shaping/ease_out_test.cpp
@@ -0,0 +1,268 @@
+// SPDX-License-Identifier: MIT
+/*!
+  \file
+  \copyright Copyright (C) 2026 Frank Secilia
+*/
+
+#include "ease_out.hpp"
+#include <curves/testing/test.hpp>
+#include <curves/shaping/ease_testing.hpp>
+#include <gmock/gmock.h>
+
+namespace curves::shaping {
+namespace {
+
+// ============================================================================
+// operator()
+// ============================================================================
+
+// ----------------------------------------------------------------------------
+// Fixture
+// ----------------------------------------------------------------------------
+
+template <typename Transition>
+struct EaseOutCallTest : TestWithParam<CallTestVector> {
+  using Sut = EaseOut<Parameter, Transition>;
+  Sut sut{{}};
+
+  auto test() const -> void {
+    const auto x = Jet{GetParam().x, 1.0};
+    const auto expected = GetParam().expected;
+
+    const auto actual = sut(x);
+
+    EXPECT_NEAR(expected.a, actual.a, 1e-10);
+    EXPECT_NEAR(expected.v, actual.v, 1e-10);
+  }
+};
+
+// ----------------------------------------------------------------------------
+// Nominal Case
+// ----------------------------------------------------------------------------
+
+namespace nominal {
+
+/*
+  These values end up being discontinuous at the end of the transition, but the
+  math still works if you sample segment by segment.
+
+  The test transition is linear: y = (x - x0) * slope, where slope =
+  height/width. EaseOut adds x0 to this in the transition segment: y = x0 + (x -
+  x0) * slope.
+*/
+constexpr auto x0 = Parameter{0.1};
+constexpr auto width = Parameter{1.2};
+constexpr auto height = Parameter{2.5};
+constexpr auto slope = height / width;
+constexpr auto ceiling = x0 + height;
+
+struct EaseOutCallTestNominal : EaseOutCallTest<Transition<x0, width, height>> {
+};
+
+TEST_P(EaseOutCallTestNominal, Parameterized) { test(); }
+
+constexpr CallTestVector test_vectors[] = {
+    // Well out of domain to left.
+    {-1, {-1, 1}},
+
+    // Linear segment interior.
+    {0, {0, 1}},
+
+    // Linear segment end, transition segment begin.
+    {x0 - kEps, {x0 - kEps, 1}},
+    {x0, {x0, slope}},
+    {x0 + kEps, {x0 + slope * kEps, slope}},
+
+    // Transition segment midpoint.
+    {x0 + width / 2, {x0 + slope * width / 2, slope}},
+
+    // Transition segment end, flat segment begin.
+    {x0 + width - kEps, {x0 + slope * (width - kEps), slope}},
+    {x0 + width, {ceiling, 0}},
+    {x0 + width + kEps, {ceiling, 0}},
+
+    // Flat segment interior.
+    {x0 + width + 10, {ceiling, 0}},
+};
+INSTANTIATE_TEST_SUITE_P(TestVectors, EaseOutCallTestNominal,
+                         ValuesIn(test_vectors));
+
+}  // namespace nominal
+
+// ----------------------------------------------------------------------------
+// Zero x0: linear segment shrinks to a point at origin
+// ----------------------------------------------------------------------------
+
+namespace zero_x0 {
+
+constexpr auto x0 = Parameter{0};
+constexpr auto width = Parameter{2};
+constexpr auto height = Parameter{3};
+constexpr auto slope = height / width;
+
+struct EaseOutCallTestZeroX0 : EaseOutCallTest<Transition<x0, width, height>> {
+};
+
+TEST_P(EaseOutCallTestZeroX0, Parameterized) { test(); }
+
+constexpr CallTestVector test_vectors[] = {
+    // Before transition. This is out of the domain.
+    {-kEps, {-kEps, 1}},
+
+    // At transition.
+    {0, {0, slope}},
+
+    // Transition segment.
+    {kEps, {kEps * slope, slope}},
+};
+INSTANTIATE_TEST_SUITE_P(TestVectors, EaseOutCallTestZeroX0,
+                         ValuesIn(test_vectors));
+
+}  // namespace zero_x0
+
+// ----------------------------------------------------------------------------
+// Zero width: transition segment vanishes
+// ----------------------------------------------------------------------------
+
+namespace zero_width {
+
+constexpr auto x0 = Parameter{0.5};
+constexpr auto ceiling = x0;
+
+struct Transition : DegenerateTransition {
+  constexpr auto x0() const noexcept -> Parameter { return zero_width::x0; }
+};
+
+struct EaseOutCallTestZeroWidth : EaseOutCallTest<Transition> {};
+
+TEST_P(EaseOutCallTestZeroWidth, Parameterized) { test(); }
+
+constexpr CallTestVector test_vectors[] = {
+    // Linear segment.
+    {x0 - kEps, {x0 - kEps, 1}},
+
+    // Flat segment begins immediately at x0.
+    {x0, {ceiling, 0}},
+
+    // Flat segment.
+    {x0 + kEps, {ceiling, 0}},
+};
+INSTANTIATE_TEST_SUITE_P(TestVectors, EaseOutCallTestZeroWidth,
+                         ValuesIn(test_vectors));
+
+}  // namespace zero_width
+
+// ----------------------------------------------------------------------------
+// Null Transition: linear segment shrinks to origin, transition vanishes
+// ----------------------------------------------------------------------------
+
+namespace null_transition {
+
+constexpr auto ceiling = Parameter{0};
+
+struct Transition : DegenerateTransition {
+  constexpr auto x0() const noexcept -> Parameter { return 0; }
+};
+
+struct EaseOutCallTestNullTransition : EaseOutCallTest<Transition> {};
+
+TEST_P(EaseOutCallTestNullTransition, Parameterized) { test(); }
+
+constexpr CallTestVector test_vectors[] = {
+    // Before what would be either the linear segment or the transition.
+    {-kEps, {-kEps, 1}},
+
+    // Flat segment begins immediately at 0.
+    {0, {ceiling, 0}},
+
+    // Flat segment.
+    {kEps, {ceiling, 0}},
+};
+INSTANTIATE_TEST_SUITE_P(TestVectors, EaseOutCallTestNullTransition,
+                         ValuesIn(test_vectors));
+
+}  // namespace null_transition
+
+}  // namespace
+
+// ============================================================================
+// inverse()
+// ============================================================================
+
+namespace inverse {
+namespace {
+
+struct EaseOutInverseTest : Test {
+  static constexpr auto x0 = Parameter{1};
+  static constexpr auto width = Parameter{1};
+  static constexpr auto height = Parameter{1};
+  static constexpr auto ceiling = x0 + height;
+
+  Inverter inverter;
+
+  StrictMock<MockTransition> mock_transition;
+
+  using Transition = inverse::Transition<x0, width, height>;
+
+  using Sut = EaseOut<Parameter, Transition>;
+  Sut sut{Transition{{}, &mock_transition}};
+};
+
+TEST_F(EaseOutInverseTest, LinearSegment) {
+  const auto y = x0 / 2;  // Below x0, in linear segment.
+  const auto expected = y;
+
+  const auto actual = sut.inverse(y, inverter);
+
+  EXPECT_DOUBLE_EQ(expected, actual);
+}
+
+TEST_F(EaseOutInverseTest, FlatSegment) {
+  const auto y = ceiling + 1;  // Above ceiling, in flat segment.
+  const auto expected = x0 + width;
+
+  const auto actual = sut.inverse(y, inverter);
+
+  EXPECT_DOUBLE_EQ(expected, actual);
+}
+
+TEST_F(EaseOutInverseTest, TransitionSegment) {
+  const auto y = (x0 + ceiling) / 2;  // Between x0 and ceiling.
+  const auto transition_y = y - x0;   // What we pass to transition_.inverse.
+  const auto expected = 17;
+  EXPECT_CALL(mock_transition, inverse(transition_y, Ref(inverter)))
+      .WillOnce(Return(expected));
+
+  const auto actual = sut.inverse(y, inverter);
+
+  EXPECT_DOUBLE_EQ(expected, actual);
+}
+
+}  // namespace
+}  // namespace inverse
+
+namespace {
+
+// ============================================================================
+// required_knots()
+// ============================================================================
+
+struct EaseOutRequiredKnotsTest : Test {
+  static constexpr auto x0 = Parameter{2};
+  static constexpr auto width = Parameter{5};
+  static constexpr auto height = Parameter{11};
+
+  using Sut = EaseOut<Parameter, Transition<x0, width, height>>;
+  Sut sut{{}};
+};
+
+TEST_F(EaseOutRequiredKnotsTest, RequiredKnots) {
+  const auto expected = std::array{x0, x0 + width};
+
+  const auto actual = sut.required_knots();
+
+  ASSERT_EQ(expected, actual);
+}
+
+}  // namespace
+}  // namespace curves::shaping
