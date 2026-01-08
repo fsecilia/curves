b3913dbf86eb60f97cace68e4a3fbe791da46e6e
Frank Secilia <frank.secilia@gmail.com>
2026-01-08 13:23:22 -0500

    feat(shaping): add reflected transition function
    This is a function adapter that reflects the composed transition
    function about the point (1, f(1)).

---
+++ src/curves/shaping/transition_functions/reflected.hpp
@@ -0,0 +1,39 @@
+// SPDX-License-Identifier: MIT
+/*!
+  \file
+  \brief Smooth transition segment between two linear segments.
+
+  \copyright Copyright (C) 2026 Frank Secilia
+*/
+
+#pragma once
+
+#include <curves/lib.hpp>
+#include <cassert>
+
+namespace curves::shaping::transition_functions {
+
+/*!
+  Composes a transition function to reflect it about the point (1, at_1(1)).
+*/
+template <typename TransitionFunction>
+struct Reflected {
+  [[no_unique_address]] TransitionFunction transition_function;
+
+  /*!
+    \returns value of reflected function at x=1.
+
+    Nominally, the result at x=1 is f(1) - f(1 - 0), but by definition,
+    transition functions always return 0 at 0, so f(0) vanishes and the result
+    is the same as the original function at x=1.
+  */
+  constexpr auto at_1() const noexcept { return transition_function.at_1(); }
+
+  template <typename Value>
+  constexpr auto operator()(const Value& t) const {
+    return Value{transition_function.at_1()} -
+           transition_function(Value{1} - t);
+  }
+};
+
+}  // namespace curves::shaping::transition_functions
---
+++ src/curves/shaping/transition_functions/reflected_test.cpp
@@ -0,0 +1,74 @@
+// SPDX-License-Identifier: MIT
+/*!
+  \file
+  \copyright Copyright (C) 2026 Frank Secilia
+*/
+
+#include "reflected.hpp"
+#include <curves/testing/test.hpp>
+#include <curves/math/jet.hpp>
+
+namespace curves::shaping::transition_functions {
+namespace {
+
+using Parameter = double;
+using Jet = math::Jet<Parameter>;
+
+static constexpr auto m = 2.1;
+static constexpr auto x0 = 13.5;
+
+const Parameter test_vectors[] = {
+    0.0 / 4, 1.0 / 4, 2.0 / 4, 3.0 / 4, 4.0 / 4,
+};
+
+// ============================================================================
+// Test Doubles
+// ============================================================================
+
+struct LinearTransitionFunction {
+  constexpr auto at_1() const noexcept -> Parameter { return m * (1 - x0); }
+
+  template <typename Value>
+  constexpr auto operator()(const Value& t) const noexcept -> Value {
+    return m * (t - x0);
+  }
+};
+
+// ============================================================================
+// ReflectedTransitionFunction
+// ============================================================================
+
+struct ReflectedTransitionTest : Test {
+  using TransitionFunction = LinearTransitionFunction;
+  static constexpr TransitionFunction transition_function{};
+
+  using Sut = Reflected<TransitionFunction>;
+  static constexpr Sut sut{transition_function};
+};
+
+TEST_F(ReflectedTransitionTest, at_1) {
+  EXPECT_DOUBLE_EQ(transition_function.at_1(), sut.at_1());
+}
+
+// ----------------------------------------------------------------------------
+// Parameterized Test
+// ----------------------------------------------------------------------------
+
+struct ReflectedTransitionParameterizedTest : ReflectedTransitionTest,
+                                              WithParamInterface<Parameter> {};
+
+TEST_P(ReflectedTransitionParameterizedTest, evaluate) {
+  const auto x = Jet{GetParam(), 1.0};
+  const auto expected = transition_function.at_1() - m * ((1 - x) - x0);
+
+  const auto actual = sut(x);
+
+  EXPECT_EQ(expected, actual);
+}
+
+INSTANTIATE_TEST_SUITE_P(ReflectedTransitionTest,
+                         ReflectedTransitionParameterizedTest,
+                         ValuesIn(test_vectors));
+
+}  // namespace
+}  // namespace curves::shaping::transition_functions
