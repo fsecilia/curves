f668779daab09de67f882f1e3be983908aa1a4b5
Frank Secilia <frank.secilia@gmail.com>
2026-01-19 17:07:04 -0500

    build: shell ui executable
    This is literally just main(). It's primary purpose is to depend on the
    driver backed we are about to start building.

---
+++ src/crv/ui/main.cpp
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+/*!
+  \file
+  \brief Config app entry point.
+
+  \copyright Copyright (C) 2026 Frank Secilia
+*/
+
+#include <cstdlib>
+
+auto main(int /*argc*/, char* /*argv*/[]) -> int
+{
+    return EXIT_SUCCESS;
+}
