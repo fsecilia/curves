e2b4d4c2d3bc13519d141b8f7e3108e41e56b825
Frank Secilia <frank.secilia@gmail.com>
2026-01-19 17:25:21 -0500

    build(driver): rough in entry_point.c
    This doesn't actually use the c++, but it will exercise the
    shim

---
+++ src/crv/driver/entry_point.c
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+/**
+ * Curves kernel module entry point.
+ *
+ * Copyright (C) 2026 Frank Secilia
+ * Author: Frank Secilia <frank.secilia@gmail.com>
+ */
+
+#include <linux/errno.h>
+#include <linux/fpu.h>
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/stringify.h>
+
+static int __init crv_init(void)
+{
+    printk("crv_init\n");
+    if (!kernel_fpu_available()) return -ENOSYS;
+    return 0;
+}
+
+static void __exit crv_exit(void)
+{
+    printk("crv_exit\n");
+}
+
+module_init(crv_init);
+module_exit(crv_exit);
+
+MODULE_LICENSE("MIT");
+MODULE_AUTHOR("Frank Secilia");
+MODULE_DESCRIPTION("Curves Mouse Acceleration Input Handler");
+MODULE_VERSION(__stringify(CRV_DRIVER_VERSION));
