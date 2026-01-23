4368c8c092bea220b02be5800594c0ecbf5530df
Frank Secilia <frank.secilia@gmail.com>
2026-01-23 17:26:22 -0500

    feat(driver): rough in compiler.h shim

---
+++ src/kernel/shim/compiler.h
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: GPL-2.0+ OR MIT
+/*!
+    \file
+    \brief Shim header for compiler.h
+
+    \copyright Copyright (C) 2026 Frank Secilia
+*/
+
+#pragma once
+
+#define __cold
+#define __maybe_unused __attribute__((unused))
+#define __packed       __attribute__((packed))
+
+#ifndef likely
+#define likely(x) __builtin_expect(!!(x), 1)
+#endif
+
+#ifndef unlikely
+#define unlikely(x) __builtin_expect(!!(x), 0)
+#endif
+
+#define check_add_overflow(a, b, d) __builtin_add_overflow(a, b, d)
+#define check_sub_overflow(a, b, d) __builtin_sub_overflow(a, b, d)
+#define check_mul_overflow(a, b, d) __builtin_mul_overflow(a, b, d)
