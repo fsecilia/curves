38a42b59025b32d8330b118616e3a9ed9b2eb71f
Frank Secilia <frank.secilia@gmail.com>
2026-01-23 16:13:32 -0500

    feat(ui): use dink to create message box

--- src/user/crv/ui/main.cpp
+++ src/user/crv/ui/main.cpp
@@ -6,18 +6,29 @@
   \copyright Copyright (C) 2026 Frank Secilia
 */
 
+#include <dink/container.hpp>
 #include <QApplication>
 #include <QMessageBox>
 
 namespace curves {
 
+struct default_message_box_t
+{
+    auto exec() const noexcept -> int
+    {
+        return QMessageBox{QMessageBox::Information, "Curves Configuration", "Package installed successfully!"}.exec();
+    }
+};
+
 static inline auto main(int argc, char* argv[]) -> int
 {
+    auto container = dink::Container{};
+
     auto app = QApplication{argc, argv};
     QApplication::setApplicationName("curves");
     QApplication::setOrganizationName("");
 
-    return QMessageBox{QMessageBox::Information, "Curves Configuration", "Package installed successfully!"}.exec();
+    return container.resolve<default_message_box_t>().exec();
 }
 
 } // namespace curves
