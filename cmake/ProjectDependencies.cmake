9eed2447eefb815e4bcca82ccc539da1b7c6099f
Frank Secilia <frank.secilia@gmail.com>
2026-01-23 14:13:11 -0500

    build: add dependency on ccache, when available

--- cmake/ProjectDependencies.cmake
+++ cmake/ProjectDependencies.cmake
@@ -4,3 +4,13 @@
 # find system threading library
 set(THREADS_PREFER_PTHREAD_FLAG True)
 find_package(Threads REQUIRED)
+
+# use ccache, if available
+find_program(ccache ccache)
+if (ccache)
+    set(CMAKE_C_COMPILER_LAUNCHER "${ccache}")
+    set(CMAKE_CXX_COMPILER_LAUNCHER "${ccache}")
+    set(CMAKE_C_LINKER_LAUNCHER "${ccache}")
+    set(CMAKE_CXX_LINKER_LAUNCHER "${ccache}")
+endif()
+
