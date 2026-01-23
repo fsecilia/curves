d35e739b50a92f49c9b33b5eecb5823537c750e6
Frank Secilia <frank.secilia@gmail.com>
2026-01-23 11:43:11 -0500

    build: set cmake policy CMP0156 to new

---
+++ cmake/ProjectPolicies.cmake
@@ -0,0 +1,7 @@
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2026 Frank Secilia
+
+# Don't repeat libraries on build command lines.
+if (POLICY CMP0156)
+    cmake_policy(SET CMP0156 NEW)
+endif()
