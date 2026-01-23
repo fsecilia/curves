2bf61254b881ccd6b7cb09706381f47c173f3476
Frank Secilia <frank.secilia@gmail.com>
2026-01-23 14:14:38 -0500

    build: add dependency on gtest

--- cmake/ProjectDependencies.cmake
+++ cmake/ProjectDependencies.cmake
@@ -14,3 +14,19 @@ if (ccache)
     set(CMAKE_CXX_LINKER_LAUNCHER "${ccache}")
 endif()
 
+# find gtest; enable testing if found
+set(INSTALL_GTEST OFF)
+if(NOT TARGET GTest::gtest)
+    # try finding system package normally
+    find_package(GTest QUIET)
+endif()
+if(NOT TARGET GTest::gtest)
+    # look where debian puts the source build
+    if (EXISTS /usr/src/googletest)
+        add_subdirectory(/usr/src/googletest ${CMAKE_BINARY_DIR}/external/googletest EXCLUDE_FROM_ALL)
+    endif()
+endif()
+if(TARGET GTest::gtest)
+    set(enable_testing True)
+endif()
+
