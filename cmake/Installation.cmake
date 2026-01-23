ee54fef6fa3ee7a756a4c653a80144175ad4d984
Frank Secilia <frank.secilia@gmail.com>
2026-01-23 15:33:52 -0500

    docs: add file docblock to installation

--- cmake/Installation.cmake
+++ cmake/Installation.cmake
@@ -1,5 +1,8 @@
 # SPDX-License-Identifier: MIT
 # Copyright (c) 2026 Frank Secilia
+#
+# This file declares common installation metadata needed by both project installers in src and packaging ops in dist.
+# These are the naming contracts they share to ensure they refer to the same paths and files.
 
 # determine install dirs a configure time
 include(GNUInstallDirs)
