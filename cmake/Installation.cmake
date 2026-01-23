2e46b167b736b80ae67673ccd41745170c372467
Frank Secilia <frank.secilia@gmail.com>
2026-01-23 15:57:35 -0500

    build: quote strings and paths in cmake

--- cmake/Installation.cmake
+++ cmake/Installation.cmake
@@ -11,8 +11,8 @@ include(GNUInstallDirs)
 set(CMAKE_INSTALL_MESSAGE NEVER)
 
 # define long names for build output
-set(project_name_underscores ${PROJECT_NAME}_mouse_acceleration)        # internal filenames, like the .ko
-string(REPLACE "_" "-" project_name_dashes ${project_name_underscores}) # external filenames, like the config ui
+set(project_name_underscores "${PROJECT_NAME}_mouse_acceleration")        # internal filenames, like the .ko
+string(REPLACE "_" "-" project_name_dashes "${project_name_underscores}") # external filenames, like the config ui
 
 # define dkms relative install path
-set(dkms_dst_dir src/${project_name_dashes}-${PROJECT_VERSION})
+set(dkms_dst_dir "src/${project_name_dashes}-${PROJECT_VERSION}")
