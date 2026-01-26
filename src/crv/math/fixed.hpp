e986fb3f0ea24aa51281d057e8dabcc85a41d9bf
Frank Secilia <frank.secilia@gmail.com>
2026-01-26 03:54:20 -0500

    build: set up lib unit test, fixed module
    Setting up the test executable required a module. The first one we'll be
    working on is the fixed point stuff, so shell that and include it in the
    build.

---
+++ src/crv/math/fixed.hpp
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+/*!
+  \file
+  \brief fixed point integer type
+
+  \copyright Copyright (C) 2026 Frank Secilia
+*/
+
+#pragma once
+
+#include <crv/lib.hpp>
+#include <cassert>
+
+namespace crv {
+
+//
+
+} // namespace crv
