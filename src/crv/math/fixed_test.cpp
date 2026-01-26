795d1ca2ae36b9ea3f66fdb4a09898fc8b453bc7
Frank Secilia <frank.secilia@gmail.com>
2026-01-26 17:11:01 -0500

    test(fixed): add labels to asserts

--- src/crv/math/fixed_test.cpp
+++ src/crv/math/fixed_test.cpp
@@ -15,16 +15,16 @@ constexpr auto frac_bits = 21;
 using sut_t              = fixed_t<value_t, frac_bits>;
 
 // zero initialization works
-static_assert(sut_t{}.value == 0);
+static_assert(sut_t{}.value == 0, "zero initialization failed");
 
 // zero is always zero; there is no offset
-static_assert(sut_t{0}.value == 0);
+static_assert(sut_t{0}.value == 0, "value initialization tranlsated value");
 
 // value initialization is direct; no rescaling is performed
-static_assert(sut_t{1}.value == 1);
+static_assert(sut_t{1}.value == 1, "value initialization scaled value");
 
-// 1 is not a special value
-static_assert(sut_t{0xF1234}.value == 0xF1234);
+// 0 and 1 are not special
+static_assert(sut_t{0xF1234}.value == 0xF1234, "value initialization failed");
 
 } // namespace
 } // namespace crv
