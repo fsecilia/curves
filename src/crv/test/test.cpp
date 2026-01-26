264d83ae84228108466fe659a70c7db02d95c9f4
Frank Secilia <frank.secilia@gmail.com>
2026-01-26 01:49:26 -0500

    feat(test): add testing library

---
+++ src/crv/test/test.cpp
@@ -0,0 +1,13 @@
+// SPDX-License-Identifier: MIT
+/*!
+  \file
+  \copyright Copyright (C) 2026 Frank Secilia
+*/
+
+#include "test.hpp"
+
+namespace crv {
+
+//
+
+} // namespace crv
---
+++ src/crv/test/test.hpp
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+/*!
+  \file
+  \brief main test header
+
+  This file is included by every test file.
+
+  \copyright Copyright (C) 2026 Frank Secilia
+*/
+
+#pragma once
+
+#include <gtest/gtest.h>
+
+namespace crv {
+
+using namespace testing;
+
+} // namespace crv
