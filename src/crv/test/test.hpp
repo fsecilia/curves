f378cc7f99de5b93145a9c3a6787f218a1d88402
Frank Secilia <frank.secilia@gmail.com>
2026-01-26 01:57:20 -0500

    feat(test): add gtest name generator

--- src/crv/test/test.hpp
+++ src/crv/test/test.hpp
@@ -16,4 +16,15 @@ namespace crv {
 
 using namespace testing;
 
+//! name generator for readable test output
+template <typename test_vector_t> struct test_name_generator_t
+{
+    auto operator()(const TestParamInfo<test_vector_t>& info) const -> std::string
+    {
+        auto s = std::string{info.param.name};
+        std::replace_if(s.begin(), s.end(), [](char c) { return !std::isalnum(c); }, '_');
+        return s;
+    }
+};
+
 } // namespace crv
