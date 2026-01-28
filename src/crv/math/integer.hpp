ff251fb030e61bf4173591e1ec4d53026abc9b35
Frank Secilia <frank.secilia@gmail.com>
2026-01-28 15:13:52 -0500

    style(math): regroup banners
    One banner per function isn't really useful. It's ok for their tests,
    since these are looser collections, but it's too much noise in the
    header.
    
    These are now grouped by topic in the header.

--- src/crv/math/integer.hpp
+++ src/crv/math/integer.hpp
@@ -18,7 +18,7 @@
 namespace crv {
 
 // --------------------------------------------------------------------------------------------------------------------
-// int_cast
+// Conversions
 // --------------------------------------------------------------------------------------------------------------------
 
 //! asserts that from is in the representable range of to_t
@@ -28,10 +28,6 @@ template <integral to_t, integral from_t> constexpr auto int_cast(from_t from) n
     return static_cast<to_t>(from);
 }
 
-// --------------------------------------------------------------------------------------------------------------------
-// to_unsigned_abs
-// --------------------------------------------------------------------------------------------------------------------
-
 //! converts to unsigned type, applying abs when negative
 template <signed_integral signed_t>
 constexpr auto to_unsigned_abs(signed_t src) noexcept -> std::make_unsigned_t<signed_t>
@@ -39,10 +35,6 @@ constexpr auto to_unsigned_abs(signed_t src) noexcept -> std::make_unsigned_t<si
     using unsigned_t = std::make_unsigned_t<signed_t>;
     return src < 0 ? -static_cast<unsigned_t>(src) : static_cast<unsigned_t>(src);
 }
-
-// --------------------------------------------------------------------------------------------------------------------
-// to_signed_copysign
-// --------------------------------------------------------------------------------------------------------------------
 
 //! converts to signed type, applying sign of sign to result
 template <unsigned_integral unsigned_t>
