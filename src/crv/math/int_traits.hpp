d6809fe93909dc277b1e5c47f9b6bd04ef44c70b
Frank Secilia <frank.secilia@gmail.com>
2026-01-28 09:37:35 -0500

    refactor(math): move sized_integer_t to traits

--- src/crv/math/int_traits.hpp
+++ src/crv/math/int_traits.hpp
@@ -50,4 +50,30 @@ template <typename value_t> constexpr auto is_arithmetic_v = is_arithmetic<value
 template <typename value_t>
 concept arithmetic = is_arithmetic_v<value_t>;
 
+// --------------------------------------------------------------------------------------------------------------------
+// sized_integer_t
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace detail::integer {
+
+template <int_t size, bool is_signed> struct sized_integer_f;
+
+// clang-format off
+template <> struct sized_integer_f<1, false> { using type = uint8_t; };
+template <> struct sized_integer_f<2, false> { using type = uint16_t; };
+template <> struct sized_integer_f<4, false> { using type = uint32_t; };
+template <> struct sized_integer_f<8, false> { using type = uint64_t; };
+template <> struct sized_integer_f<16, false> { using type = uint128_t; };
+
+template <> struct sized_integer_f<1, true> { using type = int8_t; };
+template <> struct sized_integer_f<2, true> { using type = int16_t; };
+template <> struct sized_integer_f<4, true> { using type = int32_t; };
+template <> struct sized_integer_f<8, true> { using type = int64_t; };
+template <> struct sized_integer_f<16, true> { using type = int128_t; };
+// clang-format on
+
+} // namespace detail::integer
+
+template <int_t size, bool is_signed> using sized_integer_t = detail::integer::sized_integer_f<size, is_signed>::type;
+
 } // namespace crv
--- src/crv/math/integer_test.cpp
+++ src/crv/math/integer_test.cpp
@@ -10,42 +10,7 @@
 namespace crv {
 namespace {
 
-// --------------------------------------------------------------------------------------------------------------------
-// sized_integer_t
-// --------------------------------------------------------------------------------------------------------------------
-
-namespace sized_integer_tests {
-
-/*
-    gcc's implementation of std::is_signed is conditional on std::is_arithmetic. int128 is not arithmetic, so checking
-    it against is_signed fails. This implementation does the same check as is_signed, without being obstructed by
-    is_arithmetic.
-*/
-template <typename value_t> constexpr auto is_signed_v = value_t(-1) < value_t(0);
-
-// tests is_integral, is_signed, and size for the given sized_integer_t
-template <int_t expected_size, bool expected_is_signed> constexpr auto test_size() noexcept -> void
-{
-    using actual_t = sized_integer_t<expected_size, expected_is_signed>;
-
-    static_assert(integral<actual_t>, "sized_integer_t: result was not integral");
-    static_assert(expected_size == sizeof(actual_t), "sized_integer_t: size did not match");
-    static_assert(expected_is_signed == is_signed_v<actual_t>, "sized_integer_t: is_signed did not match");
-};
-
-// runs signed and unsigned test_size() for given sizes
-template <int_t... sizes> constexpr auto test_sizes() noexcept -> void
-{
-    (..., (test_size<sizes, true>(), test_size<sizes, false>()));
-}
-
-// runs test_sizes() for all supported integer sizes
-constexpr auto test_all_sizes() noexcept -> void
-{
-    test_sizes<1, 2, 4, 8, 16>();
-}
-
-} // namespace sized_integer_tests
+//
 
 } // namespace
 } // namespace crv
