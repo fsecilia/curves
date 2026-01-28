ad8b3b78c442ab6bb778d7fb109caadac9aa954f
Frank Secilia <frank.secilia@gmail.com>
2026-01-28 06:49:56 -0500

    feat(math): add sized_integer_t

--- src/crv/math/integer.hpp
+++ src/crv/math/integer.hpp
@@ -15,6 +15,28 @@
 
 namespace crv {
 
+namespace detail::integer {
+
+template <int_t size, bool is_signed> struct sized_integer_f;
+
+// clang-format off
+template <> struct sized_integer_f<1, false> { using type = uint8_t; };
+template <> struct sized_integer_f<2, false> { using type = uint16_t; };
+template <> struct sized_integer_f<4, false> { using type = uint32_t; };
+template <> struct sized_integer_f<8, false> { using type = uint64_t; };
+template <> struct sized_integer_f<16, false> { using type = uint128_t; };
+
+template <> struct sized_integer_f<1, true> { using type = int8_t; };
+template <> struct sized_integer_f<2, true> { using type = int16_t; };
+template <> struct sized_integer_f<4, true> { using type = int32_t; };
+template <> struct sized_integer_f<8, true> { using type = int64_t; };
+template <> struct sized_integer_f<16, true> { using type = int128_t; };
+// clang-format on
+
+} // namespace detail::integer
+
+template <int_t size, bool is_signed> using sized_integer_t = detail::integer::sized_integer_f<size, is_signed>::type;
+
 //! asserts that from is in the representable range of to_t
 template <std::integral to_t, std::integral from_t> constexpr auto int_cast(from_t from) noexcept -> to_t
 {
