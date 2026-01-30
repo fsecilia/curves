6ff385d3d3a3f50a3209728b2abe88385b2ad171
Frank Secilia <frank.secilia@gmail.com>
2026-01-30 11:53:22 -0500

    feat(jet): add scalar fallbacks for primal, derivative

--- src/crv/math/jet/jet.hpp
+++ src/crv/math/jet/jet.hpp
@@ -12,6 +12,18 @@
 
 namespace crv {
 
-//
+// --------------------------------------------------------------------------------------------------------------------
+// Scalar Fallbacks
+// --------------------------------------------------------------------------------------------------------------------
+
+template <typename scalar_t> constexpr auto primal(scalar_t const& scalar) noexcept -> scalar_t
+{
+    return scalar;
+}
+
+template <typename scalar_t> constexpr auto derivative(scalar_t const&) noexcept -> scalar_t
+{
+    return scalar_t{};
+}
 
 } // namespace crv
--- src/crv/math/jet/test/jet.cpp
+++ src/crv/math/jet/test/jet.cpp
@@ -10,7 +10,31 @@
 namespace crv {
 namespace {
 
-//
+struct jet_test_t : Test
+{
+    using scalar_t = real_t;
+
+    static constexpr scalar_t f  = 37.2; // arbitrary
+    static constexpr scalar_t df = 26.3; // arbitrary
+};
+
+// ====================================================================================================================
+// Scalar Fallbacks
+// ====================================================================================================================
+
+TEST_F(jet_test_t, primal)
+{
+    static_assert(f == primal(f));
+    static_assert(-f == primal(-f));
+    static_assert(df == primal(df));
+}
+
+TEST_F(jet_test_t, derivative)
+{
+    static_assert(scalar_t{} == derivative(f));
+    static_assert(scalar_t{} == derivative(-f));
+    static_assert(scalar_t{} == derivative(df));
+}
 
 } // namespace
 } // namespace crv
