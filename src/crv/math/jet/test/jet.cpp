8a9cb6ab64d060c6ffb5d0a824395b3520130319
Frank Secilia <frank.secilia@gmail.com>
2026-01-30 16:16:35 -0500

    test(jet): use static_assert where possible

--- src/crv/math/jet/test/jet.cpp
+++ src/crv/math/jet/test/jet.cpp
@@ -82,38 +82,38 @@ TEST_F(jet_test_construction_t, default_construction)
 {
     constexpr auto sut = sut_t{};
 
-    EXPECT_EQ(sut.f, 0.0);
-    EXPECT_EQ(sut.df, 0.0);
+    static_assert(sut.f == 0.0);
+    static_assert(sut.df == 0.0);
 }
 
 TEST_F(jet_test_construction_t, scalar)
 {
     constexpr auto sut = jet_t{f};
 
-    EXPECT_EQ(sut.f, f);
-    EXPECT_EQ(sut.df, 0.0);
+    static_assert(sut.f == f);
+    static_assert(sut.df == 0.0);
 }
 
 TEST_F(jet_test_construction_t, nested_scalar)
 {
     constexpr auto sut = jet_t<jet_t<double>>{x};
 
-    EXPECT_EQ(sut.f, x);
-    EXPECT_EQ(sut.df, jet_t{0.0});
+    static_assert(sut.f == x);
+    static_assert(sut.df == jet_t{0.0});
 }
 
 TEST_F(jet_test_construction_t, pair)
 {
-    EXPECT_EQ(x.f, f);
-    EXPECT_EQ(x.df, df);
+    static_assert(x.f == f);
+    static_assert(x.df == df);
 }
 
 TEST_F(jet_test_construction_t, broadcast)
 {
     constexpr auto sut = jet_t<jet_t<double>>{f};
 
-    EXPECT_EQ(sut.f, jet_t{f});
-    EXPECT_EQ(sut.df, jet_t{0.0});
+    static_assert(sut.f == jet_t{f});
+    static_assert(sut.df == jet_t{0.0});
 }
 
 // ====================================================================================================================
@@ -131,8 +131,8 @@ TEST_F(jet_test_conversion_t, ctor)
     constexpr auto jet_int = jet_t{f_int, df_int};
     constexpr auto sut     = sut_t{jet_int};
 
-    EXPECT_DOUBLE_EQ(primal(sut), static_cast<scalar_t>(f_int));
-    EXPECT_DOUBLE_EQ(derivative(sut), static_cast<scalar_t>(df_int));
+    static_assert(primal(sut) == static_cast<scalar_t>(f_int));
+    static_assert(derivative(sut) == static_cast<scalar_t>(df_int));
 }
 
 TEST_F(jet_test_conversion_t, assign)
@@ -148,16 +148,16 @@ TEST_F(jet_test_conversion_t, assign)
 
 TEST_F(jet_test_conversion_t, to_bool_true)
 {
-    EXPECT_TRUE(static_cast<bool>(sut_t{1.0, 0.0}));
-    EXPECT_TRUE(static_cast<bool>(sut_t{-1.0, 0.0}));
-    EXPECT_TRUE(static_cast<bool>(sut_t{0.001, 0.0}));
-    EXPECT_TRUE(static_cast<bool>(sut_t{1.0, 999.0}));
+    static_assert(static_cast<bool>(sut_t{1.0, 0.0}));
+    static_assert(static_cast<bool>(sut_t{-1.0, 0.0}));
+    static_assert(static_cast<bool>(sut_t{0.001, 0.0}));
+    static_assert(static_cast<bool>(sut_t{1.0, 999.0}));
 }
 
 TEST_F(jet_test_conversion_t, to_bool_false)
 {
-    EXPECT_FALSE(static_cast<bool>(sut_t{0.0, 0.0}));
-    EXPECT_FALSE(static_cast<bool>(sut_t{0.0, 999.0}));
+    static_assert(!static_cast<bool>(sut_t{0.0, 0.0}));
+    static_assert(!static_cast<bool>(sut_t{0.0, 999.0}));
 }
 
 // ====================================================================================================================
@@ -169,12 +169,12 @@ struct jet_test_accessors_t : jet_test_t
 
 TEST_F(jet_test_construction_t, primal)
 {
-    EXPECT_EQ(x.f, primal(x));
+    static_assert(x.f == primal(x));
 }
 
 TEST_F(jet_test_construction_t, derivative)
 {
-    EXPECT_EQ(x.df, derivative(x));
+    static_assert(x.df == derivative(x));
 }
 
 // ====================================================================================================================
@@ -187,42 +187,42 @@ struct jet_test_comparison_t : jet_test_t
 TEST_F(jet_test_comparison_t, element_equality)
 {
     // equal only if primal matches AND derivative is zero
-    EXPECT_TRUE((jet_t{5.0, 0.0} == 5.0));
-    EXPECT_FALSE((jet_t{5.0, 1.0} == 5.0));
-    EXPECT_FALSE((jet_t{5.1, 0.0} == 5.0));
+    static_assert(jet_t{5.0, 0.0} == 5.0);
+    static_assert(jet_t{5.0, 1.0} != 5.0);
+    static_assert(jet_t{5.1, 0.0} != 5.0);
 }
 
 TEST_F(jet_test_comparison_t, element_ordering)
 {
-    EXPECT_TRUE((jet_t{3.0, 999.0} != 4.0));
-    EXPECT_TRUE((jet_t{3.0, 999.0} < 4.0));
+    static_assert(jet_t{3.0, 999.0} != 4.0);
+    static_assert(jet_t{3.0, 999.0} < 4.0);
 
-    EXPECT_TRUE((jet_t{5.0, 999.0} != 4.0));
-    EXPECT_TRUE((jet_t{5.0, 999.0} > 4.0));
+    static_assert(jet_t{5.0, 999.0} != 4.0);
+    static_assert(jet_t{5.0, 999.0} > 4.0);
 
-    EXPECT_TRUE((jet_t{4.0, 999.0} != 4.0));
-    EXPECT_TRUE((jet_t{4.0, 999.0} <= 4.0));
-    EXPECT_TRUE((jet_t{4.0, 999.0} >= 4.0));
+    static_assert(jet_t{4.0, 999.0} != 4.0);
+    static_assert(jet_t{4.0, 999.0} <= 4.0);
+    static_assert(jet_t{4.0, 999.0} >= 4.0);
 }
 
 TEST_F(jet_test_comparison_t, jet_equality)
 {
-    EXPECT_TRUE((jet_t{3.0, 2.0} == jet_t{3.0, 2.0}));
-    EXPECT_FALSE((jet_t{3.0, 2.0} == jet_t{3.0, 3.0}));
-    EXPECT_FALSE((jet_t{3.0, 2.0} == jet_t{4.0, 2.0}));
+    static_assert(jet_t{3.0, 2.0} == jet_t{3.0, 2.0});
+    static_assert(jet_t{3.0, 2.0} != jet_t{3.0, 3.0});
+    static_assert(jet_t{3.0, 2.0} != jet_t{4.0, 2.0});
 }
 
 TEST_F(jet_test_comparison_t, jet_ordering)
 {
-    EXPECT_TRUE((jet_t{3.0, 999.0} != jet_t{4.0, 0.0}));
-    EXPECT_TRUE((jet_t{3.0, 999.0} < jet_t{4.0, 0.0}));
+    static_assert(jet_t{3.0, 999.0} != jet_t{4.0, 0.0});
+    static_assert(jet_t{3.0, 999.0} < jet_t{4.0, 0.0});
 
-    EXPECT_TRUE((jet_t{5.0, 0.0} != jet_t{4.0, 999.0}));
-    EXPECT_TRUE((jet_t{5.0, 0.0} > jet_t{4.0, 999.0}));
+    static_assert(jet_t{5.0, 0.0} != jet_t{4.0, 999.0});
+    static_assert(jet_t{5.0, 0.0} > jet_t{4.0, 999.0});
 
-    EXPECT_TRUE((jet_t{4.0, 1.0} != jet_t{4.0, 2.0}));
-    EXPECT_TRUE((jet_t{4.0, 1.0} <= jet_t{4.0, 2.0}));
-    EXPECT_TRUE((jet_t{4.0, 2.0} >= jet_t{4.0, 1.0}));
+    static_assert(jet_t{4.0, 1.0} != jet_t{4.0, 2.0});
+    static_assert(jet_t{4.0, 1.0} <= jet_t{4.0, 2.0});
+    static_assert(jet_t{4.0, 2.0} >= jet_t{4.0, 1.0});
 }
 
 } // namespace
