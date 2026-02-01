f74a3ea41302c5b8d5bfa0c24d89660c60d2664a
Frank Secilia <frank.secilia@gmail.com>
2026-02-01 16:20:52 -0500

    test(jet): shell nested fixture

--- src/crv/math/jet/test/nested_jet.cpp
+++ src/crv/math/jet/test/nested_jet.cpp
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: MIT
 /**
     \file
+    \brief tests jet composition over jets
+
+    These tests verify that autodiff composes correctly to calc second derivatives via jet_t<jet_t<double>>.
+
     \copyright Copyright (C) 2026 Frank Secilia
 */
 
@@ -10,7 +14,20 @@
 namespace crv {
 namespace {
 
-//
+struct nested_jet_test_t : Test
+{
+    using scalar_t = double;
+    using value_t  = jet_t<scalar_t>;
+    using sut_t    = jet_t<value_t>;
+
+    // seed with primish numbers
+    static constexpr scalar_t s{1.3};
+    static constexpr value_t  v{1.7, 1.9};
+    static constexpr sut_t    x{{2.3, 3.1}, {5.3, 7.1}};
+    static constexpr sut_t    y{{5.9, 7.3}, {8.3, 9.7}};
+
+    static constexpr scalar_t eps = 1e-10;
+};
 
 } // namespace
 } // namespace crv
