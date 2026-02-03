2cab1d21886bb453af66d0e2017ca94333c39556
Frank Secilia <frank.secilia@gmail.com>
2026-02-03 11:17:30 -0500

    build: add integration test

---
+++ src/crv/test/integration/integration_test.hpp
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+/*!
+    \file
+    \brief main integration test header
+
+    This file is included by every integrationtest file.
+
+    \copyright Copyright (C) 2026 Frank Secilia
+*/
+
+#pragma once
+
+#include <crv/test/test.hpp>
+
+namespace crv {
+
+//
+
+} // namespace crv
