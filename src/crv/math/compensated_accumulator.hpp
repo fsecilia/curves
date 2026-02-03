8053da502df40cc8641bdf409d50824bc7b1ec2c
Frank Secilia <frank.secilia@gmail.com>
2026-02-02 20:11:35 -0500

    feat(math): add compensated accumulator

---
+++ src/crv/math/compensated_accumulator.hpp
@@ -0,0 +1,40 @@
+// SPDX-License-Identifier: MIT
+/*!
+    \file
+    \brief provides an accumulator using Kahan summation to compensate for precision loss during addition
+
+    \copyright Copyright (C) 2026 Frank Secilia
+*/
+
+#pragma once
+
+#include <crv/lib.hpp>
+
+namespace crv {
+
+/*!
+    accumulates a sum using Kahan summation
+
+    Kahan summation tracks the error from each addition, then reintroduces it in the next, increasing the accuracy of
+    the sum overall.
+
+    This is a minimal implementation meant to be a drop-in replacement for simple sums consisting soley as a series of
+    operator +=, then reading the final value.
+*/
+template <typename real_t> struct compensated_accumulator_t
+{
+    real_t sum          = real_t{0};
+    real_t compensation = real_t{0};
+
+    auto operator+=(real_t value) -> void
+    {
+        auto const y = value - compensation;
+        auto const t = sum + y;
+        compensation = (t - sum) - y;
+        sum          = t;
+    }
+
+    operator real_t() const { return sum; }
+};
+
+} // namespace crv
---
+++ src/crv/math/compensated_accumulator_test.cpp
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: MIT
+/**
+    \file
+    \copyright Copyright (C) 2026 Frank Secilia
+*/
+
+#include "compensated_accumulator.hpp"
+#include <crv/math/limits.hpp>
+#include <crv/test/test.hpp>
+
+namespace crv {
+namespace {
+
+struct compensated_accumulator_test_t : Test
+{
+    using real_t = float32_t;
+    using sut_t  = compensated_accumulator_t<real_t>;
+
+    static constexpr auto iterations = int_t{1'000'000};
+};
+
+TEST_F(compensated_accumulator_test_t, sums_normally)
+{
+    auto sut = sut_t{0};
+
+    for (auto i = 0; i < iterations; ++i) sut += i;
+
+    EXPECT_EQ(static_cast<float>(iterations * (iterations - 1)) / 2, static_cast<float>(sut));
+}
+
+TEST_F(compensated_accumulator_test_t, catches_vanishing_udates)
+{
+    auto const large_value     = real_t{1.0};
+    auto const small_value     = epsilon<real_t>() / 10;
+    auto const expected_change = small_value * iterations;
+
+    auto reference = large_value;
+    auto sut       = sut_t{large_value};
+    for (int i = 0; i < iterations; ++i)
+    {
+        reference += small_value;
+        sut += small_value;
+    }
+
+    EXPECT_EQ(reference, large_value);
+    EXPECT_EQ(static_cast<float>(sut), large_value + expected_change);
+}
+
+} // namespace
+} // namespace crv
