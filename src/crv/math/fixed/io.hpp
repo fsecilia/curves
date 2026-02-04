c56a2b2294330dc865241f0d1e15a421c7f095ce
Frank Secilia <frank.secilia@gmail.com>
2026-02-04 00:10:43 -0500

    feat(fixed): ostream inserter prints float
    There is a hint of circularity here because the test for fixed point
    conversions, now used here, also prints via this function. However,
    it is technically correct, printing fixed is unlikely to break in a
    way that breaks the test, and breaking printing can't affect the test
    results, only its output.

--- src/crv/math/fixed/io.hpp
+++ src/crv/math/fixed/io.hpp
@@ -10,6 +10,7 @@
 
 #include <crv/lib.hpp>
 #include <crv/math/fixed/fixed.hpp>
+#include <crv/math/fixed/float.hpp>
 #include <ostream>
 
 namespace crv {
@@ -17,7 +18,7 @@ namespace crv {
 template <integral value_type, int_t frac_bits>
 auto operator<<(std::ostream& out, fixed_t<value_type, frac_bits> const& src) -> std::ostream&
 {
-    return out << src.value;
+    return out << from_fixed<float_t>(src);
 }
 
 } // namespace crv
