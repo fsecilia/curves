04a463f5b2852d4a62b6f2dfdc6e063aad2e8061
Frank Secilia <frank.secilia@gmail.com>
2026-02-26 16:27:55 -0500

    feat(div): add is_result

---
+++ src/crv/math/division/concepts.hpp
@@ -0,0 +1,33 @@
+// SPDX-License-Identifier: MIT
+/*!
+    \file
+    \brief division-specific concepts
+
+    \copyright Copyright (C) 2026 Frank Secilia
+*/
+
+#pragma once
+
+#include <crv/lib.hpp>
+#include <crv/math/division/result.hpp>
+#include <type_traits>
+
+namespace crv::division {
+
+// --------------------------------------------------------------------------------------------------------------------
+// is_result
+// --------------------------------------------------------------------------------------------------------------------
+
+template <typename result_t> struct is_result_f : std::false_type
+{};
+
+template <typename quotient_t, typename remainder_t>
+struct is_result_f<result_t<quotient_t, remainder_t>> : std::true_type
+{};
+
+template <typename result_t> inline constexpr auto is_result_v = is_result_f<result_t>::value;
+
+template <typename result_t>
+concept is_result = is_result_v<std::remove_cvref_t<result_t>>;
+
+} // namespace crv::division
