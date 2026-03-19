d7a6e341246f44a2b3b6b1801b982b343fa1db6e
Frank Secilia <frank.secilia@gmail.com>
2026-03-19 11:34:52 -0400

    test(div): simplify rounding_divider_t test
    The mock test is conventional, but can't test constexprness. To test
    that would require a test with almost as many moving parts. Instead,
    this test now replaces the whole mock test using prime product hashes
    to verify that all of the parameters are passed in the correct order.
    
    Using what amounts to Gödel numbering in a unit test like this feels
    like focusing a galactic mazer to power a toy, but it is quite effective
    here.

--- src/crv/math/division/rounding_divider_test.cpp
+++ src/crv/math/division/rounding_divider_test.cpp
@@ -6,82 +6,48 @@
 #include "rounding_divider.hpp"
 #include <crv/math/division/result.hpp>
 #include <crv/test/test.hpp>
-#include <gmock/gmock.h>
 
 namespace crv::division {
 namespace {
 
-struct division_rounding_mode_test_t : Test
+using narrow_t = uint32_t;
+using wide_t   = wider_t<narrow_t>;
+using result_t = result_t<wide_t, narrow_t>;
+
+struct rounding_mode_t
 {
-    using narrow_t = uint8_t;
-    using wide_t   = wider_t<narrow_t>;
-    using result_t = result_t<wide_t, narrow_t>;
-
-    struct mock_rounding_mode_t
+    constexpr auto div_bias(wide_t dividend, narrow_t divisor) const noexcept -> wide_t
     {
-        MOCK_METHOD(wide_t, div_bias, (wide_t dividend, narrow_t divisor), (const, noexcept));
-        MOCK_METHOD(wide_t, div_carry, (wide_t quotient, narrow_t divisor, narrow_t remainder), (const, noexcept));
-        virtual ~mock_rounding_mode_t() = default;
-    };
-    StrictMock<mock_rounding_mode_t> mock_rounding_mode{};
+        return dividend * 2 + divisor;
+    }
 
-    struct rounding_mode_t
+    constexpr auto div_carry(wide_t quotient, narrow_t divisor, narrow_t remainder) const noexcept -> wide_t
     {
-        mock_rounding_mode_t* mock = nullptr;
-
-        auto div_bias(wide_t dividend, narrow_t divisor) const noexcept -> wide_t
-        {
-            return mock->div_bias(dividend, divisor);
-        }
-
-        auto div_carry(wide_t quotient, narrow_t divisor, narrow_t remainder) const noexcept -> wide_t
-        {
-            return mock->div_carry(quotient, divisor, remainder);
-        }
-    };
-    rounding_mode_t rounding_mode{&mock_rounding_mode};
-
-    struct mock_divider_t
-    {
-        MOCK_METHOD(result_t, divide, (wide_t dividend, narrow_t divisor), (const, noexcept));
-        virtual ~mock_divider_t() = default;
-    };
-    StrictMock<mock_divider_t> mock_divider;
-
-    struct divider_t
-    {
-        mock_divider_t* mock = nullptr;
-
-        auto operator()(wide_t dividend, narrow_t divisor) const noexcept -> result_t
-        {
-            return mock->divide(dividend, divisor);
-        }
-    };
-
-    using sut_t = rounding_divider_t<narrow_t, divider_t>;
-    sut_t const sut{divider_t{&mock_divider}};
+        return quotient * 7 + divisor * 5 + remainder * 3;
+    }
 };
 
-TEST_F(division_rounding_mode_test_t, divide)
+struct divider_t
 {
-    auto const seq = InSequence{};
+    constexpr auto operator()(wide_t dividend, narrow_t divisor) const noexcept -> result_t
+    {
+        return {dividend * 13, divisor * 11};
+    }
+};
 
-    // all arbitrary
-    auto const dividend          = wide_t{2593};
-    auto const divisor           = narrow_t{59};
-    auto const biased_dividend   = dividend + 1;
-    auto const result            = result_t{443, 3};
-    auto const expected_quotient = wide_t{541};
+// arbitrary test inputs
+constexpr auto dividend = wide_t{17};
+constexpr auto divisor  = narrow_t{23};
 
-    EXPECT_CALL(mock_rounding_mode, div_bias(dividend, divisor)).WillOnce(Return(biased_dividend));
-    EXPECT_CALL(mock_divider, divide(biased_dividend, divisor)).WillOnce(Return(result));
-    EXPECT_CALL(mock_rounding_mode, div_carry(result.quotient, divisor, result.remainder))
-        .WillOnce(Return(expected_quotient));
+// expected flow through fakes
+constexpr auto expected_biased_dividend = dividend * 2 + divisor;
+constexpr auto expected_quotient        = expected_biased_dividend * 13;
+constexpr auto expected_remainder       = divisor * 11;
 
-    auto const actual_quotient = sut(dividend, divisor, rounding_mode);
+// expected final output
+constexpr auto expected_final_result = expected_quotient * 7 + divisor * 5 + expected_remainder * 3;
 
-    EXPECT_EQ(expected_quotient, actual_quotient);
-}
+static_assert(rounding_divider_t<narrow_t, divider_t>{}(dividend, divisor, rounding_mode_t{}) == expected_final_result);
 
 } // namespace
 } // namespace crv::division
