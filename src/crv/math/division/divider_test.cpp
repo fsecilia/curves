2981952f76dd06edee27655874f453c8d5ecaa83
Frank Secilia <frank.secilia@gmail.com>
2026-03-23 14:09:45 -0400

    build: rename to divider
    long_divider_t, dispatching_divider_t, and rounding_divider_t will be
    merged into divider_t. The file with the deepest history is
    long_divider, so that is the one we start with.

--- src/crv/math/division/divider_test.cpp
+++ src/crv/math/division/divider_test.cpp
@@ -3,7 +3,7 @@
 /// \file
 /// \copyright Copyright (C) 2026 Frank Secilia
 
-#include "long_divider.hpp"
+#include "divider.hpp"
 #include <crv/math/io.hpp>
 #include <crv/test/test.hpp>
 #include <random>
