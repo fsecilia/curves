a2a1ef08d05be43bc5fefc3e5ad4a0d08d84f4cc
fsecilia <frank.secilia@gmail.com>
2026-04-03 09:36:36 -0400

    refactor(mesh): rename to match literature
    This revision changes names to match approximation theory terms.

---
+++ src/crv/math/mesh/residual_estimator.hpp
@@ -0,0 +1,67 @@
+// SPDX-License-Identifier: MIT
+
+/// \file
+/// \brief residual estimator for adaptive mesh refinement
+/// \copyright Copyright (C) 2026 Frank Secilia
+
+#pragma once
+
+#include <crv/lib.hpp>
+#include <crv/math/mesh/measured_error.hpp>
+#include <cmath>
+
+namespace crv {
+
+/// \brief estimates the maximum residual error of an approximant over a specific domain interval
+///
+/// This type is an orchestrater. It organizes a minimax search to find the worst-case error between a target function
+/// and its generated approximant. The search space is defined by a node generator to find collocation points. The
+/// domain is quantized to fixed-point precision prior to evaluation.
+template <typename real_t, typename target_function_t, typename approximant_evaluator_t, typename node_generator_t,
+          typename quantizer_t>
+struct residual_estimator_t
+{
+    target_function_t       evaluate_target;
+    approximant_evaluator_t evaluate_approximant;
+    node_generator_t        generate_nodes;
+    quantizer_t             quantize;
+
+    auto operator()(auto const& approximant, real_t left, real_t right) const noexcept -> measured_error_t<real_t>
+    {
+        auto const center = (right + left) * 0.5;
+        auto const radius = (right - left) * 0.5;
+
+        auto result = measured_error_t<real_t>{};
+
+        for (auto const node : generate_nodes())
+        {
+            // sub exact center and boundries with no truncation error using cmov
+            //
+            // Applying this to left and right prevents truncation from landing the evaluated position outside of the
+            // domain. We also apply it to center because it's trivial to and center tends to suffer from truncation.
+            auto target_position = center + radius * node;
+            if (node == -1.0) target_position = left;
+            if (node == 0.0) target_position = center;
+            if (node == 1.0) target_position = right;
+
+            auto const quantized_position = quantize(target_position);
+
+            // evaluate both target at target position and approxmant at quantized position, measure error as
+            // difference
+            auto const target        = evaluate_target(target_position);
+            auto const approximation = evaluate_approximant(approximant, quantized_position);
+            auto const magnitude     = std::abs(target - approximation);
+
+            // argmax on magnitude
+            if (magnitude > result.magnitude)
+            {
+                result.position  = quantized_position;
+                result.magnitude = magnitude;
+            }
+        }
+
+        return result;
+    }
+};
+
+} // namespace crv
--- src/crv/math/mesh/residual_estimator_test.cpp
+++ src/crv/math/mesh/residual_estimator_test.cpp
@@ -3,21 +3,21 @@
 /// \file
 /// \copyright Copyright (C) 2026 Frank Secilia
 
-#include "analyzer.hpp"
+#include "residual_estimator.hpp"
 #include <crv/test/test.hpp>
 #include <gmock/gmock.h>
 
-namespace crv::equioscillation {
+namespace crv {
 namespace {
 
-struct equioscillation_error_metric_test_t : Test
+struct residual_estimator_test_t : Test
 {
-    using real_t    = float_t;
-    using payload_t = int_t;
+    using real_t        = float_t;
+    using approximant_t = int_t;
 
-    static constexpr auto const payload = payload_t{123};
-    static constexpr auto const left    = 0.0;
-    static constexpr auto const right   = 10.0;
+    static constexpr auto const approximant = approximant_t{123};
+    static constexpr auto const left        = 0.0;
+    static constexpr auto const right       = 10.0;
 
     using measured_error_t                             = measured_error_t<real_t>;
     static constexpr auto expected_max_error_magnitude = 10.0;
@@ -25,35 +25,42 @@ struct equioscillation_error_metric_test_t : Test
     static constexpr auto expected_positions           = std::array{0.0, 2.5, 5.0, 7.5, 10.0};
     static constexpr auto expected_quantized_positions = std::array{1.1, 2.2, 3.3, 4.4, 5.5};
 
-    struct mock_ideal_function_t
+    struct mock_target_function_t
     {
         MOCK_METHOD(real_t, call, (real_t), (const, noexcept));
-        virtual ~mock_ideal_function_t() = default;
+        virtual ~mock_target_function_t() = default;
     };
-    StrictMock<mock_ideal_function_t> mock_ideal_function;
+    StrictMock<mock_target_function_t> mock_target_function;
 
-    struct ideal_function_t
+    struct target_function_t
     {
-        mock_ideal_function_t* mock = nullptr;
+        mock_target_function_t* mock = nullptr;
 
         auto operator()(real_t position) const noexcept -> real_t { return mock->call(position); }
     };
 
-    struct mock_evaluator_t
+    struct mock_approximant_evaluator_t
     {
-        MOCK_METHOD(real_t, call, (payload_t, real_t), (const, noexcept));
-        virtual ~mock_evaluator_t() = default;
+        MOCK_METHOD(real_t, call, (approximant_t, real_t), (const, noexcept));
+        virtual ~mock_approximant_evaluator_t() = default;
     };
-    StrictMock<mock_evaluator_t> mock_evaluator;
+    StrictMock<mock_approximant_evaluator_t> mock_approximant_evaluator;
 
-    struct evaluator_t
+    struct approximant_evaluator_t
     {
-        mock_evaluator_t* mock = nullptr;
+        mock_approximant_evaluator_t* mock = nullptr;
 
-        auto operator()(payload_t payload, real_t position) const noexcept -> real_t
+        auto operator()(approximant_t approximant, real_t position) const noexcept -> real_t
         {
-            return mock->call(payload, position);
+            return mock->call(approximant, position);
         }
+    };
+
+    struct node_generator_t
+    {
+        using sample_locations_t = std::array<real_t, 5>;
+        static constexpr sample_locations_t sample_locations{-1.0, -0.5, 0.0, 0.5, 1.0};
+        auto operator()() const noexcept -> sample_locations_t const& { return sample_locations; }
     };
 
     struct mock_quantizer_t
@@ -70,25 +77,19 @@ struct equioscillation_error_metric_test_t : Test
         auto operator()(real_t value) const noexcept -> real_t { return mock->call(value); }
     };
 
-    struct sample_locator_t
-    {
-        using sample_locations_t = std::array<real_t, 5>;
-        static constexpr sample_locations_t sample_locations{-1.0, -0.5, 0.0, 0.5, 1.0};
-        auto operator()() const noexcept -> sample_locations_t const& { return sample_locations; }
-    };
-
-    using sut_t = error_metric_t<real_t, ideal_function_t, evaluator_t, quantizer_t, sample_locator_t>;
+    using sut_t
+        = residual_estimator_t<real_t, target_function_t, approximant_evaluator_t, node_generator_t, quantizer_t>;
     sut_t sut{
-        .ideal_function = {&mock_ideal_function},
-        .evaluator      = {&mock_evaluator},
-        .quantizer{&mock_quantizer},
-        .sample_locator{},
+        .evaluate_target      = {&mock_target_function},
+        .evaluate_approximant = {&mock_approximant_evaluator},
+        .generate_nodes{},
+        .quantize{&mock_quantizer},
     };
 
     // these must evaluate in ascending order to maximize quadrature cache hits
     InSequence seq;
 
-    equioscillation_error_metric_test_t() {}
+    residual_estimator_test_t() {}
 
     auto expect_iteration(int_t position_index, real_t result = 0.0) -> void
     {
@@ -96,12 +97,13 @@ struct equioscillation_error_metric_test_t : Test
         auto const expected_quantized_position = expected_quantized_positions[position_index];
 
         EXPECT_CALL(mock_quantizer, call(expected_position)).WillOnce(Return(expected_quantized_position));
-        EXPECT_CALL(mock_ideal_function, call(expected_position)).WillOnce(Return(0.0));
-        EXPECT_CALL(mock_evaluator, call(payload, expected_quantized_position)).WillOnce(Return(result));
+        EXPECT_CALL(mock_target_function, call(expected_position)).WillOnce(Return(0.0));
+        EXPECT_CALL(mock_approximant_evaluator, call(approximant, expected_quantized_position))
+            .WillOnce(Return(result));
     }
 };
 
-TEST_F(equioscillation_error_metric_test_t, max_error_at_first_sample_location)
+TEST_F(residual_estimator_test_t, max_error_at_first_sample_location)
 {
     expect_iteration(0, expected_max_error_magnitude);
     expect_iteration(1, expected_max_error_magnitude / 2);
@@ -112,12 +114,12 @@ TEST_F(equioscillation_error_metric_test_t, max_error_at_first_sample_location)
     auto const expected
         = measured_error_t{.position = expected_quantized_positions[0], .magnitude = expected_max_error_magnitude};
 
-    auto const actual = sut(payload, left, right);
+    auto const actual = sut(approximant, left, right);
 
     EXPECT_EQ(actual, expected);
 }
 
-TEST_F(equioscillation_error_metric_test_t, max_error_at_last_sample_location)
+TEST_F(residual_estimator_test_t, max_error_at_last_sample_location)
 {
     expect_iteration(0, expected_max_error_magnitude / 2);
     expect_iteration(1);
@@ -128,12 +130,12 @@ TEST_F(equioscillation_error_metric_test_t, max_error_at_last_sample_location)
     auto const expected
         = measured_error_t{.position = expected_quantized_positions[4], .magnitude = expected_max_error_magnitude};
 
-    auto const actual = sut(payload, left, right);
+    auto const actual = sut(approximant, left, right);
 
     EXPECT_EQ(actual, expected);
 }
 
-TEST_F(equioscillation_error_metric_test_t, handles_negative_error_correctly)
+TEST_F(residual_estimator_test_t, handles_negative_error_correctly)
 {
     expect_iteration(0);
     expect_iteration(1, -expected_max_error_magnitude);
@@ -144,7 +146,7 @@ TEST_F(equioscillation_error_metric_test_t, handles_negative_error_correctly)
     auto const expected
         = measured_error_t{.position = expected_quantized_positions[1], .magnitude = expected_max_error_magnitude};
 
-    auto const actual = sut(payload, left, right);
+    auto const actual = sut(approximant, left, right);
 
     EXPECT_EQ(actual, expected);
 }
@@ -154,7 +156,7 @@ TEST_F(equioscillation_error_metric_test_t, handles_negative_error_correctly)
 // sut_t subs literal {left, center, right} explicitly for sample locations at {-1, 0, +1}, respectively. This test
 // deliberately generates a segment that would normally truncate left or right and makes sure the literal positions are
 // still subbed.
-TEST_F(equioscillation_error_metric_test_t, evaluates_exact_boundaries_despite_truncation)
+TEST_F(residual_estimator_test_t, evaluates_exact_boundaries_despite_truncation)
 {
     real_t expected_left  = 0.0;
     real_t expected_right = 0.0;
@@ -186,29 +188,29 @@ TEST_F(equioscillation_error_metric_test_t, evaluates_exact_boundaries_despite_t
     // cache expected values using same calcs as sut
     auto const expected_center     = (expected_right + expected_left) * 0.5;
     auto const expected_radius     = (expected_right - expected_left) * 0.5;
-    auto const expected_position_1 = expected_center + expected_radius * sample_locator_t::sample_locations[1];
-    auto const expected_position_3 = expected_center + expected_radius * sample_locator_t::sample_locations[3];
+    auto const expected_position_1 = expected_center + expected_radius * node_generator_t::sample_locations[1];
+    auto const expected_position_3 = expected_center + expected_radius * node_generator_t::sample_locations[3];
 
     // verify evaluations
 
     EXPECT_CALL(mock_quantizer, call(expected_left)).WillOnce(Return(expected_left));
-    EXPECT_CALL(mock_ideal_function, call(expected_left)).WillOnce(Return(0.0));
-    EXPECT_CALL(mock_evaluator, call(payload, expected_left)).WillOnce(Return(0.0));
+    EXPECT_CALL(mock_target_function, call(expected_left)).WillOnce(Return(0.0));
+    EXPECT_CALL(mock_approximant_evaluator, call(approximant, expected_left)).WillOnce(Return(0.0));
     EXPECT_CALL(mock_quantizer, call(expected_position_1)).WillOnce(Return(expected_position_1));
-    EXPECT_CALL(mock_ideal_function, call(expected_position_1)).WillOnce(Return(0.0));
-    EXPECT_CALL(mock_evaluator, call(payload, expected_position_1)).WillOnce(Return(0.0));
+    EXPECT_CALL(mock_target_function, call(expected_position_1)).WillOnce(Return(0.0));
+    EXPECT_CALL(mock_approximant_evaluator, call(approximant, expected_position_1)).WillOnce(Return(0.0));
     EXPECT_CALL(mock_quantizer, call(expected_center)).WillOnce(Return(expected_center));
-    EXPECT_CALL(mock_ideal_function, call(expected_center)).WillOnce(Return(0.0));
-    EXPECT_CALL(mock_evaluator, call(payload, expected_center)).WillOnce(Return(0.0));
+    EXPECT_CALL(mock_target_function, call(expected_center)).WillOnce(Return(0.0));
+    EXPECT_CALL(mock_approximant_evaluator, call(approximant, expected_center)).WillOnce(Return(0.0));
     EXPECT_CALL(mock_quantizer, call(expected_position_3)).WillOnce(Return(expected_position_3));
-    EXPECT_CALL(mock_ideal_function, call(expected_position_3)).WillOnce(Return(0.0));
-    EXPECT_CALL(mock_evaluator, call(payload, expected_position_3)).WillOnce(Return(0.0));
+    EXPECT_CALL(mock_target_function, call(expected_position_3)).WillOnce(Return(0.0));
+    EXPECT_CALL(mock_approximant_evaluator, call(approximant, expected_position_3)).WillOnce(Return(0.0));
     EXPECT_CALL(mock_quantizer, call(expected_right)).WillOnce(Return(expected_right));
-    EXPECT_CALL(mock_ideal_function, call(expected_right)).WillOnce(Return(0.0));
-    EXPECT_CALL(mock_evaluator, call(payload, expected_right)).WillOnce(Return(0.0));
+    EXPECT_CALL(mock_target_function, call(expected_right)).WillOnce(Return(0.0));
+    EXPECT_CALL(mock_approximant_evaluator, call(approximant, expected_right)).WillOnce(Return(0.0));
 
-    sut(payload, expected_left, expected_right);
+    sut(approximant, expected_left, expected_right);
 }
 
 } // namespace
-} // namespace crv::equioscillation
+} // namespace crv
