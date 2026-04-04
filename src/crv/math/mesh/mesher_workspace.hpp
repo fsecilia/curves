92bba493b8fddf26d77c1640033c907263bcc160
fsecilia <frank.secilia@gmail.com>
2026-04-04 11:47:06 -0400

    feat(mesh): add mesher_workspace_t

---
+++ src/crv/math/mesh/mesher_workspace.hpp
@@ -0,0 +1,44 @@
+// SPDX-License-Identifier: MIT
+
+/// \file
+/// \brief mutable workspace for stateless mesher
+/// \copyright Copyright (C) 2026 Frank Secilia
+
+#pragma once
+
+#include <crv/lib.hpp>
+#include <crv/priority_queue.hpp>
+#include <vector>
+
+namespace crv {
+namespace generic {
+
+/// mutable state for adaptive mesh refinement
+///
+/// This type encapsulates mutable state and separates memory allocation from the meshing logic. This way, the
+/// allocations can be done once and the buffers maintained between mesh generations while keeping the logic stateless.
+template <typename result_t, typename queue_t> struct mesher_workspace_t
+{
+    result_t result;
+    queue_t  queue;
+
+    constexpr auto clear() noexcept -> void
+    {
+        result.clear();
+        queue.clear();
+    }
+
+    constexpr auto reserve(std::size_t max_segments) -> void
+    {
+        result.reserve(max_segments);
+        queue.reserve(max_segments);
+    }
+};
+
+} // namespace generic
+
+template <typename segment_t, typename queue_pred_t>
+using mesher_workspace_t
+    = generic::mesher_workspace_t<std::vector<segment_t>, priority_queue_t<std::vector<segment_t>, queue_pred_t>>;
+
+} // namespace crv
---
+++ src/crv/math/mesh/mesher_workspace_test.cpp
@@ -0,0 +1,57 @@
+// SPDX-License-Identifier: MIT
+
+/// \file
+/// \copyright Copyright (C) 2026 Frank Secilia
+
+#include "mesher_workspace.hpp"
+#include <crv/test/test.hpp>
+
+namespace crv::generic {
+namespace {
+
+struct mesher_workspace_test_t : Test
+{
+    struct container_t
+    {
+        bool clear_called   = false;
+        bool reserve_called = false;
+
+        std::size_t reserved_capacity = 0;
+
+        constexpr auto clear() noexcept -> void { clear_called = true; }
+
+        constexpr auto reserve(std::size_t capacity) -> void
+        {
+            reserve_called    = true;
+            reserved_capacity = capacity;
+        }
+    };
+
+    using sut_t = mesher_workspace_t<container_t, container_t>;
+};
+
+TEST_F(mesher_workspace_test_t, clear_forwards_to_both_members)
+{
+    auto sut = sut_t{};
+
+    sut.clear();
+
+    EXPECT_TRUE(sut.result.clear_called);
+    EXPECT_TRUE(sut.queue.clear_called);
+}
+
+TEST_F(mesher_workspace_test_t, reserve_forwards_to_both_members)
+{
+    auto sut = sut_t{};
+
+    sut.reserve(256);
+
+    EXPECT_TRUE(sut.result.reserve_called);
+    EXPECT_EQ(sut.result.reserved_capacity, 256);
+
+    EXPECT_TRUE(sut.queue.reserve_called);
+    EXPECT_EQ(sut.queue.reserved_capacity, 256);
+}
+
+} // namespace
+} // namespace crv::generic
