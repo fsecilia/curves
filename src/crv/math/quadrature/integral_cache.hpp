4ece1a23cdafb7917f6b30f6d05750c684aede9a
fsecilia <frank.secilia@gmail.com>
2026-04-07 16:36:18 -0400

    feat(quad): add integral_cache_builder_t

--- src/crv/math/quadrature/integral_cache.hpp
+++ src/crv/math/quadrature/integral_cache.hpp
@@ -17,6 +17,10 @@ namespace crv::quadrature {
 template <typename real_t> class integral_cache_t
 {
 public:
+    using map_t             = std::flat_map<real_t, real_t>;
+    using boundaries_t      = map_t::key_container_type;
+    using cumulative_sums_t = map_t::mapped_container_type;
+
     struct interval_t
     {
         real_t left_bound;
@@ -26,7 +30,7 @@ public:
         constexpr auto operator==(interval_t const&) const noexcept -> bool  = default;
     };
 
-    explicit integral_cache_t(std::flat_map<real_t, real_t> intervals) : intervals_{std::move(intervals)}
+    explicit integral_cache_t(map_t intervals) noexcept : intervals_{std::move(intervals)}
     {
         assert(!intervals_.empty() && "integral_cache_t: empty cache provided");
         assert(intervals_.begin()->first == real_t{0} && "integral_cache_t: origin must start at 0");
@@ -44,8 +48,47 @@ public:
         return {left->first, left->second};
     }
 
+    auto operator<=>(integral_cache_t const&) const noexcept -> auto = default;
+    auto operator==(integral_cache_t const&) const noexcept -> bool  = default;
+
 private:
-    std::flat_map<real_t, real_t> intervals_;
+    map_t intervals_;
+};
+
+template <typename real_t, typename integral_cache_t, typename accumulator_t> class integral_cache_builder_t
+{
+public:
+    using boundaries_t      = integral_cache_t::boundaries_t;
+    using cumulative_sums_t = integral_cache_t::cumulative_sums_t;
+
+    integral_cache_builder_t()
+    {
+        boundaries_.reserve(32);
+        boundaries_.push_back(real_t{0});
+
+        cumulative_sums_.reserve(32);
+        cumulative_sums_.push_back(real_t{0});
+    }
+
+    auto append(real_t right_bound, real_t sum) -> void
+    {
+        assert(right_bound > boundaries_.back() && "integral_cache_t: boundaries must be monotonically increasing");
+
+        running_sum_ += sum;
+
+        boundaries_.push_back(right_bound);
+        cumulative_sums_.push_back(static_cast<real_t>(running_sum_));
+    }
+
+    auto finalize() && noexcept -> integral_cache_t
+    {
+        return integral_cache_t{std::flat_map{std::sorted_unique, std::move(boundaries_), std::move(cumulative_sums_)}};
+    }
+
+private:
+    boundaries_t      boundaries_{};
+    cumulative_sums_t cumulative_sums_{};
+    accumulator_t     running_sum_{};
 };
 
 } // namespace crv::quadrature
--- src/crv/math/quadrature/integral_cache_test.cpp
+++ src/crv/math/quadrature/integral_cache_test.cpp
@@ -9,6 +9,10 @@
 namespace crv::quadrature {
 namespace {
 
+// ====================================================================================================================
+// integral_cache_t
+// ====================================================================================================================
+
 struct quadrature_integral_cache_test_t : Test
 {
     using real_t = float_t;
@@ -65,27 +69,23 @@ TEST_F(quadrature_integral_cache_test_small_cache_t, domain_max)
 // death tests
 // --------------------------------------------------------------------------------------------------------------------
 
-#ifndef NDEBUG
-
 // test queries below the valid domain bounds
 TEST_F(quadrature_integral_cache_test_small_cache_t, query_below_domain_aborts)
 {
-    EXPECT_DEATH(sut.interval(-0.01), "domain error");
+    EXPECT_DEBUG_DEATH(sut.interval(-0.01), "domain error");
 }
 
 // test queries strictly above the valid domain bounds
 TEST_F(quadrature_integral_cache_test_small_cache_t, query_above_domain_aborts)
 {
-    EXPECT_DEATH(sut.interval(3.01), "domain error");
+    EXPECT_DEBUG_DEATH(sut.interval(3.01), "domain error");
 }
 
 // test NaN injection
 TEST_F(quadrature_integral_cache_test_small_cache_t, query_nan_aborts)
 {
-    EXPECT_DEATH(sut.interval(std::numeric_limits<real_t>::quiet_NaN()), "domain error");
+    EXPECT_DEBUG_DEATH(sut.interval(std::numeric_limits<real_t>::quiet_NaN()), "domain error");
 }
-
-#endif
 
 // --------------------------------------------------------------------------------------------------------------------
 // minmal cache
@@ -115,6 +115,59 @@ TEST_F(quadrature_integral_cache_test_minimal_cache_t, interior)
 TEST_F(quadrature_integral_cache_test_minimal_cache_t, domain_max)
 {
     EXPECT_EQ((interval_t{1.5, 3.0}), sut.interval(1.5));
+}
+
+// ====================================================================================================================
+// integral_cache_builder_t
+// ====================================================================================================================
+
+struct integral_cache_builder_test_t : Test
+{
+    using real_t        = float_t;
+    using accumulator_t = real_t;
+
+    struct integral_cache_t
+    {
+        using map_t             = std::flat_map<real_t, real_t>;
+        using boundaries_t      = map_t::key_container_type;
+        using cumulative_sums_t = map_t::mapped_container_type;
+
+        map_t map;
+    };
+
+    using sut_t = integral_cache_builder_t<real_t, integral_cache_t, accumulator_t>;
+
+    sut_t sut{};
+};
+
+TEST_F(integral_cache_builder_test_t, append_none)
+{
+    EXPECT_EQ((typename integral_cache_t::map_t{{0, 0}}), std::move(sut).finalize().map);
+}
+
+TEST_F(integral_cache_builder_test_t, append_one)
+{
+    sut.append(3.0, 5.0);
+    EXPECT_EQ((typename integral_cache_t::map_t{{0, 0}, {3.0, 5.0}}), std::move(sut).finalize().map);
+}
+
+TEST_F(integral_cache_builder_test_t, append_many)
+{
+    sut.append(3.0, 5.0);
+    sut.append(5.5, 2.3);
+    sut.append(11.6, 1.1);
+    EXPECT_EQ((typename integral_cache_t::map_t{{0, 0}, {3.0, 5.0}, {5.5, 5.0 + 2.3}, {11.6, 5.0 + 2.3 + 1.1}}),
+              std::move(sut).finalize().map);
+}
+
+TEST_F(integral_cache_builder_test_t, nonmonotonic)
+{
+    EXPECT_DEBUG_DEATH(sut.append(0.0, -1.0), "monotonically increasing");
+}
+
+TEST_F(integral_cache_builder_test_t, monotonic_nonincreasing)
+{
+    EXPECT_DEBUG_DEATH(sut.append(0.0, 0.0), "monotonically increasing");
 }
 
 } // namespace
