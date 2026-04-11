cb012e8f497e6c6702eec1bedb37a4189a8ba5fa
fsecilia <frank.secilia@gmail.com>
2026-04-11 13:09:30 -0400

    feat(quad): add adaptive_integrator_t

---
+++ src/crv/math/quadrature/adaptive_integrator.hpp
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: MIT
+
+/// \file
+/// \brief
+/// \copyright Copyright (C) 2026 Frank Secilia
+
+#pragma once
+
+#include <crv/lib.hpp>
+#include <crv/math/compensated_accumulator.hpp>
+#include <crv/math/quadrature/antiderivative.hpp>
+#include <crv/math/quadrature/bisector.hpp>
+#include <crv/math/quadrature/segment.hpp>
+#include <crv/math/quadrature/stack.hpp>
+#include <crv/math/quadrature/subdivider.hpp>
+#include <crv/ranges.hpp>
+#include <cassert>
+#include <type_traits>
+#include <utility>
+#include <vector>
+
+namespace crv::quadrature {
+
+template <std::floating_point real_t, typename integral_t>
+using antiderivative_of_t = antiderivative_t<real_t, std::remove_cvref_t<integral_t>>;
+
+template <std::floating_point real_t, typename integral_t>
+using result_of_t = integration_result_t<real_t, antiderivative_of_t<real_t, integral_t>>;
+
+namespace generic {
+
+/// top-level adaptive quadrature entrypoint
+template <std::floating_point real_t, typename accumulator_t, typename subdivider_t, typename stack_seeder_t>
+class adaptive_integrator_t
+{
+public:
+    constexpr adaptive_integrator_t(real_t tolerance, int_t depth_limit, subdivider_t subdivider = {},
+                                    stack_seeder_t stack_seeder = {})
+        : subdivider_{std::move(subdivider)}, stack_seeder_{std::move(stack_seeder)}, tolerance_{tolerance},
+          depth_limit_{depth_limit}
+    {
+        stack_.reserve(32);
+    }
+
+    /// wide overload: full DI, every ephemeral injected
+    template <typename bisector_t, typename antiderivative_builder_t>
+    constexpr auto operator()(bisector_t bisector, antiderivative_builder_t antiderivative_builder, real_t domain_max,
+                              compatible_range<real_t> auto const& critical_points) ->
+        typename antiderivative_builder_t::result_t
+    {
+        // TODO: Automatically clearing the stack here prevents issues if a previous run threw an exception. However,
+        // this becomes a slippery slope when we support incremental evaluation or resuming and interrupted integration.
+        // When those features are added, this clear must be removed and replaced with explicit lifecycle management by
+        // the caller.
+        stack_.clear();
+
+        stack_seeder_.seed(stack_, bisector, domain_max, tolerance_, critical_points);
+        subdivider_.run(stack_, bisector, antiderivative_builder, depth_limit_);
+
+        return std::move(antiderivative_builder).finalize(std::move(bisector).finalize());
+    }
+
+    /// narrow overload: convenience, constructs ephemerals and delegates to wide
+    template <typename integral_t>
+    constexpr auto operator()(integral_t integral, real_t domain_max,
+                              compatible_range<real_t> auto const& critical_points) -> result_of_t<real_t, integral_t>
+    {
+        using antiderivative_t         = antiderivative_t<real_t, integral_t>;
+        using antiderivative_builder_t = antiderivative_builder_t<real_t, accumulator_t, antiderivative_t>;
+        return operator()(bisector_t{std::move(integral)}, antiderivative_builder_t{}, domain_max, critical_points);
+    }
+
+    /// narrow overload: convenience, delegates with empty critial points
+    template <typename integral_t>
+    constexpr auto operator()(integral_t integral, real_t domain_max) -> result_of_t<real_t, integral_t>
+    {
+        return operator()(std::move(integral), domain_max, std::array<real_t, 0>{});
+    }
+
+private:
+    using segment_t = segment_t<real_t>;
+    using stack_t   = std::vector<segment_t>;
+
+    subdivider_t   subdivider_;
+    stack_seeder_t stack_seeder_;
+    stack_t        stack_;
+    real_t         tolerance_;
+    int_t          depth_limit_;
+};
+
+} // namespace generic
+
+template <std::floating_point real_t>
+using adaptive_integrator_t
+    = generic::adaptive_integrator_t<real_t, compensated_accumulator_t<real_t>, subdivider_t<real_t>, stack_seeder_t>;
+
+} // namespace crv::quadrature
---
+++ src/crv/math/quadrature/adaptive_integrator_test.cpp
@@ -0,0 +1,148 @@
+// SPDX-License-Identifier: MIT
+
+/// \file
+/// \copyright Copyright (C) 2026 Frank Secilia
+
+#include "adaptive_integrator.hpp"
+#include <crv/math/quadrature/rules.hpp>
+#include <crv/test/test.hpp>
+#include <gmock/gmock.h>
+
+namespace crv::quadrature::generic {
+namespace {
+
+struct quadrature_adaptive_integrator_test : Test
+{
+    using real_t        = float_t;
+    using segment_t     = segment_t<real_t>;
+    using accumulator_t = real_t;
+    using rule_t        = rules::gauss_kronrod_t<real_t, 15>;
+
+    using stack_t           = std::vector<segment_t>;
+    using critical_points_t = std::vector<real_t>;
+
+    static constexpr auto integrand = [](real_t x) static noexcept -> real_t { return x * x; };
+    using integrand_t               = decltype(integrand);
+
+    struct integral_t
+    {
+        integrand_t integrand;
+        rule_t      rule;
+
+        auto operator==(integral_t const&) const noexcept -> bool = default;
+    };
+
+    struct mock_bisector_t
+    {
+        virtual ~mock_bisector_t() = default;
+
+        MOCK_METHOD(integral_t, finalize, (), (noexcept));
+    };
+    StrictMock<mock_bisector_t> mock_bisector;
+
+    struct bisector_t
+    {
+        mock_bisector_t* mock = nullptr;
+
+        auto finalize() && -> integral_t { return mock->finalize(); }
+    };
+
+    using antiderivative_t               = int_t;
+    static constexpr auto antiderivative = antiderivative_t{371};
+
+    struct mock_antiderivative_builder_t
+    {
+        virtual ~mock_antiderivative_builder_t() = default;
+
+        MOCK_METHOD((integration_result_t<real_t, antiderivative_t>), finalize, (integral_t), (noexcept));
+    };
+    StrictMock<mock_antiderivative_builder_t> mock_antiderivative_builder;
+
+    struct antiderivative_builder_t
+    {
+        using result_t = integration_result_t<real_t, antiderivative_t>;
+
+        mock_antiderivative_builder_t* mock = nullptr;
+
+        auto finalize(integral_t integral) && -> result_t { return mock->finalize(integral); }
+    };
+
+    struct mock_subdivider_t
+    {
+        virtual ~mock_subdivider_t() = default;
+
+        MOCK_METHOD(void, run,
+                    (stack_t & stack, mock_bisector_t const& bisector,
+                     mock_antiderivative_builder_t& antiderivative_builder, int_t depth_limit),
+                    (const));
+    };
+    StrictMock<mock_subdivider_t> mock_subdivider;
+
+    struct subdivider_t
+    {
+        mock_subdivider_t* mock = nullptr;
+
+        auto run(stack_t& stack, bisector_t const& bisector, antiderivative_builder_t& antiderivative_builder,
+                 int_t depth_limit) -> void
+        {
+            mock->run(stack, *bisector.mock, *antiderivative_builder.mock, depth_limit);
+        }
+    };
+
+    struct mock_stack_seeder_t
+    {
+        virtual ~mock_stack_seeder_t() = default;
+
+        MOCK_METHOD(void, seed,
+                    (stack_t & stack, mock_bisector_t const& bisector, real_t domain_max, real_t global_tolerance,
+                     critical_points_t const& critical_points),
+                    (const));
+    };
+    StrictMock<mock_stack_seeder_t> mock_stack_seeder;
+
+    struct stack_seeder_t
+    {
+        mock_stack_seeder_t* mock = nullptr;
+
+        auto seed(stack_t& stack, bisector_t const& bisector, real_t domain_max, real_t global_tolerance,
+                  critical_points_t const& critical_points) -> void
+        {
+            mock->seed(stack, *bisector.mock, domain_max, global_tolerance, critical_points);
+        }
+    };
+
+    static constexpr auto tolerance      = 1e-12;
+    static constexpr auto depth_limit    = 64;
+    static constexpr auto domain_max     = 256;
+    static constexpr auto integral       = integral_t{integrand, rule_t{}};
+    static constexpr auto achieved_error = 3.25e-10;
+    static constexpr auto max_error      = 6.98e-8;
+
+    using sut_t = adaptive_integrator_t<real_t, accumulator_t, subdivider_t, stack_seeder_t>;
+    sut_t sut{tolerance, depth_limit, subdivider_t{&mock_subdivider}, stack_seeder_t{&mock_stack_seeder}};
+};
+
+TEST_F(quadrature_adaptive_integrator_test, orchestrates_dependencies)
+{
+    using result_t = integration_result_t<real_t, antiderivative_t>;
+
+    auto const seq = InSequence{};
+
+    auto const critical_points = critical_points_t{0.25, 0.33, 1.0};
+    EXPECT_CALL(mock_stack_seeder, seed(_, Ref(mock_bisector), domain_max, tolerance, critical_points));
+    EXPECT_CALL(mock_subdivider, run(_, Ref(mock_bisector), Ref(mock_antiderivative_builder), depth_limit));
+    EXPECT_CALL(mock_bisector, finalize()).WillOnce(Return(integral));
+
+    auto const expected_result
+        = result_t{.antiderivative = antiderivative, .achieved_error = achieved_error, .max_error = max_error};
+    EXPECT_CALL(mock_antiderivative_builder, finalize(integral)).WillOnce(Return(expected_result));
+
+    [[maybe_unused]] result_t actual_result
+        = sut(bisector_t{&mock_bisector}, antiderivative_builder_t{&mock_antiderivative_builder}, domain_max,
+              critical_points);
+
+    EXPECT_EQ(expected_result, actual_result);
+}
+
+} // namespace
+} // namespace crv::quadrature::generic
