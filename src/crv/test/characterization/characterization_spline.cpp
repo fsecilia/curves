3868176c47458260b4831abcdccc249e7a85b634
fsecilia <frank.secilia@gmail.com>
2026-04-17 03:30:30 -0400

    feat(spline): shell characterization test

---
+++ src/crv/test/characterization/characterization_spline.cpp
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+/// \file
+/// \brief characterizes the minimum number of bits necessary for the spline input, t, to maintain an accuracy threshold
+/// \copyright Copyright (C) 2026 Frank Secilia
+
+#include <crv/lib.hpp>
+#include <cstdlib>
+
+namespace crv {
+namespace {
+
+auto main(int, char*[]) -> int
+{
+    return EXIT_SUCCESS;
+}
+
+} // namespace
+} // namespace crv
+
+auto main(int arg_count, char* args[]) -> int
+{
+    return crv::main(arg_count, args);
+}
