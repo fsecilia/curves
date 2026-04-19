d2d056c0d1f6c4ca604427ec1d82dbce39b6902a
fsecilia <frank.secilia@gmail.com>
2026-04-19 15:39:24 -0400

    feat(spline): hoist template to type

--- src/crv/math/spline/fixed/io.hpp
+++ src/crv/math/spline/fixed/io.hpp
@@ -12,9 +12,10 @@
 
 namespace crv::spline::fixed_point {
 
-template <integral value_t> auto operator<<(std::ostream& out, cubic_monomial_t<value_t> const& src) -> std::ostream&
+template <is_fixed out_t, is_fixed in_t, signed_integral coeff_t, typename shifter_t>
+auto operator<<(std::ostream& out, cubic_monomial_t<out_t, in_t, coeff_t, shifter_t> const& src) -> std::ostream&
 {
-    using src_t = cubic_monomial_t<value_t>;
+    using src_t = cubic_monomial_t<out_t, in_t, coeff_t, shifter_t>;
 
     out << "{.coeffs = {" << src.coeffs[0];
     for (auto coeff = 1; coeff < src_t::coeff_count; ++coeff) out << ", " << src.coeffs[coeff];
