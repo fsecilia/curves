e305da1e68207e689567bc68bbc5e1273ab27ff2
fsecilia <frank.secilia@gmail.com>
2026-04-20 03:52:14 -0400

    feat(spline): add cubic_segment_t

--- src/crv/math/spline/fixed/io_test.cpp
+++ src/crv/math/spline/fixed/io_test.cpp
@@ -30,5 +30,21 @@ TEST(spline_fixed_point_io, cubic_monomial)
     EXPECT_EQ(expected, actual);
 }
 
+TEST(spline_fixed_point_io, cubic_segment)
+{
+    using sut_t    = cubic_segment_t<cubic_monomial_t<out_t, in_t, coeff_t, shifter_t>, in_t, shifter_t>;
+    auto const sut = sut_t{.monomial{.coeffs = {2, 3, 5, 7}, .shifts = {11, 13, 17}, .final_frac_bits = 19},
+                           .rwidth           = 23,
+                           .rwidth_frac_bits = 29};
+
+    auto expected = std::ostringstream{};
+    expected << "{.monomial = " << sut.monomial << ", .rwidth = 23, .rwidth_frac_bits = 29}";
+
+    auto actual = std::ostringstream{};
+    actual << sut;
+
+    EXPECT_EQ(expected.str(), actual.str());
+}
+
 } // namespace
 } // namespace crv::spline::fixed_point
