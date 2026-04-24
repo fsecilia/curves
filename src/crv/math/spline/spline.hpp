467a9c0e596f7de1402c352ca0068159fc7fe298
fsecilia <frank.secilia@gmail.com>
2026-04-24 07:57:43 -0400

    feat(spline): add prefetch to spline

--- src/crv/math/spline/spline.hpp
+++ src/crv/math/spline/spline.hpp
@@ -10,6 +10,8 @@
 #include <crv/math/int_traits.hpp>
 #include <array>
 #include <cassert>
+#include <cstddef>
+#include <new>
 #include <utility>
 
 namespace crv::spline {
@@ -25,7 +27,8 @@ public:
     using segments_t = std::array<segment_t, max_segments>;
 
     /// \pre segment_count > 0
-    constexpr spline_t(segment_locator_t locator, segments_t segments, int_t segment_count, in_t x_max) noexcept
+    constexpr spline_t(
+        segment_locator_t const& locator, segments_t const& segments, int_t segment_count, in_t x_max) noexcept
         : x_max_{x_max}, segment_count_{segment_count}, locate_segment_{std::move(locator)},
           segments_{std::move(segments)}
     {
@@ -45,6 +48,9 @@ public:
             && "spline_t: located segment index out of bounds");
 
         assert(location.origin <= x && "spline_t: located segment origin underflows");
+
+        if !consteval { prev_segment_index_ = location.index; }
+
         return segments_[location.index](x - location.origin);
     }
 
@@ -69,17 +75,46 @@ public:
         return true;
     }
 
+    constexpr auto prefetch(auto const& prefetcher) const noexcept -> void
+    {
+        prefetch_segments(prefetcher);
+        locate_segment_.prefetch(prefetcher);
+    }
+
 private:
     constexpr auto extend_final_tangent(in_t x) const noexcept -> out_t
     {
         return segments_[segment_count_ - 1].extend_final_tangent(x - x_max_);
     }
 
+    /// prefetches the most recently selected segment and the two adjacent
+    ///
+    /// Prefetching these 3 segments serves as our hint to exploit the natural temporal locality of mouse velocity.
+    constexpr auto prefetch_segments(auto const& prefetcher) const noexcept -> void
+    {
+        // these casts are required to prevent ub when forming addresses outside of the array
+        auto const* base = static_cast<std::byte const*>(static_cast<void const*>(segments_.data()));
+
+        // prefetch most recent segment
+        //
+        // Because segments_ is aligned to a cache line, one of these two will contain the cache line containing the
+        // previous segment and one adjacent, and the other will contain the other adjacent. If prev_segment_index is 0
+        // or segment_count_ - 1, these both technically are out of bounds, either prefetching the end of the segment
+        // locator or whatever follows this type as a whole, but prefetching is built to be resiliant to this pattern.
+        // We do not have to guard it with an condition, saving a pair of misprediction sources.
+        prefetcher.prefetch(base + ((prev_segment_index_ - 1) * sizeof(segment_t)));
+        prefetcher.prefetch(base + ((prev_segment_index_ + 1) * sizeof(segment_t)));
+    }
+
     // these are ordered for overall cache-friendliness in operator ()
     in_t x_max_{};
     int_t segment_count_{};
     segment_locator_t locate_segment_{};
-    segments_t segments_{};
+
+    // *must* be aligned or the prefetching scheme is useless
+    alignas(std::hardware_constructive_interference_size) segments_t segments_{};
+
+    mutable int_t prev_segment_index_ = 0;
 };
 
 } // namespace crv::spline
