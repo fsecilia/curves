97538fdc17fcc6230351a80613ab98296049c6a9
fsecilia <frank.secilia@gmail.com>
2026-04-26 16:24:39 -0400

    test(spline): test against more general ctor
    This test originally tested against a factory function that hardcoded
    one of the ctor values. Now the main test tests against an instance
    generated directly from the more general ctor. A specific test case
    tests the factory function now.

--- src/crv/math/spline/construction/weight_function_test.cpp
+++ src/crv/math/spline/construction/weight_function_test.cpp
@@ -11,21 +11,6 @@ namespace {
 
 using real_t = float_t;
 
-// --------------------------------------------------------------------------------------------------------------------
-// uniform_t
-// --------------------------------------------------------------------------------------------------------------------
-
-constexpr auto uniform = uniform_t<real_t>{};
-
-static_assert(uniform(0.0) == 1.0);
-static_assert(uniform(0.5) == 1.0);
-static_assert(uniform(1.0) == 1.0);
-static_assert(uniform(10.0) == 1.0);
-
-// --------------------------------------------------------------------------------------------------------------------
-// exponential_decay_t
-// --------------------------------------------------------------------------------------------------------------------
-
 struct test_vector_t
 {
     real_t target_position;
@@ -37,16 +22,49 @@ struct test_vector_t
     }
 };
 
-struct weight_functions_exponential_decay_test_t : TestWithParam<test_vector_t>
+// ====================================================================================================================
+// uniform_t
+// ====================================================================================================================
+
+constexpr auto uniform = uniform_t<real_t>{};
+
+static_assert(uniform(0.0) == 1.0);
+static_assert(uniform(0.5) == 1.0);
+static_assert(uniform(1.0) == 1.0);
+static_assert(uniform(10.0) == 1.0);
+
+// ====================================================================================================================
+// exponential_decay_t
+// ====================================================================================================================
+
+struct weight_functions_exponential_decay_test_t : Test
+{
+    using sut_t = exponential_decay_t<real_t>;
+};
+
+TEST_F(weight_functions_exponential_decay_test_t, from_ratio_and_halflife)
+{
+    auto const actual = sut_t::from_ratio_and_halflife(12.3, 5.6);
+    EXPECT_DOUBLE_EQ(11.3, actual.amplitude);
+    EXPECT_DOUBLE_EQ(std::log(2.0) / 5.6, actual.decay_rate);
+    EXPECT_DOUBLE_EQ(1.0, actual.baseline_offset);
+}
+
+// --------------------------------------------------------------------------------------------------------------------
+// vector_test
+// --------------------------------------------------------------------------------------------------------------------
+
+struct weight_functions_exponential_decay_vector_test_t : weight_functions_exponential_decay_test_t,
+                                                          WithParamInterface<test_vector_t>
 {
     real_t target_position = GetParam().target_position;
     real_t expected = GetParam().expected;
 
     using sut_t = exponential_decay_t<real_t>;
-    sut_t sut{sut_t::from_ratio_and_halflife(12.3, 5.6)};
+    sut_t sut{.amplitude = 7.11, .decay_rate = std::log(2.0) / 13.17, .baseline_offset = 2.3};
 };
 
-TEST_P(weight_functions_exponential_decay_test_t, result)
+TEST_P(weight_functions_exponential_decay_vector_test_t, result)
 {
     auto const actual = sut(target_position);
 
@@ -55,14 +73,15 @@ TEST_P(weight_functions_exponential_decay_test_t, result)
 
 // clang-format off
 test_vector_t const vectors[] = {
-    {0.0, 12.300000000000000}, // (12.3 - 1.0)*exp(-(log(2)/5.6)*0.0) + 1.0
-    {1.0, 10.984425645447145}, // (12.3 - 1.0)*exp(-(log(2)/5.6)*1.0) + 1.0
-    {9.1,  4.663571741889102}, // (12.3 - 1.0)*exp(-(log(2)/5.6)*9.1) + 1.0
+    { 0.00, 9.410000000000000}, // 7.11*exp(-(log(2)/13.17)* 0.00) + 2.3
+    { 1.00, 9.045472090551783}, // 7.11*exp(-(log(2)/13.17)* 1.00) + 2.3
+    { 3.10, 8.339654878652086}, // 7.11*exp(-(log(2)/13.17)* 3.10) + 2.3
+    {41.43, 3.103329456213085}, // 7.11*exp(-(log(2)/13.17)*41.43) + 2.3
 
     // at exactly halflife, the initial amplitude should exactly halved
-    {5.6, 6.65}, // (12.3 - 1.0)*exp(-(log(2)/5.6)*5.6) + 1.0
+    {13.17, 5.855}, // 7.11*exp(-(log(2)/13.17)*13.17) + 2.3
 };
-INSTANTIATE_TEST_SUITE_P(vectors, weight_functions_exponential_decay_test_t, ValuesIn(vectors));
+INSTANTIATE_TEST_SUITE_P(vectors, weight_functions_exponential_decay_vector_test_t, ValuesIn(vectors));
 // clang-format on
 
 } // namespace
