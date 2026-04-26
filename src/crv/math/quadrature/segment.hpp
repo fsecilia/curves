fed8d34ef5b12e0f3abae153535dd9efb2643790
fsecilia <frank.secilia@gmail.com>
2026-04-26 19:23:16 -0400

    refactor(quad): rename bisect to refine

--- src/crv/math/quadrature/segment.hpp
+++ src/crv/math/quadrature/segment.hpp
@@ -25,7 +25,7 @@ template <std::floating_point t_real_t> struct segment_t
     auto operator==(segment_t const&) const noexcept -> bool = default;
 };
 
-template <std::floating_point t_real_t> struct bisection_t
+template <std::floating_point t_real_t> struct refinement_t
 {
     using real_t = t_real_t;
     using segment_t = segment_t<real_t>;
@@ -35,8 +35,8 @@ template <std::floating_point t_real_t> struct bisection_t
     real_t integral;
     real_t error_estimate;
 
-    auto operator<=>(bisection_t const&) const noexcept -> auto = default;
-    auto operator==(bisection_t const&) const noexcept -> bool = default;
+    auto operator<=>(refinement_t const&) const noexcept -> auto = default;
+    auto operator==(refinement_t const&) const noexcept -> bool = default;
 };
 
 } // namespace crv::quadrature
