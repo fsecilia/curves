41801a317f790f2afdf76bbe054a9607e7513c63
fsecilia <frank.secilia@gmail.com>
2026-04-27 12:35:03 -0400

    fix(math): fill gaps in abs polyfill

--- src/crv/math/abs_test.cpp
+++ src/crv/math/abs_test.cpp
@@ -4,15 +4,15 @@
 /// \copyright Copyright (C) 2026 Frank Secilia
 
 #include "abs.hpp"
+#include <crv/math/limits.hpp>
 #include <crv/test/test.hpp>
 #include <cmath>
-#include <limits>
 
 namespace crv {
 namespace {
 
 // ====================================================================================================================
-// Compile-Time Path
+// compile-time path
 // ====================================================================================================================
 
 // float
@@ -25,21 +25,68 @@ static_assert(abs(0.0) == 0.0);
 static_assert(abs(-100) == 100);
 static_assert(abs(100) == 100);
 
+// int boundaries
+static_assert(abs(max<int32_t>()) == max<int32_t>());
+static_assert(abs(-max<int32_t>()) == max<int32_t>());
+
+// 128-bit types
+static_assert(crv::abs(static_cast<int128_t>(-100)) == static_cast<int128_t>(100));
+
 // ====================================================================================================================
-// Runtime Path
+// runtime path
 // ====================================================================================================================
 
-struct abs_test_t : Test
+// --------------------------------------------------------------------------------------------------------------------
+// integers
+// --------------------------------------------------------------------------------------------------------------------
+
+struct int_test_vector_t
+{
+    int32_t input;
+    int32_t expected;
+
+    friend auto operator<<(std::ostream& out, int_test_vector_t const& src) -> std::ostream&
+    {
+        return out << "{.input = " << src.input << ", .expected = " << src.expected << "}";
+    }
+};
+
+struct abs_int_vector_test_t : Test, WithParamInterface<int_test_vector_t>
+{
+    int32_t input = GetParam().input;
+    int32_t expected = GetParam().expected;
+};
+
+TEST_P(abs_int_vector_test_t, result)
+{
+    using crv::abs;
+    EXPECT_EQ(expected, abs(input));
+}
+
+constexpr int_test_vector_t int_vectors[] = {
+    {-100, 100},
+    {100, 100},
+    {0, 0},
+    {-std::numeric_limits<int32_t>::max(), std::numeric_limits<int32_t>::max()},
+    {std::numeric_limits<int32_t>::max(), std::numeric_limits<int32_t>::max()},
+};
+INSTANTIATE_TEST_SUITE_P(int_vectors, abs_int_vector_test_t, ValuesIn(int_vectors));
+
+// --------------------------------------------------------------------------------------------------------------------
+// floating point
+// --------------------------------------------------------------------------------------------------------------------
+
+struct abs_float_test_t : Test
 {};
 
-TEST_F(abs_test_t, preserves_qnan)
+TEST_F(abs_float_test_t, preserves_qnan)
 {
     constexpr auto qnan = std::numeric_limits<float_t>::quiet_NaN();
     EXPECT_TRUE(std::isnan(abs(-qnan)));
     EXPECT_TRUE(std::isnan(abs(qnan)));
 }
 
-TEST_F(abs_test_t, preserves_snan)
+TEST_F(abs_float_test_t, preserves_snan)
 {
     constexpr auto snan = std::numeric_limits<float_t>::signaling_NaN();
     EXPECT_TRUE(std::isnan(abs(-snan)));
@@ -47,27 +94,27 @@ TEST_F(abs_test_t, preserves_snan)
 }
 
 // --------------------------------------------------------------------------------------------------------------------
-// Parameterized Test
+// vector test
 // --------------------------------------------------------------------------------------------------------------------
 
-struct test_vector_t
+struct float_test_vector_t
 {
     float_t input;
     float_t expected;
 
-    friend auto operator<<(std::ostream& out, test_vector_t const& src) -> std::ostream&
+    friend auto operator<<(std::ostream& out, float_test_vector_t const& src) -> std::ostream&
     {
         return out << "{.input = " << src.input << ", .expected = " << src.expected << "}";
     }
 };
 
-struct abs_vector_test_t : abs_test_t, WithParamInterface<test_vector_t>
+struct abs_float_vector_test_t : abs_float_test_t, WithParamInterface<float_test_vector_t>
 {
     float_t input = GetParam().input;
     float_t expected = GetParam().expected;
 };
 
-TEST_P(abs_vector_test_t, result)
+TEST_P(abs_float_vector_test_t, result)
 {
     using crv::abs;
 
@@ -82,7 +129,7 @@ constexpr auto min = std::numeric_limits<float_t>::min();
 constexpr auto max = std::numeric_limits<float_t>::max();
 
 // clang-format off
-constexpr test_vector_t vectors[] = {
+constexpr float_test_vector_t vectors[] = {
     {-42.5, 42.5},
     { 42.5, 42.5},
     { -0.0,  0.0},
@@ -96,7 +143,7 @@ constexpr test_vector_t vectors[] = {
     { -inf,  inf},
     {  inf,  inf},
 };
-INSTANTIATE_TEST_SUITE_P(vectors, abs_vector_test_t, ValuesIn(vectors));
+INSTANTIATE_TEST_SUITE_P(vectors, abs_float_vector_test_t, ValuesIn(vectors));
 // clang-format on
 
 } // namespace
