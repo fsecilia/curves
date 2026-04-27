17304ab25e89b20d9adcb2f8b16800674d34d699
fsecilia <frank.secilia@gmail.com>
2026-04-27 17:22:15 -0400

    feat(spline): rough in approximant

---
+++ src/crv/math/spline/construction/approximant.hpp
@@ -0,0 +1,24 @@
+// SPDX-License-Identifier: MIT
+
+/// \file
+/// \brief interval approximant
+/// \copyright Copyright (C) 2026 Frank Secilia
+
+#pragma once
+
+#include <crv/lib.hpp>
+#include <crv/math/jet/jet.hpp>
+#include <crv/math/spline/segment.hpp>
+
+namespace crv::spline {
+
+template <typename real_t, typename segment_t> struct approximant_t
+{
+    using jet_t = jet_t<real_t>;
+    jet_t left_knot;
+    jet_t right_knot;
+
+    segment_t monomial;
+};
+
+} // namespace crv::spline
