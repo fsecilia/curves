af62b597c62857c0a8a7af89d69f1d336d6f0600
fsecilia <frank.secilia@gmail.com>
2026-04-27 09:36:51 -0400

    refactor(spline): rename to interval

--- src/crv/math/spline/construction/interval.hpp
+++ src/crv/math/spline/construction/interval.hpp
@@ -13,7 +13,7 @@ namespace crv {
 /// represents single evaluated interval in an adaptive mesh
 ///
 /// Stores domain boundaries, approximant, and max error measured across the segment.
-template <typename real_t, typename approximant_t> struct mesh_segment_t
+template <typename real_t, typename approximant_t> struct interval_t
 {
     real_t left;
     real_t right;
@@ -22,12 +22,12 @@ template <typename real_t, typename approximant_t> struct mesh_segment_t
     int_t depth;
     approximant_t approximant;
 
-    constexpr auto operator<=>(mesh_segment_t const& src) const noexcept -> auto = default;
-    constexpr auto operator==(mesh_segment_t const& src) const noexcept -> bool = default;
+    constexpr auto operator<=>(interval_t const& src) const noexcept -> auto = default;
+    constexpr auto operator==(interval_t const& src) const noexcept -> bool = default;
 };
 
 /// ordered mesh segments by max_measured_error
-struct mesh_segment_pred_t
+struct interval_pred_t
 {
     constexpr auto operator()(auto const& lhs, auto const& rhs) const noexcept -> bool
     {
