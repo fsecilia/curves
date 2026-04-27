0199ffaaf438c7d2591f18f11d92a38333cfef5c
fsecilia <frank.secilia@gmail.com>
2026-04-27 17:22:15 -0400

    feat(spline): rough in approximant

---
+++ src/crv/math/spline/construction/approximant_test.cpp
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+/// \file
+/// \copyright Copyright (C) 2026 Frank Secilia
+
+#include "approximant.hpp"
+#include <crv/test/test.hpp>
+
+namespace crv::spline {
+namespace {
+
+//
+
+} // namespace
+} // namespace crv::spline
