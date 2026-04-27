eeb6718e30fc7d5eda167d88a637b5aae149f027
fsecilia <frank.secilia@gmail.com>
2026-04-27 09:36:51 -0400

    refactor(spline): rename to interval

--- src/crv/math/spline/construction/interval_test.cpp
+++ src/crv/math/spline/construction/interval_test.cpp
@@ -10,13 +10,13 @@
 namespace crv {
 namespace {
 
-using mesh_segment_t = mesh_segment_t<float_t, int_t>;
+using interval_t = interval_t<float_t, int_t>;
 constexpr auto lesser
-    = mesh_segment_t{.left = 0.0, .right = 0.0, .max_error = 1.0, .tolerance = 0.0, .depth = 0, .approximant = 0};
+    = interval_t{.left = 0.0, .right = 0.0, .max_error = 1.0, .tolerance = 0.0, .depth = 0, .approximant = 0};
 constexpr auto greater
-    = mesh_segment_t{.left = 0.0, .right = 0.0, .max_error = 2.0, .tolerance = 0.0, .depth = 0, .approximant = 0};
+    = interval_t{.left = 0.0, .right = 0.0, .max_error = 2.0, .tolerance = 0.0, .depth = 0, .approximant = 0};
 
-constexpr auto pred = mesh_segment_pred_t{};
+constexpr auto pred = interval_pred_t{};
 static_assert(!pred(lesser, lesser));
 static_assert(pred(lesser, greater));
 static_assert(!pred(greater, lesser));
