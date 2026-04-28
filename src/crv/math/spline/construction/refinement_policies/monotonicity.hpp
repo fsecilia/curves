6aacc2894372f2436329eeab68d11ab2471a61ad
fsecilia <frank.secilia@gmail.com>
2026-04-28 17:15:52 -0400

    fix(spline): do calcs wide

--- src/crv/math/spline/construction/refinement_policies/monotonicity.hpp
+++ src/crv/math/spline/construction/refinement_policies/monotonicity.hpp
@@ -7,6 +7,7 @@
 #pragma once
 
 #include <crv/lib.hpp>
+#include <crv/math/fixed/fixed.hpp>
 
 namespace crv::spline::refinement_policies {
 
@@ -20,17 +21,19 @@ struct monotonicity_t
         // Check both endpoints. If the parabola opens upward, check if the vertex, `v_t = -b/3a`, is in `(0, 1)`, and
         // `p'(v_t) < 0`
 
+        using wide_t = fixed::product_t<coeff_t, coeff_t>;
+
         // check the left endpoint: `p'(0) = C >= 0`
         if (c < coeff_t{0}) return false;
 
         // check the right endpoint: `p'(1) = 3A + 2B + C >= 0`
-        if (3 * a + 2 * b + c < coeff_t{0}) return false;
+        if (multiply(coeff_t{3}, a) + multiply(coeff_t{2}, b) + wide_t::convert(c) < wide_t{0}) return false;
 
         // if `a <= 0`, the parabola opens downward or is linear, and the minimum must already be at an endpoint
         if (a <= coeff_t{0}) return true;
 
         // the parabola opens upward, so check if `v_t` is in `(0, 1)`; since `3a > 0`, `0 < -b/3a < 1 == -3a < b < 0`
-        if (b <= -3 * a || coeff_t{0} <= b) return true;
+        if (wide_t::convert(b) <= multiply(coeff_t{-3}, a) || coeff_t{0} <= b) return true;
 
         // `v_t` is inside `(0, 1)`, so check if `p'(v_t) >= 0`; `c - b^2/3a >= 0 == 3ac >= b^2`
         auto const wide_3ac = 3 * multiply(a, c);
