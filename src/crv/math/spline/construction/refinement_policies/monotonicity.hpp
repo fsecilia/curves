e83f0b4cd8d31918a0c342e121d07ab4e49827de
fsecilia <frank.secilia@gmail.com>
2026-04-29 14:06:00 -0400

    refactor(spline): use relaxed comparisons

--- src/crv/math/spline/construction/refinement_policies/monotonicity.hpp
+++ src/crv/math/spline/construction/refinement_policies/monotonicity.hpp
@@ -24,16 +24,17 @@ struct monotonicity_t
         using wide_t = fixed::product_t<coeff_t, coeff_t>;
 
         // check the left endpoint: `p'(0) = C >= 0`
-        if (c < coeff_t{0}) return false;
+        if (c < 0) return false;
 
         // check the right endpoint: `p'(1) = 3A + 2B + C >= 0`
-        if (multiply(coeff_t{3}, a) + multiply(coeff_t{2}, b) + wide_t::convert(c) < wide_t{0}) return false;
+        auto const wide_3a = multiply(coeff_t{3}, a);
+        if (wide_3a + multiply(coeff_t{2}, b) + wide_t::convert(c) < 0) return false;
 
         // if `a <= 0`, the parabola opens downward or is linear, and the minimum must already be at an endpoint
-        if (a <= coeff_t{0}) return true;
+        if (a <= 0) return true;
 
         // the parabola opens upward, so check if `v_t` is in `(0, 1)`; since `3a > 0`, `0 < -b/3a < 1 == -3a < b < 0`
-        if (wide_t::convert(b) <= multiply(coeff_t{-3}, a) || coeff_t{0} <= b) return true;
+        if (wide_t::convert(b) <= -wide_3a || 0 <= b) return true;
 
         // `v_t` is inside `(0, 1)`, so check if `p'(v_t) >= 0`; `c - b^2/3a >= 0 == 3ac >= b^2`
         auto const wide_3ac = 3 * multiply(a, c);
