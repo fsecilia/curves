aec505a271c813d2ec907fc9e643d51d37281637
fsecilia <frank.secilia@gmail.com>
2026-04-30 07:11:58 -0400

    build(spline): rename to overflow

--- src/crv/math/spline/construction/refinement_policies/overflow_test.cpp
+++ src/crv/math/spline/construction/refinement_policies/overflow_test.cpp
@@ -3,7 +3,7 @@
 /// \file
 /// \copyright Copyright (C) 2026 Frank Secilia
 
-#include "saturation.hpp"
+#include "overflow.hpp"
 #include <crv/math/fixed/fixed.hpp>
 #include <crv/math/fixed/io.hpp>
 #include <crv/test/test.hpp>
