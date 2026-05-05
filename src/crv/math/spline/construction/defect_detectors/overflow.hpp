bd50bfff019f25d685a47981af6e9d6a49080be4
Frank Secilia <frank.secilia@gmail.com>
2026-05-05 12:10:42 -0400

    refactor(spline): extract endpoint test

--- src/crv/math/spline/construction/defect_detectors/overflow.hpp
+++ src/crv/math/spline/construction/defect_detectors/overflow.hpp
@@ -72,9 +72,7 @@ private:
     template <is_fixed coeff_t>
     constexpr auto primal_overflows(cubic_polynomial_t<coeff_t> const& coeffs) const noexcept -> bool
     {
-        // test endpoints; this covers the linear intermediate in all cases
-        if (operator()(coeffs, normalized_t{0})) return true;
-        if (operator()(coeffs, normalized_t{1})) return true; // this won't compile if the type has no integer bits
+        if (endpoints_overflow(coeffs)) return true;
 
         auto const a = from_fixed<real_t>(coeffs[0]);
         auto const b = from_fixed<real_t>(coeffs[1]);
@@ -148,6 +146,16 @@ private:
         auto const derivative = cubic_polynomial_t<coeff_t>{
             coeff_t::literal(3 * c0.value), coeff_t::literal(2 * c1.value), coeffs[2], coeff_t{0}};
         return primal_overflows(derivative);
+    }
+
+    // test endpoints; this covers the linear intermediate in all cases
+    template <std::ranges::range coeffs_t>
+    constexpr auto endpoints_overflow(coeffs_t const& coeffs) const noexcept -> bool
+        requires(is_fixed<std::ranges::range_value_t<coeffs_t>>)
+    {
+        if (operator()(coeffs, normalized_t{0})) return true;
+        if (operator()(coeffs, normalized_t{1})) return true; // this won't compile if the type has no integer bits
+        return false;
     }
 };
 
