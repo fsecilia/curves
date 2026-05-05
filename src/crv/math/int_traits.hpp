9a4bf921dc921ea9502d345ae07b3abe79df298e
Frank Secilia <frank.secilia@gmail.com>
2026-05-04 22:36:12 -0400

    refactor(math): move integer promotions
    Integer promotions are going to need int_cast, so they're really the
    same module.

--- src/crv/math/int_traits.hpp
+++ src/crv/math/int_traits.hpp
@@ -3,7 +3,7 @@
 /// \file
 /// \brief integer traits that include 128-bit types
 ///
-/// This module contains traits for integers. Much of it is dedicated to 128-bit types that are not included in the
+/// This module contains traits for integers. Most of it is dedicated to 128-bit types that are not included in the
 /// equivalent standard library traits.
 ///
 /// \copyright Copyright (C) 2026 Frank Secilia
@@ -12,9 +12,7 @@
 
 #include <crv/lib.hpp>
 #include <crv/traits.hpp>
-#include <bit>
 #include <cassert>
-#include <climits>
 #include <type_traits>
 
 namespace crv {
@@ -109,50 +107,5 @@ template <typename src_t> struct make_unsigned : detail::make_unsigned_f<src_t, 
 {};
 
 template <typename src_t> using make_unsigned_t = make_unsigned<src_t>::type;
-
-// --------------------------------------------------------------------------------------------------------------------
-// Sized Integers
-// --------------------------------------------------------------------------------------------------------------------
-
-namespace detail::integer {
-
-template <int_t byte_count, bool is_signed> struct int_by_bytes_t;
-
-// clang-format off
-template <> struct int_by_bytes_t<1, false> { using type = uint8_t; };
-template <> struct int_by_bytes_t<2, false> { using type = uint16_t; };
-template <> struct int_by_bytes_t<4, false> { using type = uint32_t; };
-template <> struct int_by_bytes_t<8, false> { using type = uint64_t; };
-template <> struct int_by_bytes_t<16, false> { using type = uint128_t; };
-
-template <> struct int_by_bytes_t<1, true> { using type = int8_t; };
-template <> struct int_by_bytes_t<2, true> { using type = int16_t; };
-template <> struct int_by_bytes_t<4, true> { using type = int32_t; };
-template <> struct int_by_bytes_t<8, true> { using type = int64_t; };
-template <> struct int_by_bytes_t<16, true> { using type = int128_t; };
-// clang-format on
-
-} // namespace detail::integer
-
-/// defines an integer with given size in bytes and signedness
-template <int byte_count, bool is_signed>
-    requires(byte_count > 0)
-using int_by_bytes_t = detail::integer::int_by_bytes_t<byte_count, is_signed>::type;
-
-/// defines an integer with given size in bits and signedness
-template <int bit_count, bool is_signed>
-    requires(bit_count > 0)
-using int_by_bits_t
-    = int_by_bytes_t<std::bit_ceil(static_cast<uint_t>(bit_count + CHAR_BIT - 1) / CHAR_BIT), is_signed>;
-
-// --------------------------------------------------------------------------------------------------------------------
-// Integer Promotions
-// --------------------------------------------------------------------------------------------------------------------
-
-/// doubles width of value_t, retaining signedness
-template <integral value_t> using widened_t = int_by_bytes_t<sizeof(value_t) * 2, is_signed_v<value_t>>;
-
-/// true when a type can be widened; true up to native word size
-template <integral value_t> inline constexpr auto can_widen = sizeof(value_t) <= sizeof(int_t);
 
 } // namespace crv
--- src/crv/math/integer.hpp
+++ src/crv/math/integer.hpp
@@ -11,12 +11,9 @@
 #include <crv/math/limits.hpp>
 #include <bit>
 #include <cassert>
+#include <climits>
 
 namespace crv {
-
-// ====================================================================================================================
-// Math
-// ====================================================================================================================
 
 // --------------------------------------------------------------------------------------------------------------------
 // log2
@@ -28,9 +25,9 @@ template <integral value_t> constexpr auto log2(value_t value) noexcept -> value
     return static_cast<value_t>(std::bit_width(value) - 1);
 }
 
-// ====================================================================================================================
-// Conversions
-// ====================================================================================================================
+// --------------------------------------------------------------------------------------------------------------------
+// conversions
+// --------------------------------------------------------------------------------------------------------------------
 
 /// extends std::in_range to support 128-bit types
 template <integral to_t, integral from_t> constexpr auto in_range(from_t from) noexcept -> bool
@@ -54,5 +51,50 @@ template <integral to_t, integral from_t> constexpr auto int_cast(from_t from) n
     assert(in_range<to_t>(from) && "int_cast: input out of range");
     return static_cast<to_t>(from);
 }
+
+// --------------------------------------------------------------------------------------------------------------------
+// types by size
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace detail::integer {
+
+template <int_t byte_count, bool is_signed> struct int_by_bytes_t;
+
+// clang-format off
+template <> struct int_by_bytes_t<1, false> { using type = uint8_t; };
+template <> struct int_by_bytes_t<2, false> { using type = uint16_t; };
+template <> struct int_by_bytes_t<4, false> { using type = uint32_t; };
+template <> struct int_by_bytes_t<8, false> { using type = uint64_t; };
+template <> struct int_by_bytes_t<16, false> { using type = uint128_t; };
+
+template <> struct int_by_bytes_t<1, true> { using type = int8_t; };
+template <> struct int_by_bytes_t<2, true> { using type = int16_t; };
+template <> struct int_by_bytes_t<4, true> { using type = int32_t; };
+template <> struct int_by_bytes_t<8, true> { using type = int64_t; };
+template <> struct int_by_bytes_t<16, true> { using type = int128_t; };
+// clang-format on
+
+} // namespace detail::integer
+
+/// defines an integer with given size in bytes and signedness
+template <int byte_count, bool is_signed>
+    requires(byte_count > 0)
+using int_by_bytes_t = detail::integer::int_by_bytes_t<byte_count, is_signed>::type;
+
+/// defines an integer with given size in bits and signedness
+template <int bit_count, bool is_signed>
+    requires(bit_count > 0)
+using int_by_bits_t
+    = int_by_bytes_t<std::bit_ceil(static_cast<uint_t>(bit_count + CHAR_BIT - 1) / CHAR_BIT), is_signed>;
+
+// --------------------------------------------------------------------------------------------------------------------
+// promotions
+// --------------------------------------------------------------------------------------------------------------------
+
+/// doubles width of value_t, retaining signedness
+template <integral value_t> using widened_t = int_by_bytes_t<sizeof(value_t) * 2, is_signed_v<value_t>>;
+
+/// true when a type can be widened; true up to native word size
+template <integral value_t> inline constexpr auto can_widen = sizeof(value_t) <= sizeof(int_t);
 
 } // namespace crv
