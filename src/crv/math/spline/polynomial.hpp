29ec1478b100c083796ff5f2a58c3f9e61f86388
Frank Secilia <frank.secilia@gmail.com>
2026-05-05 08:32:44 -0400

    refactor(spline): make mac return wide
    This is necessary to support using the same mac in both the polynomial
    evaluator and overflow detector.

--- src/crv/math/spline/polynomial.hpp
+++ src/crv/math/spline/polynomial.hpp
@@ -17,23 +17,27 @@ template <is_fixed coeff_t> using cubic_polynomial_t = std::array<coeff_t, cubic
 
 /// single, fixed-point multiply and carry with normalized t
 ///
-/// This is a standard fixed-point Horner's loop step. It has no mitigation for overflow; it presumes the polynomial's
-/// intermediate steps were already checked at the extrema.
-///
-/// \pre sum known to not overflow
-struct fast_mac_t
+/// This is the standard fixed-point Horner's loop step.
+struct mac_t
 {
     template <is_fixed coeff_t, is_fixed normalized_t>
-    constexpr auto operator()(coeff_t accumulator, normalized_t t, coeff_t coeff) const noexcept -> coeff_t
+    constexpr auto operator()(coeff_t accumulator, normalized_t t, coeff_t coeff) const noexcept
+        -> widened_t<typename coeff_t::value_t>
     {
-        static constexpr auto rounding_bias = typename normalized_t::value_t{1} << (normalized_t::frac_bits - 1);
-        auto const wide_product = widen(accumulator.value) * t.value + widen(rounding_bias);
-        auto const narrow_product = narrow(wide_product >> normalized_t::frac_bits);
-        return coeff_t::literal(narrow_product + coeff.value);
+        using wide_t = widened_t<typename coeff_t::value_t>;
+        static constexpr auto rounding_bias = wide_t{1} << (normalized_t::frac_bits - 1);
+        auto const product
+            = (accumulator.value * static_cast<wide_t>(t.value) + rounding_bias) >> normalized_t::frac_bits;
+        return product + coeff.value;
     }
 };
 
 /// fixed-length horner's loop using fold
+///
+/// This has no mitigation for overflow; it presumes the polynomial's intermediate steps were already checked at the
+/// extrema.
+///
+/// \pre sum known to not overflow
 template <auto mac> struct polynomial_evaluator_t
 {
     template <typename coeff_t>
@@ -41,7 +45,7 @@ template <auto mac> struct polynomial_evaluator_t
         auto t, coeff_t highest_coeff, std::same_as<coeff_t> auto... remaining_coeffs) const noexcept -> coeff_t
     {
         auto accumulator = highest_coeff;
-        ((accumulator = mac(accumulator, t, remaining_coeffs)), ...);
+        ((accumulator = coeff_t::literal(narrow(mac(accumulator, t, remaining_coeffs)))), ...);
         return accumulator;
     }
 };
--- src/crv/math/spline/polynomial_test.cpp
+++ src/crv/math/spline/polynomial_test.cpp
@@ -12,10 +12,12 @@ namespace crv::spline {
 namespace {
 
 // --------------------------------------------------------------------------------------------------------------------
-// fast_mac_t
+// mac_t
 // --------------------------------------------------------------------------------------------------------------------
 
-template <typename coeff_t, typename normalized_t> struct fast_mac_test_t
+namespace mac_test {
+
+template <typename coeff_t, typename normalized_t> struct mac_test_t
 {
     using coeff_value_t = coeff_t::value_t;
     static constexpr auto coeff_frac_bits = coeff_t::frac_bits;
@@ -23,7 +25,7 @@ template <typename coeff_t, typename normalized_t> struct fast_mac_test_t
     using normalized_value_t = normalized_t::value_t;
     static constexpr auto normalized_frac_bits = normalized_t::frac_bits;
 
-    using sut_t = fast_mac_t;
+    using sut_t = mac_t;
     static constexpr auto sut = sut_t{};
 
     static constexpr auto c0 = coeff_t{0};
@@ -34,7 +36,7 @@ template <typename coeff_t, typename normalized_t> struct fast_mac_test_t
     static constexpr auto c1_4 = c1_2 >> 1; // 0.25
     static constexpr auto cm1_4 = -c1_4; // -0.25
     static constexpr auto c1_8 = c1_4 >> 1; // 0.125
-    static constexpr auto cm3_4 = cm1_2.value + cm1_4.value; // -0.75
+    static constexpr auto cm3_4 = coeff_t::literal(cm1_2.value + cm1_4.value); // -0.75
     static constexpr auto c5 = coeff_t{5};
     static constexpr auto c_epsilon = coeff_t::literal(1);
 
@@ -43,78 +45,103 @@ template <typename coeff_t, typename normalized_t> struct fast_mac_test_t
     static constexpr auto t_epsilon = normalized_t::literal(1);
 
     // zeros propagate correctly without shifting errors
-    static_assert(sut(c0, t0, c0).value == 0);
-    static_assert(sut(c1, t0, c0).value == 0);
-    static_assert(sut(c0, t1_2, c0).value == 0);
+    static_assert(sut(c0, t0, c0) == 0);
+    static_assert(sut(c1, t0, c0) == 0);
+    static_assert(sut(c0, t1_2, c0) == 0);
 
     // addition of coefficient is exact when multiplication yields zero
-    static_assert(sut(c0, t0, c5).value == c5.value);
+    static_assert(sut(c0, t0, c5) == c5.value);
+
+    auto test() const noexcept -> void { EXPECT_EQ(sut(-c1_4 - c_epsilon, t1_2 - t_epsilon, c0), -c1_8.value); }
 
     // negative rounding boundaries
-    static_assert(sut(-c1_4 - c_epsilon, t1_2 - t_epsilon, c0).value == -c1_8.value);
-    static_assert(sut(-c1_4 - c_epsilon, t1_2, c0).value == -c1_8.value);
-    static_assert(sut(-c1_4 - c_epsilon, t1_2 + t_epsilon, c0).value == -c1_8.value - 1);
-    static_assert(sut(-c1_4, t1_2 - t_epsilon, c0).value == -c1_8.value);
-    static_assert(sut(-c1_4, t1_2, c0).value == -c1_8.value);
-    static_assert(sut(-c1_4, t1_2 + t_epsilon, c0).value == -c1_8.value);
-    static_assert(sut(-c1_4 + c_epsilon, t1_2 - t_epsilon, c0).value == -c1_8.value + 1);
-    static_assert(sut(-c1_4 + c_epsilon, t1_2, c0).value == -c1_8.value + 1);
-    static_assert(sut(-c1_4 + c_epsilon, t1_2 + t_epsilon, c0).value == -c1_8.value);
+    static_assert(sut(-c1_4 - c_epsilon, t1_2 - t_epsilon, c0) == -c1_8.value);
+    static_assert(sut(-c1_4 - c_epsilon, t1_2, c0) == -c1_8.value);
+    static_assert(sut(-c1_4 - c_epsilon, t1_2 + t_epsilon, c0) == -c1_8.value - 1);
+    static_assert(sut(-c1_4, t1_2 - t_epsilon, c0) == -c1_8.value);
+    static_assert(sut(-c1_4, t1_2, c0) == -c1_8.value);
+    static_assert(sut(-c1_4, t1_2 + t_epsilon, c0) == -c1_8.value);
+    static_assert(sut(-c1_4 + c_epsilon, t1_2 - t_epsilon, c0) == -c1_8.value + 1);
+    static_assert(sut(-c1_4 + c_epsilon, t1_2, c0) == -c1_8.value + 1);
+    static_assert(sut(-c1_4 + c_epsilon, t1_2 + t_epsilon, c0) == -c1_8.value);
 
     // positive rounding boundaries
-    static_assert(sut(c1_4 - c_epsilon, t1_2 - t_epsilon, c0).value == c1_8.value - 1);
-    static_assert(sut(c1_4 - c_epsilon, t1_2, c0).value == c1_8.value);
-    static_assert(sut(c1_4 - c_epsilon, t1_2 + t_epsilon, c0).value == c1_8.value);
-    static_assert(sut(c1_4, t1_2 - t_epsilon, c0).value == c1_8.value);
-    static_assert(sut(c1_4, t1_2, c0).value == c1_8.value);
-    static_assert(sut(c1_4, t1_2 + t_epsilon, c0).value == c1_8.value);
-    static_assert(sut(c1_4 + c_epsilon, t1_2 - t_epsilon, c0).value == c1_8.value);
-    static_assert(sut(c1_4 + c_epsilon, t1_2, c0).value == c1_8.value + 1);
-    static_assert(sut(c1_4 + c_epsilon, t1_2 + t_epsilon, c0).value == c1_8.value + 1);
+    static_assert(sut(c1_4 - c_epsilon, t1_2 - t_epsilon, c0) == c1_8.value - 1);
+    static_assert(sut(c1_4 - c_epsilon, t1_2, c0) == c1_8.value);
+    static_assert(sut(c1_4 - c_epsilon, t1_2 + t_epsilon, c0) == c1_8.value);
+    static_assert(sut(c1_4, t1_2 - t_epsilon, c0) == c1_8.value);
+    static_assert(sut(c1_4, t1_2, c0) == c1_8.value);
+    static_assert(sut(c1_4, t1_2 + t_epsilon, c0) == c1_8.value);
+    static_assert(sut(c1_4 + c_epsilon, t1_2 - t_epsilon, c0) == c1_8.value);
+    static_assert(sut(c1_4 + c_epsilon, t1_2, c0) == c1_8.value + 1);
+    static_assert(sut(c1_4 + c_epsilon, t1_2 + t_epsilon, c0) == c1_8.value + 1);
 
     // sign
-    static_assert(sut(cm1, t1_2, c0).value == cm1_2.value);
-    static_assert(sut(cm1, t1_2, cm1_4).value == cm3_4);
+    static_assert(sut(cm1, t1_2, c0) == cm1_2.value);
+    static_assert(sut(cm1, t1_2, cm1_4) == cm3_4.value);
 
     // limits
-    static_assert(sut(min<coeff_t>(), t0, min<coeff_t>()).value == min<coeff_t>().value);
-    static_assert(sut(max<coeff_t>(), t0, min<coeff_t>()).value == min<coeff_t>().value);
-    static_assert(sut(min<coeff_t>(), t0, c0).value == 0);
-    static_assert(sut(max<coeff_t>(), t0, c0).value == 0);
-    static_assert(sut(min<coeff_t>(), t0, max<coeff_t>()).value == max<coeff_t>().value);
-    static_assert(sut(max<coeff_t>(), t0, max<coeff_t>()).value == max<coeff_t>().value);
+    static_assert(sut(min<coeff_t>(), t0, min<coeff_t>()) == min<coeff_t>().value);
+    static_assert(sut(max<coeff_t>(), t0, min<coeff_t>()) == min<coeff_t>().value);
+    static_assert(sut(min<coeff_t>(), t0, c0) == 0);
+    static_assert(sut(max<coeff_t>(), t0, c0) == 0);
+    static_assert(sut(min<coeff_t>(), t0, max<coeff_t>()) == max<coeff_t>().value);
+    static_assert(sut(max<coeff_t>(), t0, max<coeff_t>()) == max<coeff_t>().value);
 
     // to test against t_max through rounding we need headroom; |acc| must be less than 2^(normalized_frac_bits - 1)
     static constexpr auto bound_value = coeff_value_t{1} << (normalized_frac_bits - 2);
     static constexpr auto max_bound_c = coeff_t::literal(bound_value);
     static constexpr auto min_bound_c = coeff_t::literal(-bound_value);
-    static_assert(sut(min_bound_c, max<normalized_t>(), c0).value == min_bound_c.value);
-    static_assert(sut(max_bound_c, max<normalized_t>(), c0).value == max_bound_c.value);
+    static_assert(sut(min_bound_c, max<normalized_t>(), c0) == min_bound_c.value);
+    static_assert(sut(max_bound_c, max<normalized_t>(), c0) == max_bound_c.value);
 };
 
-template struct fast_mac_test_t<fixed_t<int64_t, 48>, fixed_t<uint64_t, 64>>;
-template struct fast_mac_test_t<fixed_t<int64_t, 12>, fixed_t<uint32_t, 32>>;
-template struct fast_mac_test_t<fixed_t<int32_t, 11>, fixed_t<uint16_t, 16>>;
+template struct mac_test_t<fixed_t<int64_t, 48>, fixed_t<uint64_t, 64>>;
+template struct mac_test_t<fixed_t<int64_t, 12>, fixed_t<uint32_t, 32>>;
+template struct mac_test_t<fixed_t<int32_t, 11>, fixed_t<uint16_t, 16>>;
+
+} // namespace mac_test
 
 // --------------------------------------------------------------------------------------------------------------------
 // polynomial_evaluator_t
 // --------------------------------------------------------------------------------------------------------------------
 
-// A mock MAC that builds a math equation string
-constexpr auto string_mac = [](std::string acc, std::string_view t, std::string_view coeff) {
-    return "(" + acc + "*" + std::string(t) + " + " + std::string(coeff) + ")";
+namespace polynomial_evaluator_test {
+
+struct wide_t
+{
+    std::string expression;
 };
 
-// Requires C++20 for constexpr std::string
+constexpr auto narrow(wide_t w) -> wide_t
+{
+    return w;
+}
+
+struct coeff_t
+{
+    std::string expression;
+
+    static constexpr coeff_t literal(wide_t w) { return coeff_t{std::move(w.expression)}; }
+
+    constexpr bool operator==(std::string_view other) const { return expression == other; }
+};
+
+constexpr auto string_mac = [](coeff_t accumulator, std::string_view t, coeff_t coeff) {
+    return wide_t{"(" + accumulator.expression + "*" + std::string(t) + " + " + coeff.expression + ")"};
+};
+
 constexpr auto test_structural_evaluation()
 {
     using namespace std::literals;
+
     auto evaluator = polynomial_evaluator_t<string_mac>{};
 
-    return evaluator("t"sv, "11"s, "17"s, "23"s, "29"s);
+    return evaluator("t"sv, coeff_t{"11"}, coeff_t{"17"}, coeff_t{"23"}, coeff_t{"29"});
 }
-
 static_assert(test_structural_evaluation() == "(((11*t + 17)*t + 23)*t + 29)");
+
+} // namespace polynomial_evaluator_test
 
 } // namespace
 } // namespace crv::spline
