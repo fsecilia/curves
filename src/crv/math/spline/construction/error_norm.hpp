28fd4f8429d9e584b68397e7f04a7e5969a4daff
Frank Secilia <frank.secilia@gmail.com>
2026-05-06 13:23:50 -0400

    feat(spline): add logsumexp_floor_t

--- src/crv/math/spline/construction/error_norm.hpp
+++ src/crv/math/spline/construction/error_norm.hpp
@@ -10,8 +10,44 @@
 #include <crv/algorithm.hpp>
 #include <crv/math/abs.hpp>
 #include <crv/math/scalar_traits.hpp>
+#include <cassert>
+#include <cmath>
 
 namespace crv::spline::error_norms {
+
+/// soft floor using lse
+template <typename real_t> struct logsumexp_floor_t
+{
+    static constexpr auto sharpness = real_t{10};
+
+    static constexpr auto operator()(real_t x, real_t y) noexcept -> real_t
+    {
+        using std::exp;
+        using std::isfinite;
+        using std::log;
+
+        // cache division result
+        static constexpr auto rsharpness = 1.0 / sharpness;
+
+        // stability transform
+        //
+        // The standard function is:
+        //
+        //     lse_floor(x, y) = log(exp(kx) + exp(ky))/k
+        //
+        // However, this may overflow because exponentials grow quickly. Instead, guarantee the exponent is always 0 or
+        // negative:
+        //
+        //     m = max(x, y)
+        //     lse_floor_stable(x, y) = m + log(exp(k(m - x) + exp(k(m - y)))/y
+        //
+        auto const m = max(x, y);
+        auto const result = m + log(exp(sharpness * (x - m)) + exp(sharpness * (y - m))) * rsharpness;
+        assert(isfinite(result));
+
+        return result;
+    }
+};
 
 /// standard L-infinity uniform norm for scalars evaluating position error
 template <typename scalar_t> struct uniform_t
