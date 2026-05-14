703eab56e651009186fff390da95a9b28998d2c5
Frank Secilia <frank.secilia@gmail.com>
2026-05-14 16:51:54 -0400

    refactor(spline): extract, test polynomial_t

--- src/crv/math/spline/polynomial.hpp
+++ src/crv/math/spline/polynomial.hpp
@@ -7,52 +7,32 @@
 #pragma once
 
 #include <crv/lib.hpp>
-#include <crv/math/fixed/fixed.hpp>
 #include <array>
-#include <ranges>
 
 namespace crv::spline {
 
-template <is_fixed coeff_t> using linear_polynomial_t = std::array<coeff_t, 2>;
-template <is_fixed coeff_t> using quadratic_polynomial_t = std::array<coeff_t, 3>;
-template <is_fixed coeff_t> using cubic_polynomial_t = std::array<coeff_t, 4>;
-
-/// single, fixed-point multiply and carry with normalized t
-///
-/// This is the standard fixed-point Horner's loop step.
-struct mac_t
+template <typename t_real_t, int_t t_degree = 3> struct polynomial_t : std::array<t_real_t, t_degree + 1>
 {
-    template <is_fixed coeff_t, is_fixed normalized_t>
-    constexpr auto operator()(coeff_t accumulator, normalized_t t, coeff_t coeff) const noexcept
-        -> widened_t<typename coeff_t::value_t>
-    {
-        using wide_t = widened_t<typename coeff_t::value_t>;
-        static constexpr auto rounding_bias = wide_t{1} << (normalized_t::frac_bits - 1);
-        auto const product
-            = (accumulator.value * static_cast<wide_t>(t.value) + rounding_bias) >> normalized_t::frac_bits;
-        return product + coeff.value;
-    }
-};
+    using real_t = t_real_t;
 
-/// fixed-length horner's loop using fold
-///
-/// This has no mitigation for overflow; it presumes the polynomial's intermediate steps were already checked at the
-/// extrema.
-///
-/// \pre sum known to not overflow
-template <auto mac> struct polynomial_evaluator_t
-{
-    template <std::ranges::range coeffs_t>
-    constexpr auto operator()(auto t, coeffs_t const& coeffs) const noexcept -> std::ranges::range_value_t<coeffs_t>
-    {
-        using coeff_t = std::ranges::range_value_t<coeffs_t>;
+    static constexpr auto degree = t_degree;
+    static constexpr auto term_count = degree + 1;
 
-        auto coeff = std::begin(coeffs);
-        auto const end = std::end(coeffs);
-        auto accumulator = *coeff++;
-        while (coeff != end) accumulator = coeff_t::literal(narrow(mac(accumulator, t, *coeff++)));
+    template <typename value_t> constexpr auto operator()(value_t t) const noexcept -> value_t
+    {
+        auto const coeffs = this->data();
+        auto const coeff_count = this->size();
+        auto accumulator = static_cast<value_t>(coeffs[0]);
+        for (auto coeff_index = 1u; coeff_index < coeff_count; ++coeff_index)
+        {
+            accumulator = accumulator * t + coeffs[coeff_index];
+        }
         return accumulator;
     }
 };
+
+template <typename real_t, typename... remaining_t>
+polynomial_t(real_t, remaining_t...)
+    -> polynomial_t<std::enable_if_t<(std::same_as<real_t, remaining_t> && ...), real_t>, sizeof...(remaining_t)>;
 
 } // namespace crv::spline
