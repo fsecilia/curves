51a012441db9ff728539a41f64273999aaff17e5
Frank Secilia <frank.secilia@gmail.com>
2026-05-14 18:57:06 -0400

    refactor(spline): integrate new approximant

--- src/crv/math/spline/construction/approximant.hpp
+++ src/crv/math/spline/construction/approximant.hpp
@@ -10,27 +10,28 @@
 #include <crv/math/fixed/fixed.hpp>
 #include <crv/math/fixed/float_conversions.hpp>
 #include <crv/math/jet/jet.hpp>
+#include <crv/math/spline/polynomial.hpp>
 
 namespace crv::spline {
 
-/// adapts segment with float api, anchors to origin in fixed x, supplies derivative
-template <std::floating_point t_scalar_t, typename segment_t, typename segment_derivative_t> struct approximant_t
+template <std::floating_point t_scalar_t, is_fixed t_x_t> struct approximant_t
 {
     using scalar_t = t_scalar_t;
-    using x_t = segment_t::x_t;
+    using x_t = t_x_t;
 
+    using jet_t = jet_t<scalar_t>;
+
+    polynomial_t<scalar_t> polynomial;
     x_t x0;
-    segment_t segment;
+    int_t log2_width;
 
-    [[no_unique_address]] segment_derivative_t segment_derivative{};
-
-    /// \returns jet in spline-global spatial coordinates, {y, dy/dx}
-    constexpr auto operator()(scalar_t x) const noexcept -> jet_t<scalar_t>
+    /// \returns jet in spline-global spatial coordinates, {y, dy/dx}, quantized to the x_t grid
+    constexpr auto operator()(scalar_t x) const noexcept -> jet_t
     {
-        auto const t = segment.x_to_t(to_fixed<x_t>(x) - x0);
-        auto const primal = from_fixed<scalar_t>(segment.evaluate(t));
-        auto const tangent = segment_derivative.dy_dx(segment.coeffs(), from_fixed<scalar_t>(t), segment.log2_width());
-        return jet_t{primal, tangent};
+        auto const x_local = from_fixed<scalar_t>(to_fixed<x_t>(x) - x0);
+        auto const t = std::ldexp(x_local, int_cast<int>(-log2_width));
+        auto const dt_dx = std::ldexp(scalar_t{1}, int_cast<int>(-log2_width));
+        return polynomial(jet_t{t, dt_dx});
     }
 };
 
