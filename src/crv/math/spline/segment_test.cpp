b62452f69c102b61a45686a7b89b1be7427fe2e1
Frank Secilia <frank.secilia@gmail.com>
2026-05-15 10:32:47 -0400

    test(spline): test layouts

--- src/crv/math/spline/segment_test.cpp
+++ src/crv/math/spline/segment_test.cpp
@@ -9,7 +9,12 @@
 namespace crv::spline {
 namespace {
 
-//
+// --------------------------------------------------------------------------------------------------------------------
+// layouts
+// --------------------------------------------------------------------------------------------------------------------
+
+static_assert(intermediate_layout.shift_mask() == 0x3F); // 6 bits
+static_assert(final_layout.shift_mask() == 0x7F); // 7 bits
 
 } // namespace
 } // namespace crv::spline
