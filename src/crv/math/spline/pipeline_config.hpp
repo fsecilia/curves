60f708b7632fdb231eee8a73e8e94d82aea4b94e
Frank Secilia <frank.secilia@gmail.com>
2026-05-15 15:40:09 -0400

    refactor(spline): use x_t, y_t from pipeline

--- src/crv/math/spline/pipeline_config.hpp
+++ src/crv/math/spline/pipeline_config.hpp
@@ -20,10 +20,22 @@ template <typename t_x_t, typename t_y_t, segment_layout_t t_segment_layout> str
     segment_layout_t segment_layout = t_segment_layout;
 };
 
-constexpr auto prod_pipeline_config = pipeline_config_t<fixed_t<int64_t, 42>, fixed_t<int64_t, 48>,
+// x_t = fixed_t<int64_t, 42>
+//
+// A 32khz mouse fully saturating input at max rate produces a velocity of sqrt(2*(2^15 - 1)^2)*32 ~= 20.5 bits, so
+// we need 21 integer bits, which gives Q21.42.
+
+// y_t = fixed_t<int64_t, 45>
+//
+// For a domain of [0, 2^8) and soft limiter on the integrand at y=1000, the largest integral possible is a pinned
+// straight line, integrating to 256000. The integer limit of Q18.45 is 262144, giving about 0.6% headroom.
+
+constexpr auto prod_pipeline_config = pipeline_config_t<fixed_t<int64_t, 42>, fixed_t<int64_t, 45>,
     segment_layout_t{
         .intermediate = {.shift_width = 6, .is_signed = false},
         .final = {.shift_width = 7, .is_signed = true},
     }>{};
+
+using prod_pipeline_config_t = decltype(prod_pipeline_config);
 
 } // namespace crv::spline
