48d7ddb463dba748e460dbc51cc4678635e30861
Frank Secilia <frank.secilia@gmail.com>
2026-05-15 14:26:54 -0400

    test(spline): test field_layout_t shift_mask

--- src/crv/math/spline/segment_test.cpp
+++ src/crv/math/spline/segment_test.cpp
@@ -10,12 +10,26 @@
 namespace crv::spline {
 namespace {
 
-// --------------------------------------------------------------------------------------------------------------------
+// ====================================================================================================================
 // layouts
-// --------------------------------------------------------------------------------------------------------------------
+// ====================================================================================================================
 
 static_assert(intermediate_layout.shift_mask() == 0x3F); // 6 bits
 static_assert(final_layout.shift_mask() == 0x7F); // 7 bits
+
+// --------------------------------------------------------------------------------------------------------------------
+// field_layout_t
+// --------------------------------------------------------------------------------------------------------------------
+
+static_assert(field_layout_t{0, false}.shift_mask() == 0x0);
+static_assert(field_layout_t{0, true}.shift_mask() == 0x0);
+static_assert(field_layout_t{1, false}.shift_mask() == 0x1);
+static_assert(field_layout_t{1, true}.shift_mask() == 0x1);
+static_assert(field_layout_t{4, true}.shift_mask() == 0xF);
+static_assert(field_layout_t{5, true}.shift_mask() == 0x1F);
+static_assert(field_layout_t{8, true}.shift_mask() == 0xFF);
+static_assert(field_layout_t{8, false}.shift_mask() == 0xFF);
+static_assert(field_layout_t{9, true}.shift_mask() == 0x1FF);
 
 // --------------------------------------------------------------------------------------------------------------------
 // unpacking
