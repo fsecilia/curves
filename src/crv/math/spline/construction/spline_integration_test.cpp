88b37aea9871cfb119f8379caf4152569cc84218
Frank Secilia <frank.secilia@gmail.com>
2026-05-16 11:11:34 -0400

    refactor(spline): parameterize on out_frac_bits directly

--- src/crv/math/spline/construction/spline_integration_test.cpp
+++ src/crv/math/spline/construction/spline_integration_test.cpp
@@ -489,7 +489,7 @@ TEST(spline_generator, poc)
     using float_extractor_t = float_extractor_t<scalar_t>;
     using scaled_int_t = float_extractor_t::scaled_int_t;
     using exponent_aligner_t = exponent_aligner_t<final_layout_min_shift, final_layout_max_shift>;
-    using segment_builder_t = segment_builder_t<x_t, y_t, scaled_int_t, exponent_aligner_t{}>;
+    using segment_builder_t = segment_builder_t<scaled_int_t, y_t::frac_bits, exponent_aligner_t{}>;
     using builder_factory_t = builder_factory_t<segment_builder_t>;
     using segment_packer_t = segment_packer_t<float_extractor_t, field_packer_t, builder_factory_t, x_t::frac_bits,
         log2_min_width, segment_layout>;
