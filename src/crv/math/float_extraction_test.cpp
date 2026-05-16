c8934a5e31fb72661204b530beb8425793bd7f37
Frank Secilia <frank.secilia@gmail.com>
2026-05-15 20:00:05 -0400

    refactor(math): extract float_extraction module

---
+++ src/crv/math/float_extraction_test.cpp
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+/// \file
+/// \copyright Copyright (C) 2026 Frank Secilia
+
+#include "float_extraction.hpp"
+#include <crv/test/test.hpp>
+
+namespace crv {
+namespace {
+
+//
+
+} // namespace
+} // namespace crv
