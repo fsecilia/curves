acaa92c8d0ae1f7dcae212e3d3db178629c95e77
Frank Secilia <frank.secilia@gmail.com>
2026-05-16 11:08:54 -0400

    refactor(spline): parameterize on in_frac_bits directly

--- src/crv/math/spline/dynamic_segment_test.cpp
+++ src/crv/math/spline/dynamic_segment_test.cpp
@@ -35,8 +35,8 @@ struct spline_dynamic_segment_test_t : Test
     using exponent_aligner_t = exponent_aligner_t<final_layout_min_shift, final_layout_max_shift>;
     using segment_builder_t = segment_builder_t<x_t, y_t, float_extractor_t::scaled_int_t, exponent_aligner_t{}>;
     using builder_factory_t = builder_factory_t<segment_builder_t>;
-    using segment_packer_t
-        = segment_packer_t<float_extractor_t, field_packer_t, builder_factory_t, log2_min_width, segment_layout>;
+    using segment_packer_t = segment_packer_t<float_extractor_t, field_packer_t, builder_factory_t, x_t::frac_bits,
+        log2_min_width, segment_layout>;
     using cubic_t = cubic_t<scalar_t>;
 
     segment_packer_t segment_packer;
