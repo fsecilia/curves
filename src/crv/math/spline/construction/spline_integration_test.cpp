bf0fea8faecf5f8155c36047e8f6ad72e9a67532
Frank Secilia <frank.secilia@gmail.com>
2026-05-18 08:30:58 -0400

    refactor(spline): move approximant into residual.hpp

--- src/crv/math/spline/construction/spline_integration_test.cpp
+++ src/crv/math/spline/construction/spline_integration_test.cpp
@@ -16,7 +16,6 @@
 #include <crv/math/rounding_mode.hpp>
 #include <crv/math/saturate_cast.hpp>
 #include <crv/math/shifter.hpp>
-#include <crv/math/spline/construction/approximant.hpp>
 #include <crv/math/spline/construction/function_sampler.hpp>
 #include <crv/math/spline/construction/node_generator.hpp>
 #include <crv/math/spline/construction/residual.hpp>
