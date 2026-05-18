51be3e5a86da3b95d7726e9a56bc5bf7e01a1bc8
Frank Secilia <frank.secilia@gmail.com>
2026-05-18 08:22:55 -0400

    refactor(spline): extract, test convergence_test_t

--- src/crv/math/spline/construction/interval.hpp
+++ src/crv/math/spline/construction/interval.hpp
@@ -187,4 +187,34 @@ template <std::floating_point scalar_t, typename bisector_t, typename interval_f
     }
 };
 
+// --------------------------------------------------------------------------------------------------------------------
+// convergence
+// --------------------------------------------------------------------------------------------------------------------
+
+/// decides if an interval should subdivide
+///
+/// Compares the norm's metric_error directly against global_tolerance and a noise floor, both in y.
+template <std::floating_point scalar_t, int_t log2_min_width> struct convergence_test_t
+{
+    // total noise budget in ulps relative to interval scale
+    //
+    // The margin is determined roughly by the number of ops per sample and error introduced by rounding after each op.
+    // The ops include hermite-to-monomial basis conversion, cubic Horner, and norm, each contrbuting up to ulps_per_op
+    // of error.
+    static constexpr auto ops_per_sample = int_t{16};
+    static constexpr auto ulps_per_op = int_t{4};
+    static constexpr auto relative_noise_margin
+        = std::numeric_limits<scalar_t>::epsilon() * scalar_t{ops_per_sample * ulps_per_op};
+
+    scalar_t global_tolerance;
+
+    constexpr auto operator()(auto const& interval) const noexcept -> bool
+    {
+        auto const noise_floor = interval.residual.scale * relative_noise_margin;
+        auto const local_tolerance = std::max(global_tolerance, noise_floor);
+
+        return interval.subdomain.log2_width > log2_min_width && interval.residual.metric_error > local_tolerance;
+    }
+};
+
 } // namespace crv::spline
