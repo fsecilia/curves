007ecdab961bbaba717730fd1b89657c10d27136
Frank Secilia <frank.secilia@gmail.com>
2026-05-18 09:48:40 -0400

    refactor(spline): extract tangent extension

--- src/crv/math/spline/construction/spline_integration_test.cpp
+++ src/crv/math/spline/construction/spline_integration_test.cpp
@@ -21,6 +21,7 @@
 #include <crv/math/spline/construction/residual.hpp>
 #include <crv/math/spline/construction/segment_factory.hpp>
 #include <crv/math/spline/construction/subdivision.hpp>
+#include <crv/math/spline/construction/tangent_extension.hpp>
 #include <crv/math/spline/construction/weight_function.hpp>
 #include <crv/math/spline/construction/workspace.hpp>
 #include <crv/math/spline/pipeline_config.hpp>
@@ -37,57 +38,6 @@
 namespace crv {
 namespace spline {
 namespace {
-
-template <typename t_interval_t, typename t_segment_t, typename segment_quantizer_t, typename t_segment_packer_t,
-    typename t_field_packer_t, typename saturating_shifter_t, segment_layout_t segment_layout>
-struct tangent_extender_t
-{
-    using interval_t = t_interval_t;
-    using segment_t = t_segment_t;
-    using segment_packer_t = t_segment_packer_t;
-    using field_packer_t = t_field_packer_t;
-
-    using x_t = segment_t::x_t;
-    using unpacked_field_t = segment_quantizer_t::unpacked_field_t;
-    using mantissa_t = unpacked_field_t::mantissa_t;
-    using scalar_t = segment_quantizer_t::scalar_t;
-    using cubic_t = segment_quantizer_t::cubic_t;
-
-    [[no_unique_address]] segment_quantizer_t quantize_segment;
-    [[no_unique_address]] segment_packer_t pack_segment;
-    [[no_unique_address]] field_packer_t pack_field;
-    [[no_unique_address]] saturating_shifter_t saturating_shifter;
-
-    constexpr auto operator()(interval_t const& interval, segment_t const& segment) const noexcept -> segment_t
-    {
-        auto const log2_x_max
-            = sizeof(typename x_t::value_t) * CHAR_BIT - x_t::frac_bits - is_signed_v<typename x_t::value_t>;
-
-        auto const t = scalar_t{1};
-        auto const dt_dx = std::ldexp(scalar_t{1}, int_cast<int>(log2_x_max - interval.subdomain.log2_width));
-        auto const dy_dx_extended_segment = interval.cubic(jet_t{t, dt_dx}).df;
-
-        // find fixed y at dx = width
-        auto const final_segment_width = x_t{1} << interval.subdomain.log2_width;
-        auto const y1_actual = segment(final_segment_width);
-
-        // pack proto and unpack to find shift
-        auto const y = from_fixed<scalar_t>(y1_actual);
-        auto const tangent_cubic = cubic_t{0, 0, dy_dx_extended_segment, y};
-        auto const unpacked_segment = quantize_segment(tangent_cubic, log2_x_max);
-        auto packed_segment = pack_segment(unpacked_segment);
-        auto const c3_shift = unpacked_segment[3].shift;
-
-        // solve target mantissa directly via renormalization
-        auto const y1_as_signed = saturate_cast<mantissa_t>(y1_actual.value);
-        auto const target_mantissa = saturating_shifter.shift(y1_as_signed, c3_shift);
-
-        // repack new c3
-        packed_segment[fields_per_segment - 1]
-            = pack_field(unpacked_field_t{.mantissa = target_mantissa, .shift = c3_shift}, segment_layout.final);
-        return segment_t{packed_segment};
-    }
-};
 
 template <typename t_workspace_t> struct typestates_t
 {
---
+++ src/crv/math/spline/construction/tangent_extension_test.cpp
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+
+/// \file
+/// \copyright Copyright (C) 2026 Frank Secilia
+
+#include "tangent_extension.hpp"
+#include <crv/test/test.hpp>
+#include <gmock/gmock.h>
+
+namespace crv::spline {
+namespace {
+
+//
+
+} // namespace
+} // namespace crv::spline
