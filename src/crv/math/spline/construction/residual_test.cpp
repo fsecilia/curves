5a87dc9976734c269df5c066a1180a37fc7eba19
Frank Secilia <frank.secilia@gmail.com>
2026-05-19 15:00:07 -0400

    refactor(spline): estimator requires nodes in (0, 1)

--- src/crv/math/spline/construction/residual_test.cpp
+++ src/crv/math/spline/construction/residual_test.cpp
@@ -18,8 +18,9 @@ constexpr auto max_node = 0.9;
 struct fake_node_generator_t
 {
     // to properly test the implementation uses max() correctly, the final node must not be the max node
-    static constexpr auto nodes = std::array<scalar_t, 3>{0.1, max_node, 0.5};
-    constexpr auto operator()() const noexcept { return nodes; }
+    using nodes_t = std::array<scalar_t, 3>;
+    static constexpr auto nodes = nodes_t{0.1, max_node, 0.5};
+    constexpr auto operator()() const noexcept -> nodes_t const& { return nodes; }
 };
 
 struct uniform_norm_t
@@ -78,6 +79,40 @@ TEST(spline_residual_estimator, metrics_must_be_positive)
     auto approximant = [](scalar_t) constexpr { return scalar_t{0.0}; };
 
     EXPECT_DEBUG_DEATH(sut(sample_target, approximant, left, midpoint, right), "nonnegative");
+}
+
+// nodes must be in (0, 1)
+//
+// Standard nodes must be in [0, 1], but for a cubic spline generated from a hermite basis, the spline always goes
+// through the knots, so measuring at segment endpoints is not effective.
+struct spline_residual_estimator_test_node_endpoints_t : Test
+{
+    struct single_point_node_generator_t
+    {
+        using nodes_t = std::vector<scalar_t>;
+
+        scalar_t node = 0.0;
+
+        constexpr auto operator()() const noexcept -> nodes_t { return {node}; }
+    };
+
+    static constexpr auto sample_target = [](scalar_t node) constexpr { return target_function_sample_t{node}; };
+    static constexpr auto approximant = [](scalar_t) constexpr { return scalar_t{0.0}; };
+
+    using sut_t
+        = residual_estimator_t<scalar_t, single_point_node_generator_t, uniform_norm_t, linear_weight_function_t>;
+};
+
+TEST_F(spline_residual_estimator_test_node_endpoints_t, nodes_excludes_left_endpoint)
+{
+    constexpr auto sut = sut_t{.generate_nodes = {}, .measure_error = {}, .apply_weight = {}};
+    EXPECT_DEBUG_DEATH(sut(sample_target, approximant, left, midpoint, right), "in \\(0, 1\\)");
+}
+
+TEST_F(spline_residual_estimator_test_node_endpoints_t, nodes_excludes_right_endpoint)
+{
+    constexpr auto sut = sut_t{.generate_nodes = {.node = 1.0}, .measure_error = {}, .apply_weight = {}};
+    EXPECT_DEBUG_DEATH(sut(sample_target, approximant, left, midpoint, right), "in \\(0, 1\\)");
 }
 
 #endif
