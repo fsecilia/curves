67c2d9022a520b9976d666a803ca9ff63be76ecd
Frank Secilia <frank.secilia@gmail.com>
2026-05-19 16:32:45 -0400

    build(spline): rename to exponential_decay

--- src/crv/math/spline/construction/weight_functions/exponential_decay_test.cpp
+++ src/crv/math/spline/construction/weight_functions/exponential_decay_test.cpp
@@ -3,7 +3,7 @@
 /// \file
 /// \copyright Copyright (C) 2026 Frank Secilia
 
-#include "weight_function.hpp"
+#include "exponential_decay.hpp"
 #include <crv/test/test.hpp>
 
 namespace crv::spline::weight_functions {
