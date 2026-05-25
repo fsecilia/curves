c7712184898c2e62568076f25fb6d556af85c29d
Frank Secilia <frank.secilia@gmail.com>
2026-05-25 17:09:53 -0400

    feat(ser): throw common exception type
    Previously, we were throwing an exception from toml directly, which
    resided in the toml namespace. It derived from runtime_error, so we
    could technically catch it without leaking toml, but we would lose the
    ability to distinguish parse errors from other types.

--- src/crv/ui/serialization/toml/error_reporter.hpp
+++ src/crv/ui/serialization/toml/error_reporter.hpp
@@ -6,6 +6,8 @@
 #pragma once
 
 #include <crv/lib.hpp>
+#include <crv/ui/serialization/exceptions.hpp>
+#include <sstream>
 #include <string_view>
 #include <toml++/toml.hpp>
 
@@ -18,10 +20,12 @@ public:
     /// records most recently visited source region
     auto location(toml::source_region const& src) -> void { location_ = src; }
 
-    /// throws a parse error using the recorded location with given message
+    /// throws parse_x using the recorded location with given message
     [[noreturn]] auto report_error(std::string_view message) const -> void
     {
-        throw toml::parse_error{message.data(), location_};
+        auto out = std::ostringstream{};
+        out << *location_.path << ":" << location_.begin.line << ":" << location_.begin.column << ": " << message;
+        throw parse_x{out.str()};
     }
 
 private:
--- src/crv/ui/serialization/toml/error_reporter_test.cpp
+++ src/crv/ui/serialization/toml/error_reporter_test.cpp
@@ -15,44 +15,28 @@ struct config_error_reporter_test_t : Test
 {
     using src_path_t = toml::source_path_ptr;
     src_path_t const expected_source_path = std::make_shared<std::string>("source_path");
-    toml::source_region const location{.begin = {}, .end = {}, .path = expected_source_path};
+    toml::source_region const location{
+        .begin = {.line = 3, .column = 5}, .end = {.line = 7, .column = 11}, .path = expected_source_path};
 
     using sut_t = error_reporter_t;
 
     sut_t sut{};
 };
 
-TEST_F(config_error_reporter_test_t, location_is_recorded)
-{
-    sut.location(location);
-
-    // Location is only readable from the exception after throwing.
-    auto actual_source_path = src_path_t{};
-    try
-    {
-        sut.report_error("");
-    }
-    catch (toml::parse_error const& err)
-    {
-        actual_source_path = err.source().path;
-    }
-
-    ASSERT_EQ(expected_source_path, actual_source_path);
-}
-
 TEST_F(config_error_reporter_test_t, message)
 {
-    auto const expected_message = std::string{"expected_message"};
+    sut.location(location);
+    auto const thrown_message = std::string{"thrown_message"};
+    auto const expected_message = std::string{"source_path:3:5: thrown_message"};
 
-    // message is only recorded in thrown exception.
     auto actual_message = std::string{};
     try
     {
-        sut.report_error(expected_message);
+        sut.report_error(thrown_message);
     }
-    catch (toml::parse_error const& err)
+    catch (std::runtime_error const& err)
     {
-        actual_message = err.description();
+        actual_message = err.what();
     }
 
     ASSERT_EQ(expected_message, actual_message);
