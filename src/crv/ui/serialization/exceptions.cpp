4204891f48d1f576de071c7992320e3dadd660e8
Frank Secilia <frank.secilia@gmail.com>
2026-05-25 16:50:46 -0400

    feat(ser): add exceptions

---
+++ src/crv/ui/serialization/exceptions.cpp
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+/// \file
+/// \copyright Copyright (C) 2026 Frank Secilia
+
+#include "exceptions.hpp"
+
+namespace crv::serialization {
+
+parse_x::~parse_x() = default;
+
+} // namespace crv::serialization
