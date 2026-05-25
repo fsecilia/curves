5716e628e0f6b80db614b75287540e939b35f515
Frank Secilia <frank.secilia@gmail.com>
2026-05-25 13:32:31 -0400

    feat(ref): add constraints

---
+++ src/crv/ui/lib/reflection/constraints.hpp
@@ -0,0 +1,29 @@
+// SPDX-License-Identifier: MIT
+
+/// \file
+/// \brief optional constraints applied during reflection
+/// \copyright Copyright (C) 2026 Frank Secilia
+
+#pragma once
+
+#include <crv/lib.hpp>
+#include <algorithm>
+
+namespace crv::reflection::constraints {
+
+/// no constraint applied
+template <typename value_t> struct none_t
+{
+    constexpr auto operator()(value_t const& value) const noexcept -> value_t { return value; }
+};
+
+/// constrains value between min and max
+template <typename value_t> struct range_t
+{
+    value_t min;
+    value_t max;
+
+    constexpr auto operator()(value_t const& val) const noexcept -> value_t { return std::clamp(val, min, max); }
+};
+
+} // namespace crv::reflection::constraints
