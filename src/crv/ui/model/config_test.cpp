c427a0fa4ea9ecddefa900e8feb71e9df25da0bc
Frank Secilia <frank.secilia@gmail.com>
2026-05-26 13:06:50 -0400

    feat(ui): rough in model config

---
+++ src/crv/ui/model/config_test.cpp
@@ -0,0 +1,19 @@
+// SPDX-License-Identifier: MIT
+
+/// \file
+/// \copyright Copyright (C) 2026 Frank Secilia
+
+#include "config.hpp"
+#include <crv/test/test.hpp>
+
+namespace crv {
+namespace {
+
+struct config_test_t : Test
+{
+    struct mock_visitor_t
+    {};
+};
+
+} // namespace
+} // namespace crv
