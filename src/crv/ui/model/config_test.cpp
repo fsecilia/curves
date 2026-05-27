c9238017ee6b3563da1ae0f9d509f7915fe71c46
Frank Secilia <frank.secilia@gmail.com>
2026-05-27 18:15:20 -0400

    feat(ui): add toml composition facade

--- src/crv/ui/model/config_test.cpp
+++ src/crv/ui/model/config_test.cpp
@@ -5,13 +5,7 @@
 
 #include "config.hpp"
 #include <crv/test/test.hpp>
-#include <crv/ui/serialization/deserializer.hpp>
-#include <crv/ui/serialization/reader.hpp>
-#include <crv/ui/serialization/serializer.hpp>
-#include <crv/ui/serialization/toml/archive.hpp>
-#include <crv/ui/serialization/toml/reader_adapter.hpp>
-#include <crv/ui/serialization/toml/writer_adapter.hpp>
-#include <crv/ui/serialization/writer.hpp>
+#include <crv/ui/serialization/toml/toml.hpp>
 #include <sstream>
 
 namespace crv::model {
@@ -19,17 +13,8 @@ namespace {
 
 TEST(model_test, round_trip)
 {
-    using archive_t = serialization::tomlpp::archive_t;
-
-    using writer_adapter_t = serialization::tomlpp::writer_adapter_t;
-    using writer_t = serialization::writer_t<writer_adapter_t>;
-    using writer_factory_t = serialization::writer_factory_t<writer_t>;
-    using serializer_t = serialization::serializer_t<archive_t, writer_factory_t>;
-
-    using reader_adapter_t = serialization::tomlpp::reader_adapter_t;
-    using reader_t = serialization::reader_t<reader_adapter_t>;
-    using reader_factory_t = serialization::reader_factory_t<reader_t>;
-    using deserializer_t = serialization::deserializer_t<archive_t, reader_factory_t>;
+    using serializer_t = serialization::tomlpp::serializer_t;
+    using deserializer_t = serialization::tomlpp::deserializer_t;
 
     auto expected_root = root_t{};
     auto actual_root = root_t{};
---
+++ src/crv/ui/serialization/toml/toml.hpp
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+/// \file
+/// \brief toml++ facade
+/// \copyright Copyright (C) 2026 Frank Secilia
+
+#pragma once
+
+#include <crv/lib.hpp>
+#include <crv/ui/serialization/deserializer.hpp>
+#include <crv/ui/serialization/reader.hpp>
+#include <crv/ui/serialization/serializer.hpp>
+#include <crv/ui/serialization/toml/archive.hpp>
+#include <crv/ui/serialization/toml/reader_adapter.hpp>
+#include <crv/ui/serialization/toml/writer_adapter.hpp>
+#include <crv/ui/serialization/writer.hpp>
+
+namespace crv::serialization::tomlpp {
+
+using reader_t = serialization::reader_t<reader_adapter_t>;
+using reader_factory_t = serialization::reader_factory_t<reader_t>;
+using deserializer_t = serialization::deserializer_t<archive_t, reader_factory_t>;
+
+using writer_t = serialization::writer_t<writer_adapter_t>;
+using writer_factory_t = serialization::writer_factory_t<writer_t>;
+using serializer_t = serialization::serializer_t<archive_t, writer_factory_t>;
+
+} // namespace crv::serialization::tomlpp
