703cf7ac06be9ed647326abe3d25c0fea5d73cb6
Frank Secilia <frank.secilia@gmail.com>
2026-05-27 17:09:32 -0400

    test(ser): test interaction instead of state
    I wanted to see the real difference between testing state without gmock
    and testing interaction with gmock. The gmock version is a little
    larger, but it is specifying more so that should be expected.
    
    The state version doesn't have strong typing and reads like a crypto
    puzzle. Trying to figure out what went wrong means tracking the math
    through each stage.
    
    The gmock version reads like a specification. I'll keep this version.

--- src/crv/ui/serialization/serializer_test.cpp
+++ src/crv/ui/serialization/serializer_test.cpp
@@ -5,6 +5,7 @@
 
 #include "serializer.hpp"
 #include <crv/test/test.hpp>
+#include <gmock/gmock.h>
 #include <sstream>
 
 namespace crv::serialization {
@@ -12,76 +13,118 @@ namespace {
 
 struct serialization_serializer_test_t : Test
 {
-    using document_t = int_t;
-    using writer_adapter_t = int_t;
-    using writer_t = int_t;
-
-    static constexpr auto empty_document = document_t{5};
-    static constexpr auto writer_adapter = writer_adapter_t{empty_document + 7};
-    static constexpr auto writer = writer_t{writer_adapter + 11};
-
-    struct archive_results_t
+    struct document_t
     {
-        document_t written_document = 0;
-        std::filesystem::path actual_path = {};
-        std::ostream* actual_ostream = nullptr;
+        int_t id = 0;
+        constexpr auto operator==(document_t const&) const noexcept -> bool = default;
     };
-    archive_results_t archive_results{};
+    document_t empty_document{5};
+
+    struct writer_adapter_t
+    {
+        int_t id = 0;
+        constexpr auto operator==(writer_adapter_t const&) const noexcept -> bool = default;
+    };
+    writer_adapter_t writer_adapter{7};
+
+    struct writer_t
+    {
+        int_t id = 0;
+        constexpr auto operator==(writer_t const&) const noexcept -> bool = default;
+    };
+    writer_t writer{11};
+
+    struct mock_archive_t
+    {
+        virtual ~mock_archive_t() = default;
+
+        MOCK_METHOD(document_t, create_empty_document, (), (const));
+        MOCK_METHOD(writer_adapter_t, create_writer_adapter, (document_t document), (const));
+        MOCK_METHOD(void, write_document_path, (document_t document, std::filesystem::path const& path), (const));
+        MOCK_METHOD(void, write_document_ostream, (document_t document, std::ostream& out), (const));
+    };
+    StrictMock<mock_archive_t> mock_archive;
 
     struct archive_t
     {
-        archive_results_t* results = nullptr;
+        mock_archive_t* mock = nullptr;
 
-        auto create_empty_document() const -> document_t { return empty_document; }
-        auto create_writer_adapter(document_t document) const -> writer_adapter_t { return document + 7; }
+        auto create_empty_document() const -> document_t { return mock->create_empty_document(); }
+        auto create_writer_adapter(document_t document) const -> writer_adapter_t
+        {
+            return mock->create_writer_adapter(document);
+        }
 
         auto write_document(document_t document, std::filesystem::path path) const -> void
         {
-            results->written_document = document;
-            results->actual_path = std::move(path);
+            mock->write_document_path(document, path);
         }
 
         auto write_document(document_t document, std::ostream& out) const -> void
         {
-            results->written_document = document;
-            results->actual_ostream = &out;
+            mock->write_document_ostream(document, out);
         }
     };
 
+    struct mock_writer_factory_t
+    {
+        virtual ~mock_writer_factory_t() = default;
+
+        MOCK_METHOD(writer_t, create_writer, (writer_adapter_t const& writer_adapter), (const));
+    };
+    StrictMock<mock_writer_factory_t> mock_writer_factory;
+
     struct writer_factory_t
     {
-        auto create_writer(writer_adapter_t writer_adapter) const -> writer_t { return writer_adapter + 11; }
+        mock_writer_factory_t* mock = nullptr;
+        auto create_writer(writer_adapter_t const& writer_adapter) const -> writer_t
+        {
+            return mock->create_writer(writer_adapter);
+        }
     };
+
+    struct mock_reflected_t
+    {
+        virtual ~mock_reflected_t() = default;
+        MOCK_METHOD(void, reflect, (writer_t writer), (const));
+    };
+    StrictMock<mock_reflected_t> mock_obj;
 
     struct reflected_t
     {
-        mutable writer_t actual_writer = 0;
-        auto reflect(writer_t writer) const -> void { actual_writer = writer; }
+        mock_reflected_t* mock = nullptr;
+        auto reflect(writer_t writer) const -> void { mock->reflect(writer); }
     };
-    reflected_t obj;
+    reflected_t obj{&mock_obj};
 
     using sut_t = serializer_t<archive_t, writer_factory_t>;
-    sut_t sut{archive_t{&archive_results}, writer_factory_t{}};
+    sut_t sut{archive_t{&mock_archive}, writer_factory_t{&mock_writer_factory}};
 };
 
 TEST_F(serialization_serializer_test_t, serialize_to_path)
 {
-    auto path = std::filesystem::path{"path"};
-    sut(obj, path);
+    auto const path = std::filesystem::path{"path"};
 
-    EXPECT_EQ(writer, obj.actual_writer);
-    EXPECT_EQ(empty_document, archive_results.written_document);
-    EXPECT_EQ(path, archive_results.actual_path);
+    EXPECT_CALL(mock_archive, create_empty_document()).WillOnce(Return(empty_document));
+    EXPECT_CALL(mock_archive, create_writer_adapter(empty_document)).WillOnce(Return(writer_adapter));
+    EXPECT_CALL(mock_writer_factory, create_writer(writer_adapter)).WillOnce(Return(writer));
+    EXPECT_CALL(mock_obj, reflect(writer));
+    EXPECT_CALL(mock_archive, write_document_path(empty_document, path));
+
+    sut(obj, path);
 }
 
 TEST_F(serialization_serializer_test_t, serialize_to_stream)
 {
     auto out = std::ostringstream{};
-    sut(obj, out);
 
-    EXPECT_EQ(writer, obj.actual_writer);
-    EXPECT_EQ(empty_document, archive_results.written_document);
-    EXPECT_EQ(&out, archive_results.actual_ostream);
+    EXPECT_CALL(mock_archive, create_empty_document()).WillOnce(Return(empty_document));
+    EXPECT_CALL(mock_archive, create_writer_adapter(empty_document)).WillOnce(Return(writer_adapter));
+    EXPECT_CALL(mock_writer_factory, create_writer(writer_adapter)).WillOnce(Return(writer));
+    EXPECT_CALL(mock_obj, reflect(writer));
+    EXPECT_CALL(mock_archive, write_document_ostream(empty_document, Ref(out)));
+
+    sut(obj, out);
 }
 
 } // namespace
