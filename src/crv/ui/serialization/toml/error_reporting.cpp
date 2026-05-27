47a79fb8fd9db504f3931f5dafb3047e29b8746f
Frank Secilia <frank.secilia@gmail.com>
2026-05-27 12:10:22 -0400

    feat(ser): add toml error reporting func

---
+++ src/crv/ui/serialization/toml/error_reporting.cpp
@@ -0,0 +1,28 @@
+// SPDX-License-Identifier: MIT
+
+/// \file
+/// \copyright Copyright (C) 2026 Frank Secilia
+
+#include "error_reporting.hpp"
+#include <crv/ui/serialization/exceptions.hpp>
+
+namespace crv::serialization::tomlpp {
+
+[[noreturn]] auto report_error(std::string_view message, toml::source_region location) -> void
+{
+    if (location.path)
+    {
+        report_error(std::format("{}:{}:{}: {}", *location.path, location.begin.line, location.begin.column, message));
+    }
+    else
+    {
+        report_error(std::format("{}:{}: {}", location.begin.line, location.begin.column, message));
+    }
+}
+
+[[noreturn]] auto report_error(std::string message) -> void
+{
+    throw parse_x{std::move(message)};
+}
+
+} // namespace crv::serialization::tomlpp
---
+++ src/crv/ui/serialization/toml/error_reporting.hpp
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+/// \file
+/// \brief toml error reporting
+/// \copyright Copyright (C) 2026 Frank Secilia
+
+#pragma once
+
+#include <crv/lib.hpp>
+#include <string>
+#include <string_view>
+#include <toml++/toml.hpp>
+
+namespace crv::serialization::tomlpp {
+
+/// throws parse_x with formatted location and message
+[[noreturn]] auto report_error(std::string_view message, toml::source_region location) -> void;
+
+/// throws parse_x with basic message
+[[noreturn]] auto report_error(std::string message) -> void;
+
+} // namespace crv::serialization::tomlpp
