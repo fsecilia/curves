ecd3fe96df910f4bac7e8fb527603fe65096386c
Frank Secilia <frank.secilia@gmail.com>
2026-05-27 11:17:08 -0400

    feat(ser): purge merged sandbox types
    As we worked through this sandbox, we found the only real changes were
    to fix the overwrting sections bug and simplify error handling. All
    of the original tests still apply except the one that literally
    validated the buggy behavior. That was removed and replaced with 2 to
    cover the new behavior.

--- src/crv/ui/model/config_test.cpp
+++ src/crv/ui/model/config_test.cpp
@@ -7,6 +7,8 @@
 #include <crv/test/test.hpp>
 
 #include <crv/ui/serialization/exceptions.hpp>
+#include <crv/ui/serialization/toml/reader_adapter.hpp>
+#include <crv/ui/serialization/toml/writer_adapter.hpp>
 #include <filesystem>
 #include <string_view>
 #include <toml++/toml.hpp>
@@ -14,103 +16,6 @@
 namespace crv::serialization {
 
 namespace tomlpp {
-
-class writer_adapter_t
-{
-public:
-    explicit writer_adapter_t(toml::table& table) noexcept : table_{table} {}
-
-    template <typename value_t> auto write(std::string_view key, value_t const& value) const -> void
-    {
-        table_.insert_or_assign(key, value);
-    }
-
-    template <is_enum enum_t> auto write(std::string_view key, enum_t const& value) const -> void
-    {
-        auto name = reflection::to_string(value);
-        if (!name)
-        {
-            throw parse_x{std::format(
-                "invalid value ({}) for enum \"{}\"", static_cast<std::underlying_type_t<enum_t>>(value), key)};
-        }
-
-        table_.insert_or_assign(key, *name);
-    }
-
-    [[nodiscard]] auto create_section(std::string_view key) -> writer_adapter_t
-    {
-        if (auto* node = table_.get(key))
-        {
-            auto target_table = node->as_table();
-            if (!target_table) throw parse_x{std::format("expected table for key \"{}\"", key)};
-            return writer_adapter_t{*target_table};
-        }
-        else
-        {
-            auto [iterator, inserted] = table_.insert(key, toml::table{});
-            return writer_adapter_t{*iterator->second.as_table()};
-        }
-    }
-
-private:
-    toml::table& table_;
-};
-
-class reader_adapter_t
-{
-public:
-    explicit reader_adapter_t(toml::table const& table) noexcept : table_{table} {}
-
-    template <typename value_t> auto read(std::string_view key, value_t& dst) const -> bool
-    {
-        auto* node = table_.get(key);
-        if (!node) return false;
-
-        auto value = node->value<value_t>();
-        if (!value) report_error(node->source(), std::format("type mismatch for key \"{}\"", key));
-
-        dst = std::move(*value);
-        return true;
-    }
-
-    template <is_enum enum_t> auto read(std::string_view key, enum_t& dst) -> bool
-    {
-        auto* node = table_.get(key);
-        if (!node) return false;
-
-        auto name = node->value<std::string_view>();
-        if (!name) report_error(node->source(), std::format("type mismatch for enum key \"{}\"", key));
-
-        auto value = reflection::from_string<enum_t>(*name);
-        if (!value) report_error(node->source(), std::format("invalid value \"{}\" for enum \"{}\"", *name, key));
-
-        dst = *value;
-        return true;
-    }
-
-    [[nodiscard]] auto get_section(std::string_view key) -> std::optional<reader_adapter_t>
-    {
-        if (auto* node = table_.get(key))
-        {
-            if (auto* sub_table = node->as_table()) return reader_adapter_t{*sub_table};
-            else report_error(node->source(), std::format("expected table for key \"{}\"", key));
-        }
-        return std::nullopt;
-    }
-
-private:
-    toml::table const& table_;
-
-    [[noreturn]] auto report_error(toml::source_region location, std::string_view message) const -> void
-    {
-        if (location.path)
-        {
-            throw parse_x{
-                std::format("{}:{}:{}: {}", *location.path, location.begin.line, location.begin.column, message)};
-        }
-        else throw parse_x{std::format("{}:{}: {}", location.begin.line, location.begin.column, message)};
-    }
-};
 
 struct archive_t
 {
