ecd32e12602d81db184c0ba0d85dafa07e504f90
Frank Secilia <frank.secilia@gmail.com>
2026-05-27 19:30:59 -0400

    feat(ui): link against qt quick
    This still links against qt widgets for the message box for now.

--- src/crv/ui/qt/main.cpp
+++ src/crv/ui/qt/main.cpp
@@ -4,64 +4,24 @@
 /// \brief config app qt entry point
 /// \copyright Copyright (C) 2026 Frank Secilia
 
-#include <dink/container.hpp>
+#include <crv/ui/model/config.hpp>
 #include <QApplication>
+#include <QGuiApplication>
 #include <QMessageBox>
-#include <cstdlib>
-#include <iostream>
-#include <utility>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
+#include <QStandardPaths>
 
 namespace crv {
 
-struct message_box_params_t
+static auto main(int argc, char* argv[]) -> int
 {
-    QMessageBox::Icon icon = QMessageBox::Information;
-    QString title = "Curves Configuration";
-    QString text = "Package installed successfully!";
-};
+    QGuiApplication::setApplicationName("curves");
+    QGuiApplication::setOrganizationName("");
 
-struct default_message_box_t
-{
-    message_box_params_t message_box_params;
+    auto gui_app = QGuiApplication{argc, argv};
 
-    explicit default_message_box_t(message_box_params_t message_box_params) noexcept
-        : message_box_params{std::move(message_box_params)}
-    {}
-
-    auto exec() const noexcept -> int
-    {
-        return QMessageBox{QMessageBox::Information, "Curves Configuration", "Package installed successfully!"}.exec();
-    }
-};
-
-auto run_application(int argc, char* argv[]) -> int
-{
-    auto container = dink::Container{};
-
-    auto app = QApplication{argc, argv};
-    QApplication::setApplicationName("curves");
-    QApplication::setOrganizationName("");
-
-    // This isn't how you'd normally use a container, but it proves that it works.
-    return container.resolve<default_message_box_t>().exec();
-}
-
-auto run_oneshot_config(int, char*[]) -> int
-{
-    std::cout << "configuration restored successfully!" << std::endl;
     return EXIT_SUCCESS;
-}
-
-auto main(int argc, char* argv[]) -> int
-{
-    auto const restore_config_option = std::string_view{"--restore-config"};
-
-    for (auto i = 1; i < argc; ++i)
-    {
-        if (argv[i] == restore_config_option) return run_oneshot_config(argc, argv);
-    }
-
-    return run_application(argc, argv);
 }
 
 } // namespace crv
