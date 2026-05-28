0df6bb5767f08c73634d34c44083ff7ee04f3314
Frank Secilia <frank.secilia@gmail.com>
2026-05-28 18:21:56 -0400

    test(model): perturb more values

--- src/crv/model/config_test.cpp
+++ src/crv/model/config_test.cpp
@@ -22,7 +22,7 @@ TEST(model_test, round_trip)
     // graphs initially same
     EXPECT_EQ(expected_root, actual_root);
 
-    // perturb all of expected
+    // perturb expected
     expected_root.version.value(3);
     expected_root.device.name.value("name");
     expected_root.device.dpi.value(26000);
@@ -30,6 +30,28 @@ TEST(model_test, round_trip)
     expected_root.profile.x_y_scaling.value(5.1);
     expected_root.profile.filter_halflife.value(200);
     expected_root.profile.notch_width.value(0.5);
+    expected_root.profile.synchronous.common.scale.input.value(2.0);
+    expected_root.profile.synchronous.common.scale.output.value(5.0);
+    expected_root.profile.synchronous.common.offset.begin.value(1.0);
+    expected_root.profile.synchronous.common.offset.width.value(1.5);
+    expected_root.profile.synchronous.common.floor.enabled.value(true);
+    expected_root.profile.synchronous.common.floor.anchor.value(1.2);
+    expected_root.profile.synchronous.common.limit.max.value(128.0);
+    expected_root.profile.synchronous.common.limit.width.value(7.0);
+    expected_root.profile.synchronous.specific.motivity.value(100.0);
+    expected_root.profile.synchronous.specific.gamma.value(2.5);
+    expected_root.profile.synchronous.specific.smooth.value(0.25);
+    expected_root.profile.synchronous.specific.sync_speed.value(2.75);
+    expected_root.profile.log_normal.common.scale.input.value(6.0);
+    expected_root.profile.log_normal.common.scale.output.value(9.0);
+    expected_root.profile.log_normal.common.offset.begin.value(2.0);
+    expected_root.profile.log_normal.common.offset.width.value(2.5);
+    expected_root.profile.log_normal.common.floor.enabled.value(true);
+    expected_root.profile.log_normal.common.floor.anchor.value(3.2);
+    expected_root.profile.log_normal.common.limit.max.value(256.0);
+    expected_root.profile.log_normal.common.limit.width.value(11.0);
+    expected_root.profile.log_normal.specific.center.value(10.0);
+    expected_root.profile.log_normal.specific.width.value(5.0);
 
     // graphs no longer same
     EXPECT_NE(expected_root, actual_root);
