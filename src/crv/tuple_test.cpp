213dd279383f6ca57d4a9fff79c4f265df826605
Frank Secilia <frank.secilia@gmail.com>
2026-05-29 13:25:05 -0400

    feat(tuple): add tuple_enumerate()

--- src/crv/tuple_test.cpp
+++ src/crv/tuple_test.cpp
@@ -66,12 +66,27 @@ struct counter_t
 
     constexpr auto operator()(arithmetic auto&& element) noexcept -> void { count += static_cast<int_t>(element); }
     constexpr auto operator()(a_t const&) noexcept -> void { ++count; }
+
+    constexpr auto operator()(std::size_t index, arithmetic auto&& element) noexcept -> void
+    {
+        count += static_cast<int_t>(index) + static_cast<int_t>(element);
+    }
+
+    constexpr auto operator()(std::size_t index, a_t const&) noexcept -> void
+    {
+        count += static_cast<int_t>(index) + 1;
+    }
 };
 
 static_assert(tuple_for_each(std::tuple<>{}, counter_t{}).count == 0);
 static_assert(tuple_for_each(std::tuple<int_t>{3}, counter_t{}).count == 3);
 static_assert(tuple_for_each(std::tuple<int_t const, float_t>{3, 5.0}, counter_t{}).count == 8);
 static_assert(tuple_for_each(std::tuple<int_t, float_t const&, a_t>{3, 5.0, a_t{}}, counter_t{}).count == 9);
+
+static_assert(tuple_enumerate(std::tuple<>{}, counter_t{}).count == 0);
+static_assert(tuple_enumerate(std::tuple<int_t>{3}, counter_t{}).count == 3);
+static_assert(tuple_enumerate(std::tuple<int_t const, float_t>{3, 5.0}, counter_t{}).count == 9);
+static_assert(tuple_enumerate(std::tuple<int_t, float_t const&, a_t>{3, 5.0, a_t{}}, counter_t{}).count == 12);
 
 } // namespace iteration_tests
 
