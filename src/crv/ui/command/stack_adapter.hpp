7ee91d62c3269b953f720f114748e8197951efdf
Frank Secilia <frank.secilia@gmail.com>
2026-05-28 20:27:12 -0400

    feat(ui): add command stack adapter

---
+++ src/crv/ui/command/stack_adapter.hpp
@@ -0,0 +1,32 @@
+// SPDX-License-Identifier: MIT
+
+/// \file
+/// \copyright Copyright (C) 2026 Frank Secilia
+
+#pragma once
+
+#include <crv/lib.hpp>
+#include <crv/ui/command/command.hpp>
+#include <memory>
+#include <utility>
+
+namespace crv::command {
+
+/// adapts command stack to an implementation
+///
+/// This adapter allows us to code against qt without depending on qt.
+template <typename impl_t> class stack_adapter_t
+{
+public:
+    constexpr stack_adapter_t(std::unique_ptr<impl_t> impl) noexcept : impl_{std::move(impl)} {}
+
+    template <typename command_t> auto push(command_t command) -> void { impl_->push(std::move(command)); }
+
+    constexpr auto undo() -> void { impl_->undo(); }
+    constexpr auto redo() -> void { impl_->redo(); }
+
+private:
+    std::unique_ptr<impl_t> impl_;
+};
+
+} // namespace crv::command
---
+++ src/crv/ui/command/stack_adapter_test.cpp
@@ -0,0 +1,65 @@
+// SPDX-License-Identifier: MIT
+
+/// \file
+/// \copyright Copyright (C) 2026 Frank Secilia
+
+#include "stack_adapter.hpp"
+#include <crv/test/test.hpp>
+#include <gmock/gmock.h>
+
+namespace crv::command {
+namespace {
+
+struct command_stack_adapter_test_t : Test
+{
+    struct command_t
+    {
+        int_t id = 0;
+        constexpr auto operator==(command_t const&) const noexcept -> bool = default;
+    };
+
+    struct mock_impl_t
+    {
+        virtual ~mock_impl_t() = default;
+
+        MOCK_METHOD(void, push, (command_t command));
+        MOCK_METHOD(void, undo, ());
+        MOCK_METHOD(void, redo, ());
+    };
+    StrictMock<mock_impl_t> mock_impl;
+
+    struct impl_t
+    {
+        mock_impl_t* mock = nullptr;
+
+        auto push(command_t command) -> void { mock->push(std::move(command)); }
+        auto undo() -> void { mock->undo(); }
+        auto redo() -> void { mock->redo(); }
+    };
+
+    using sut_t = stack_adapter_t<impl_t>;
+    sut_t sut{std::make_unique<impl_t>(impl_t{&mock_impl})};
+};
+
+TEST_F(command_stack_adapter_test_t, push)
+{
+    command_t expected_command{.id = 5};
+    EXPECT_CALL(mock_impl, push(expected_command));
+
+    sut.push(expected_command);
+}
+
+TEST_F(command_stack_adapter_test_t, undo)
+{
+    EXPECT_CALL(mock_impl, undo());
+    sut.undo();
+}
+
+TEST_F(command_stack_adapter_test_t, redo)
+{
+    EXPECT_CALL(mock_impl, redo());
+    sut.redo();
+}
+
+} // namespace
+} // namespace crv::command
