a56db253133390645c85939aaabcb90e298f5449
Frank Secilia <frank.secilia@gmail.com>
2026-05-31 17:20:13 -0400

    feat(math): add is_complex

--- src/crv/math/complex_traits_test.cpp
+++ src/crv/math/complex_traits_test.cpp
@@ -13,6 +13,22 @@ namespace {
 struct arbitrary_t
 {};
 
+//
+// is_complex
+//
+
+static_assert(!is_complex<arbitrary_t>);
+static_assert(!is_complex<int_t>);
+static_assert(!is_complex<float32_t>);
+static_assert(!is_complex<float64_t>);
+
+static_assert(is_complex<std::complex<float32_t>>);
+static_assert(is_complex<std::complex<float64_t>>);
+
+//
+// real_type_t
+//
+
 static_assert(std::same_as<real_type_t<arbitrary_t>, arbitrary_t>);
 static_assert(std::same_as<real_type_t<int_t>, int_t>);
 static_assert(std::same_as<real_type_t<float32_t>, float32_t>);
