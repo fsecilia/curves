c0f4555f6b664319592bdb57f4fe2f86c706478e
Frank Secilia <frank.secilia@gmail.com>
2026-05-31 17:12:56 -0400

    feat(math): add real_type_t

---
+++ src/crv/math/complex_traits_test.cpp
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+/// \file
+/// \copyright Copyright (C) 2026 Frank Secilia
+
+#include "complex_traits.hpp"
+#include <crv/test/test.hpp>
+#include <concepts>
+
+namespace crv {
+namespace {
+
+struct arbitrary_t
+{};
+
+static_assert(std::same_as<real_type_t<arbitrary_t>, arbitrary_t>);
+static_assert(std::same_as<real_type_t<int_t>, int_t>);
+static_assert(std::same_as<real_type_t<float32_t>, float32_t>);
+static_assert(std::same_as<real_type_t<float64_t>, float64_t>);
+
+static_assert(std::same_as<real_type_t<std::complex<float32_t>>, float32_t>);
+static_assert(std::same_as<real_type_t<std::complex<float64_t>>, float64_t>);
+
+} // namespace
+} // namespace crv
