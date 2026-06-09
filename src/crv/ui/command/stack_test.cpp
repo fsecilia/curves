d69e1b69d15793ab61153be8c4658607002df1bf
Frank Secilia <frank.secilia@gmail.com>
2026-06-09 07:36:17 -0400

    refactor(ui): rename stack_adapter to stack

--- src/crv/ui/command/stack_test.cpp
+++ src/crv/ui/command/stack_test.cpp
@@ -3,14 +3,14 @@
 /// \file
 /// \copyright Copyright (C) 2026 Frank Secilia
 
-#include "stack_adapter.hpp"
+#include "stack.hpp"
 #include <crv/test/test.hpp>
 #include <gmock/gmock.h>
 
 namespace crv::command {
 namespace {
 
-struct command_stack_adapter_test_t : Test
+struct command_stack_test_t : Test
 {
     struct command_t
     {
@@ -18,46 +18,46 @@ struct command_stack_adapter_test_t : Test
         constexpr auto operator==(command_t const&) const noexcept -> bool = default;
     };
 
-    struct mock_impl_t
+    struct mock_adapter_t
     {
-        virtual ~mock_impl_t() = default;
+        virtual ~mock_adapter_t() = default;
 
         MOCK_METHOD(void, push, (command_t command));
         MOCK_METHOD(void, undo, ());
         MOCK_METHOD(void, redo, ());
     };
-    StrictMock<mock_impl_t> mock_impl;
+    StrictMock<mock_adapter_t> mock_adapter;
 
-    struct impl_t
+    struct adapter_t
     {
-        mock_impl_t* mock = nullptr;
+        mock_adapter_t* mock = nullptr;
 
         auto push(command_t command) -> void { mock->push(std::move(command)); }
         auto undo() -> void { mock->undo(); }
         auto redo() -> void { mock->redo(); }
     };
 
-    using sut_t = stack_adapter_t<impl_t>;
-    sut_t sut{impl_t{&mock_impl}};
+    using sut_t = stack_t<adapter_t>;
+    sut_t sut{adapter_t{&mock_adapter}};
 };
 
-TEST_F(command_stack_adapter_test_t, push)
+TEST_F(command_stack_test_t, push)
 {
     command_t expected_command{.id = 5};
-    EXPECT_CALL(mock_impl, push(expected_command));
+    EXPECT_CALL(mock_adapter, push(expected_command));
 
     sut.push(expected_command);
 }
 
-TEST_F(command_stack_adapter_test_t, undo)
+TEST_F(command_stack_test_t, undo)
 {
-    EXPECT_CALL(mock_impl, undo());
+    EXPECT_CALL(mock_adapter, undo());
     sut.undo();
 }
 
-TEST_F(command_stack_adapter_test_t, redo)
+TEST_F(command_stack_test_t, redo)
 {
-    EXPECT_CALL(mock_impl, redo());
+    EXPECT_CALL(mock_adapter, redo());
     sut.redo();
 }
 
