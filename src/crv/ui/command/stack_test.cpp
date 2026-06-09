9962a9c21d964f5f18d8e639422718c886febb0f
Frank Secilia <frank.secilia@gmail.com>
2026-06-09 07:56:17 -0400

    refactor(ui): make stack adapter-based
    This revision makes stack use an adapter instead of an implementation.
    Semantics aside, this allows the adapter side to have qt-specifics, like
    wrapping the created command with a qt adapter directly.
    
    To this end, it has also moved from push to emplace, so clients don't
    have to create the actual command, and so the adapter can create it all
    in one go instead of a chain of 2 unique ptrs.

--- src/crv/ui/command/stack_test.cpp
+++ src/crv/ui/command/stack_test.cpp
@@ -13,16 +13,13 @@ namespace {
 struct command_stack_test_t : Test
 {
     struct command_t
-    {
-        int_t id = 0;
-        constexpr auto operator==(command_t const&) const noexcept -> bool = default;
-    };
+    {};
 
     struct mock_adapter_t
     {
         virtual ~mock_adapter_t() = default;
 
-        MOCK_METHOD(void, push, (command_t command));
+        MOCK_METHOD(void, emplace, (int_t id, std::string const& name));
         MOCK_METHOD(void, undo, ());
         MOCK_METHOD(void, redo, ());
     };
@@ -32,10 +29,16 @@ struct command_stack_test_t : Test
     {
         mock_adapter_t* mock = nullptr;
 
-        auto push(command_t command) -> void { mock->push(std::move(command)); }
+        template <typename command_t, typename... args_t> auto emplace(args_t&&... args) -> void
+        {
+            mock->emplace(std::forward<args_t>(args)...);
+        }
         auto undo() -> void { mock->undo(); }
         auto redo() -> void { mock->redo(); }
     };
+
+    int_t const id = 3;
+    std::string name = "name";
 
     using sut_t = stack_t<adapter_t>;
     sut_t sut{adapter_t{&mock_adapter}};
@@ -43,10 +46,8 @@ struct command_stack_test_t : Test
 
 TEST_F(command_stack_test_t, push)
 {
-    command_t expected_command{.id = 5};
-    EXPECT_CALL(mock_adapter, push(expected_command));
-
-    sut.push(expected_command);
+    EXPECT_CALL(mock_adapter, emplace(id, name));
+    sut.template emplace<command_t>(id, name);
 }
 
 TEST_F(command_stack_test_t, undo)
