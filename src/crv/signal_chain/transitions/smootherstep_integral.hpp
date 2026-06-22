5283b0b162b05c18a25b2d4de8658ccff7795e3e
Frank Secilia <frank.secilia@gmail.com>
2026-06-22 09:47:43 -0400

    refactor(sig): simplify description in transition

--- src/crv/signal_chain/transitions/smootherstep_integral.hpp
+++ src/crv/signal_chain/transitions/smootherstep_integral.hpp
@@ -23,16 +23,14 @@ struct smootherstep_integral_t
         if (t <= scalar_t{0}) return value_t{0};
         if (t >= scalar_t{1}) return input - scalar_t{0.5};
 
-        // strictly compute using the primal scalar to avoid implicit jet evaluation
+        // f = t^4(t^2 - 3t + 2.5)
         auto const t2 = t * t;
         auto const t4 = t2 * t2;
-
-        // f = t^6 - 3t^5 + 2.5t^4
         auto const y = t4 * (t2 - scalar_t{3} * t + scalar_t{2.5});
 
         if constexpr (is_jet<value_t>)
         {
-            // df = 6t^5 - 15t^4 + 10t^3
+            // df = t^3(6t^2 - 15t^1 + 10)
             auto const t3 = t2 * t;
             auto const dt = tangent(input);
             auto const dy_dt = t3 * ((scalar_t{6} * t - scalar_t{15}) * t + scalar_t{10});
