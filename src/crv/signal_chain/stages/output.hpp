c77eeda2ad7eab0e59f3ef6ef653aa171cc970a1
Frank Secilia <frank.secilia@gmail.com>
2026-06-22 22:54:34 -0400

    feat(sig): add output_t stage

---
+++ src/crv/signal_chain/stages/output.hpp
@@ -0,0 +1,25 @@
+// SPDX-License-Identifier: MIT
+
+/// \file
+/// \copyright Copyright (C) 2026 Frank Secilia
+
+#pragma once
+
+#include <crv/lib.hpp>
+
+namespace crv::signal_chain::stages {
+
+/// applies prev stage then transforms output
+template <typename transform_t, typename prev_stage_t> struct output_t
+{
+    transform_t transform;
+    prev_stage_t prev_stage;
+
+    /// applies transform(prev_stage(input))
+    template <typename value_t> [[nodiscard]] constexpr auto operator()(value_t input) const noexcept -> value_t
+    {
+        return transform(prev_stage(input));
+    }
+};
+
+} // namespace crv::signal_chain::stages
