84c46b86e3f29180de852a768b9d2868e6864a87
Frank Secilia <frank.secilia@gmail.com>
2026-06-27 18:32:23 -0400

    feat(shape): rough in config

---
+++ src/crv/shaping/config_test.cpp
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+
+/// \file
+/// \copyright Copyright (C) 2026 Frank Secilia
+
+#include "config.hpp"
+#include <crv/test/test.hpp>
+
+namespace crv::shaping {
+namespace {
+
+//
+
+} // namespace
+} // namespace crv::shaping
