5af85ea18d9dd25cb449f9e8a2d2a04cb6482351
Frank Secilia <frank.secilia@gmail.com>
2026-06-30 13:33:30 -0400

    feat(qt): add i18n integration test

---
+++ src/crv/ui/qt/test/test.ts
@@ -0,0 +1,17 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="en_EN">
+<context>
+    <name>test</name>
+    <message>
+        <source>test_actual</source>
+        <translation>test_expected</translation>
+    </message>
+</context>
+<context>
+    <message>
+        <source>actual</source>
+        <translation>expected</translation>
+    </message>
+</context>
+</TS>
