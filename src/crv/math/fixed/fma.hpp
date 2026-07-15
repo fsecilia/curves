0edf7ca24ec6b9344372784b290c8f5d16888f91
Frank Secilia <frank.secilia@gmail.com>
2026-07-15 09:45:18 -0400

    feat(fixed): expose output type from fma

--- src/crv/math/fixed/fma.hpp
+++ src/crv/math/fixed/fma.hpp
@@ -15,10 +15,12 @@
 namespace crv {
 
 /// fused multiply-add for fixed-point types
-template <is_fixed out_t, is_fixed multiplicand_t, is_fixed multiplier_t, is_fixed addend_t,
+template <is_fixed t_out_t, is_fixed multiplicand_t, is_fixed multiplier_t, is_fixed addend_t,
     typename shifter_t = shifter_t<>, fixed::overflow_policy_t overflow_policy = fixed::overflow_policy_t::wrap>
 struct fma_t
 {
+    using out_t = t_out_t;
+
     [[no_unique_address]] shifter_t shifter = {};
 
     // wide intermediate
