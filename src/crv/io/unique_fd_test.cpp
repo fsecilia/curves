f8028332275e7ffc740d806577490f1394888948
Frank Secilia <frank.secilia@gmail.com>
2026-07-20 10:54:04 -0400

    feat(io): extract unique_fd_t

--- src/crv/io/unique_fd_test.cpp
+++ src/crv/io/unique_fd_test.cpp
@@ -118,6 +118,7 @@ TEST_F(unique_fd_test_t, reset_to_disarmed_preserves_closer_for_later_adoption)
     auto sut = sut_t{old_fd, closer_t{&mock_closer}};
 
     auto const seq = InSequence{};
+
     expect_close(old_fd);
 
     sut.reset();
@@ -373,6 +374,7 @@ TEST_F(unique_fd_test_armed_t, reset_closes_old_and_adopts_new_fd)
 TEST_F(unique_fd_test_armed_t, reset_to_same_fd_invokes_old_and_new_closers)
 {
     auto const seq = InSequence{};
+
     expect_close(fd);
 
     sut.reset(sut.get(), closer_t{&new_mock_closer});
@@ -387,6 +389,7 @@ TEST_F(unique_fd_test_armed_t, reset_with_closer_closes_old_with_old_closer_and_
     auto const new_fd = int{1};
 
     auto const seq = InSequence{};
+
     expect_close(fd);
     expect_close(new_fd, new_mock_closer);
 
