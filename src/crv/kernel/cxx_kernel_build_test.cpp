8482b3358e118fc1835343aa067474dbedb41205
Frank Secilia <frank.secilia@gmail.com>
2026-07-22 21:00:32 -0400

    build(kernel): add c++ kernel build test

---
+++ src/crv/kernel/cxx_kernel_build_test.cpp
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: GPL-2.0+ OR MIT
+
+/// \file
+/// \brief build smoke test for building in c++ mode
+/// \copyright Copyright (C) 2026 Frank Secilia
+
+extern "C" {
+#include <crv/kernel/cxx_lib_build_test.h>
+} // extern "C" {
+
+#include "cxx_kernel_build_test.hpp"
+#include <crv/math/fixed/fixed.hpp>
---
+++ src/crv/kernel/cxx_kernel_build_test.hpp
@@ -0,0 +1,8 @@
+// SPDX-License-Identifier: GPL-2.0+ OR MIT
+
+/// \file
+/// \brief build smoke test for linux kernel headers
+///
+/// This module is used during dev to make sure headers that aren't yet included in a source module compile.
+///
+/// \copyright Copyright (C) 2026 Frank Secilia
