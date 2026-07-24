4ce903c705006d9f551eaad233dc33fa79cf7cf1
Frank Secilia <frank.secilia@gmail.com>
2026-07-24 16:03:59 -0400

    feat(kernel): start composition
    This revision starts the real composition with real types to make sure
    they actually build. They don't do anything real yet, but they do
    compile and link.

--- src/crv/kernel/cxx_build_test.cpp
+++ src/crv/kernel/cxx_build_test.cpp
@@ -5,9 +5,14 @@
 
 extern "C" {
 #include "cxx_build_test.h"
+#include <linux/errno.h>
 } // extern "C"
 
+#include <crv/kernel/composition.hpp>
 #include <crv/math/fixed/fixed.hpp>
+
+#include <array>
+#include <cstddef>
 
 namespace crv {
 
@@ -23,6 +28,12 @@ extern "C" int reference_cxx(void)
 {
     using namespace crv;
 
+    auto dst_storage = std::array<std::byte, 64>{};
+
+    auto velocity_copy_to_user_record_copier_factory = composition_t::velocity_copy_to_user_record_copier_factory_t{
+        copy_to_user_copier_factory_t{kernel::copy_to_user_factory_t{}, -EFAULT}};
+    auto velocity_copy_to_user_record_copier = velocity_copy_to_user_record_copier_factory(dst_storage.data());
+
     auto const one = q63_0i_t{1};
-    return q47_16i_t::convert(one).value;
+    return q47_16i_t::convert(one).value + velocity_copy_to_user_record_copier.error();
 }
