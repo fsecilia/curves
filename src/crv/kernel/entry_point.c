e685de7e765527e3e413d547f28cd23d3e4aab9f
Frank Secilia <frank.secilia@gmail.com>
2026-07-27 20:28:16 -0400

    build(filt): move into timing_recovery

--- src/crv/kernel/entry_point.c
+++ src/crv/kernel/entry_point.c
@@ -6,7 +6,7 @@
 
 #define pr_fmt(fmt) KBUILD_MODNAME ": " fmt
 
-#include <crv/filter/poll_interval_quantizer.h>
+#include <crv/filter/timing_recovery/poll_interval_quantizer.h>
 #include <crv/kernel/abi.h>
 
 #include <linux/bitops.h>
