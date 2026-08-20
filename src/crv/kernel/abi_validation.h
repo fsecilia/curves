3b6c5e9316f5be336d11d59bb14e7a0a0419c93b
Frank Secilia <frank.secilia@gmail.com>
2026-08-19 21:02:40 -0400

    fix(kern): make abi_validation.h usable from c++

--- src/crv/kernel/abi_validation.h
+++ src/crv/kernel/abi_validation.h
@@ -20,6 +20,17 @@
 extern "C" {
 #endif
 
+//
+// compatibility
+//
+
+/// compatible alignof across languages
+#if defined(__cplusplus)
+#define CRV_ALIGNOF(type) alignof(type)
+#else
+#define CRV_ALIGNOF(type) _Alignof(type)
+#endif
+
 /// compatible static_assert across languages
 #if defined(__cplusplus)
 #define CRV_STATIC_ASSERT(condition, message) static_assert(condition, message)
@@ -30,46 +41,50 @@ extern "C" {
 /// compatible typeof across languages
 #define CRV_TYPEOF(expression) __typeof__(expression)
 
+//
+// validation
+//
+
 /// true if type is signed
 #define CRV_IS_SIGNED_TYPE(type) (((type) - 1) < (type)1)
-
-/// references a member of a struct
-#define CRV_MEMBER(type, member) (((type*)0)->member)
-
-/// references type of a member of a struct
-#define CRV_MEMBER_TYPE(type, member) CRV_TYPEOF(CRV_MEMBER(type, member))
-
-/// true if type of member is signed
-#define CRV_MEMBER_SIGNED(type, member) CRV_IS_SIGNED_TYPE(CRV_MEMBER_TYPE(type, member))
-
-/// asserts member has specific size and offset
-#define CRV_MEMBER_LAYOUT(type, member, size, offset)                                                       \
-    CRV_STATIC_ASSERT(sizeof(CRV_MEMBER_TYPE(type, member)) == size, #type "::" #member ": size mismatch"); \
-    CRV_STATIC_ASSERT(offsetof(type, member) == offset, #type "::" #member ": offset mismatch");
-
-/// aserts member has specific size, offset, and signedness
-#define CRV_MEMBER_LAYOUT_INTEGER(type, member, size, offset, signedness) \
-    CRV_MEMBER_LAYOUT(type, member, size, offset);                        \
-    CRV_STATIC_ASSERT(CRV_MEMBER_SIGNED(type, member) == signedness, #type "::" #member ": sign mismatch")
 
 /// asserts types share size and alignment
 #define CRV_SAME_LAYOUT(compat_type, ref_type)                                                                    \
     CRV_STATIC_ASSERT(                                                                                            \
         sizeof(compat_type) == sizeof(ref_type), #compat_type ": size does not match reference type " #ref_type); \
-    CRV_STATIC_ASSERT(_Alignof(compat_type) == _Alignof(ref_type),                                                \
+    CRV_STATIC_ASSERT(CRV_ALIGNOF(compat_type) == CRV_ALIGNOF(ref_type),                                          \
         #compat_type ": alignment does not match reference type " #ref_type)
 
-/// asserts member size and offset in compat_type matches member size and offset in ref_type
+/// references a member of a struct
+#define CRV_MEMBER(type, member) (((type*)0)->member)
+
+/// references type of a member of a struct
+#define CRV_MEMBER_TYPEOF(type, member) CRV_TYPEOF(CRV_MEMBER(type, member))
+
+/// true if type of member is signed
+#define CRV_MEMBER_IS_SIGNED_TYPE(type, member) CRV_IS_SIGNED_TYPE(CRV_MEMBER_TYPEOF(type, member))
+
+/// asserts member has specific size and offset
+#define CRV_MEMBER_LAYOUT(type, member, size, offset)                                                         \
+    CRV_STATIC_ASSERT(sizeof(CRV_MEMBER_TYPEOF(type, member)) == size, #type "::" #member ": size mismatch"); \
+    CRV_STATIC_ASSERT(offsetof(type, member) == offset, #type "::" #member ": offset mismatch")
+
+/// asserts member has specific size, offset, and signedness
+#define CRV_MEMBER_LAYOUT_INTEGER(type, member, size, offset, signedness) \
+    CRV_MEMBER_LAYOUT(type, member, size, offset);                        \
+    CRV_STATIC_ASSERT(CRV_MEMBER_IS_SIGNED_TYPE(type, member) == signedness, #type "::" #member ": sign mismatch")
+
+/// asserts same member size and offset in compat_type matches member size and offset in ref_type
 #define CRV_MEMBER_SAME_LAYOUT(compat_type, ref_type, member)                                          \
     CRV_STATIC_ASSERT(sizeof(CRV_MEMBER(compat_type, member)) == sizeof(CRV_MEMBER(ref_type, member)), \
         #compat_type "::" #member ": size does not match reference type " #ref_type);                  \
     CRV_STATIC_ASSERT(offsetof(compat_type, member) == offsetof(ref_type, member),                     \
         #compat_type "::" #member ": offset does not match reference type " #ref_type)
 
-/// asserts member type and signedness
-#define CRV_MEMBER_SAME_LAYOUT_INTEGER(compat_type, ref_type, member)                                \
-    CRV_MEMBER_SAME_LAYOUT(compat_type, ref_type, member);                                           \
-    CRV_STATIC_ASSERT(CRV_MEMBER_SIGNED(compat_type, member) == CRV_MEMBER_SIGNED(ref_type, member), \
+/// asserts same member type and signedness
+#define CRV_MEMBER_SAME_LAYOUT_INTEGER(compat_type, ref_type, member)                                                \
+    CRV_MEMBER_SAME_LAYOUT(compat_type, ref_type, member);                                                           \
+    CRV_STATIC_ASSERT(CRV_MEMBER_IS_SIGNED_TYPE(compat_type, member) == CRV_MEMBER_IS_SIGNED_TYPE(ref_type, member), \
         #compat_type "::" #member ": signedness does not match reference type " #ref_type)
 
 #ifdef __cplusplus
