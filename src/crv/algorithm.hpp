f4d5dff68f07bf3affe4e30d5ef55ebc6428598b
Frank Secilia <frank.secilia@gmail.com>
2026-08-25 19:55:53 -0400

    doc: update comments in algorithm.hpp
    Since adding -m80387, we can #include <algorithm> from kernel modules
    just fine. This revision updates that comment. It also adds small blocks
    for min() and max().

--- src/crv/algorithm.hpp
+++ src/crv/algorithm.hpp
@@ -1,11 +1,6 @@
 // SPDX-License-Identifier: MIT
 
 /// \file
-/// \brief simplified, kernel-compatible algorithm header
-///
-/// libstdc++ algorithm is not freestanding when compiled with clang; there are floating-point specializations clang
-/// does not know how to avoid. This module implements the mechanisms we need from that file.
-///
 /// \copyright Copyright (C) 2026 Frank Secilia
 
 #pragma once
@@ -15,6 +10,11 @@
 
 namespace crv {
 
+/// variadic max
+///
+/// This function has the same semantics as the 2-arg std::max, but takes any number of args.
+///
+/// \returns const reference to max arg
 template <typename first_t, typename... remaining_t>
     requires(std::same_as<first_t, remaining_t> && ...) && (sizeof...(remaining_t) > 0)
 constexpr auto max(first_t const& first, remaining_t const&... remaining) noexcept -> first_t const&
@@ -24,6 +24,11 @@ constexpr auto max(first_t const& first, remaining_t const&... remaining) noexce
     return *result;
 }
 
+/// variadic min
+///
+/// This function has the same semantics as the 2-arg std::min, but takes any number of args.
+///
+/// \returns const reference to min arg
 template <typename first_t, typename... remaining_t>
     requires(std::same_as<first_t, remaining_t> && ...) && (sizeof...(remaining_t) > 0)
 constexpr auto min(first_t const& first, remaining_t const&... rest) noexcept -> first_t const&
