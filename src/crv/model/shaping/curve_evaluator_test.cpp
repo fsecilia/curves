34b221209fcf9ef55a1c66bbd346293e8a541e0e
Frank Secilia <frank.secilia@gmail.com>
2026-09-04 11:31:25 -0400

    refactor(test): use mock_ as a prefix

--- src/crv/model/shaping/curve_evaluator_test.cpp
+++ src/crv/model/shaping/curve_evaluator_test.cpp
@@ -20,22 +20,22 @@ struct curve_evaluator_test_t : Test
     using jet_t = crv::jet_t<scalar_t>;
     using input_domain_t = model::input_domain_t<scalar_t>;
 
-    struct evaluator_mock_t
+    struct mock_evaluator_t
     {
-        virtual ~evaluator_mock_t() = default;
+        virtual ~mock_evaluator_t() = default;
         MOCK_METHOD(scalar_t, scalar, (scalar_t), (const, noexcept));
         MOCK_METHOD(jet_t, jet, (jet_t), (const, noexcept));
         MOCK_METHOD(input_domain_t, input_domain, (), (const, noexcept));
         MOCK_METHOD(std::vector<scalar_t>, critical_points, (), (const));
     };
-    StrictMock<evaluator_mock_t> evaluator_mock;
+    StrictMock<mock_evaluator_t> mock_evaluator;
 
     struct evaluator_t
     {
         using scalar_t = curve_evaluator_test_t::scalar_t;
         using jet_t = curve_evaluator_test_t::jet_t;
 
-        evaluator_mock_t* mock;
+        mock_evaluator_t* mock;
 
         [[nodiscard]] auto operator()(scalar_t input) const noexcept -> scalar_t { return mock->scalar(input); }
         [[nodiscard]] auto operator()(jet_t input) const noexcept -> jet_t { return mock->jet(input); }
@@ -52,18 +52,18 @@ struct curve_evaluator_test_t : Test
     static_assert(std::same_as<typename curve_evaluator_t::result_t::error_type, curve_evaluation_error_t>);
     static_assert(is_curve<curve_evaluator_t, scalar_t>);
 
-    curve_evaluator_t curve_evaluator{{&evaluator_mock}};
+    curve_evaluator_t curve_evaluator{{&mock_evaluator}};
 };
 
 TEST_F(curve_evaluator_test_t, trusted_evaluation_returns_scalar)
 {
-    EXPECT_CALL(evaluator_mock, scalar(3.0)).WillOnce(Return(5.0));
+    EXPECT_CALL(mock_evaluator, scalar(3.0)).WillOnce(Return(5.0));
     EXPECT_EQ(curve_evaluator(3.0), 5.0);
 }
 
 TEST_F(curve_evaluator_test_t, trusted_evaluation_calls_wrapped_evaluator_once)
 {
-    EXPECT_CALL(evaluator_mock, scalar(3.0)).WillOnce(Return(5.0));
+    EXPECT_CALL(mock_evaluator, scalar(3.0)).WillOnce(Return(5.0));
     static_cast<void>(curve_evaluator(3.0));
 }
 
@@ -71,60 +71,60 @@ TEST_F(curve_evaluator_test_t, jet_evaluation_forwards_to_wrapped_evaluator)
 {
     auto const input = jet_t{3.0, 5.0};
     auto const output = jet_t{7.0, 11.0};
-    EXPECT_CALL(evaluator_mock, jet(input)).WillOnce(Return(output));
+    EXPECT_CALL(mock_evaluator, jet(input)).WillOnce(Return(output));
     EXPECT_EQ(curve_evaluator(input), output);
 }
 
 TEST_F(curve_evaluator_test_t, input_domain_forwards_to_wrapped_evaluator)
 {
     auto const domain = input_domain_t{3.0, 5.0};
-    EXPECT_CALL(evaluator_mock, input_domain).WillOnce(Return(domain));
+    EXPECT_CALL(mock_evaluator, input_domain).WillOnce(Return(domain));
     EXPECT_EQ(curve_evaluator.input_domain(), domain);
 }
 
 TEST_F(curve_evaluator_test_t, critical_points_forward_to_wrapped_evaluator)
 {
     auto const points = std::vector<scalar_t>{3.0, 5.0};
-    EXPECT_CALL(evaluator_mock, critical_points).WillOnce(Return(points));
+    EXPECT_CALL(mock_evaluator, critical_points).WillOnce(Return(points));
     EXPECT_EQ(curve_evaluator.critical_points(), points);
 }
 
 TEST_F(curve_evaluator_test_t, checked_finite_valid_evaluation_returns_scalar)
 {
-    EXPECT_CALL(evaluator_mock, scalar(3.0)).WillOnce(Return(5.0));
+    EXPECT_CALL(mock_evaluator, scalar(3.0)).WillOnce(Return(5.0));
     EXPECT_EQ(curve_evaluator.try_evaluate(3.0), 5.0);
 }
 
 TEST_F(curve_evaluator_test_t, checked_finite_valid_evaluation_calls_wrapped_evaluator_once)
 {
-    EXPECT_CALL(evaluator_mock, scalar(3.0)).WillOnce(Return(5.0));
+    EXPECT_CALL(mock_evaluator, scalar(3.0)).WillOnce(Return(5.0));
     [[maybe_unused]] auto const result = curve_evaluator.try_evaluate(3.0);
 }
 
 TEST_F(curve_evaluator_test_t, checked_negative_finite_evaluation_returns_classified_error)
 {
-    EXPECT_CALL(evaluator_mock, scalar(3.0)).WillOnce(Return(-5.0));
+    EXPECT_CALL(mock_evaluator, scalar(3.0)).WillOnce(Return(-5.0));
     EXPECT_EQ(curve_evaluator.try_evaluate(3.0), std::unexpected{curve_evaluation_error_t::negative_finite_result});
 }
 
 TEST_F(curve_evaluator_test_t, checked_positive_infinity_returns_scalar)
 {
     auto const infinity = std::numeric_limits<scalar_t>::infinity();
-    EXPECT_CALL(evaluator_mock, scalar(3.0)).WillOnce(Return(infinity));
+    EXPECT_CALL(mock_evaluator, scalar(3.0)).WillOnce(Return(infinity));
     EXPECT_EQ(curve_evaluator.try_evaluate(3.0), infinity);
 }
 
 TEST_F(curve_evaluator_test_t, checked_negative_infinity_returns_classified_error)
 {
     auto const infinity = -std::numeric_limits<scalar_t>::infinity();
-    EXPECT_CALL(evaluator_mock, scalar(3.0)).WillOnce(Return(infinity));
+    EXPECT_CALL(mock_evaluator, scalar(3.0)).WillOnce(Return(infinity));
     EXPECT_EQ(curve_evaluator.try_evaluate(3.0), std::unexpected{curve_evaluation_error_t::negative_infinity});
 }
 
 TEST_F(curve_evaluator_test_t, checked_nan_returns_classified_error)
 {
     auto const nan = std::numeric_limits<scalar_t>::quiet_NaN();
-    EXPECT_CALL(evaluator_mock, scalar(3.0)).WillOnce(Return(nan));
+    EXPECT_CALL(mock_evaluator, scalar(3.0)).WillOnce(Return(nan));
     EXPECT_EQ(curve_evaluator.try_evaluate(3.0), std::unexpected{curve_evaluation_error_t::nan});
 }
 
--- src/crv/model/shaping/output_positioned_curve_test.cpp
+++ src/crv/model/shaping/output_positioned_curve_test.cpp
@@ -20,19 +20,19 @@ struct output_positioned_curve_test_t : Test
 
     using input_domain_t = model::input_domain_t<scalar_t>;
 
-    struct curve_mock_t
+    struct mock_curve_t
     {
-        virtual ~curve_mock_t() = default;
+        virtual ~mock_curve_t() = default;
         MOCK_METHOD(scalar_t, scalar, (scalar_t), (const, noexcept));
         MOCK_METHOD(jet_t, jet, (jet_t), (const, noexcept));
         MOCK_METHOD(std::vector<scalar_t>, critical_points, (), (const));
     };
-    StrictMock<curve_mock_t> curve_mock;
+    StrictMock<mock_curve_t> mock_curve;
 
     struct curve_t
     {
         using scalar_t = output_positioned_curve_test_t::scalar_t;
-        curve_mock_t* mock;
+        mock_curve_t* mock;
         input_domain_t domain;
 
         [[nodiscard]] auto operator()(scalar_t input) const noexcept -> scalar_t { return mock->scalar(input); }
@@ -41,17 +41,17 @@ struct output_positioned_curve_test_t : Test
         [[nodiscard]] auto critical_points() const -> std::vector<scalar_t> { return mock->critical_points(); }
     };
 
-    struct transform_mock_t
+    struct mock_transform_t
     {
-        virtual ~transform_mock_t() = default;
+        virtual ~mock_transform_t() = default;
         MOCK_METHOD(scalar_t, scalar, (scalar_t), (const, noexcept));
         MOCK_METHOD(jet_t, jet, (jet_t), (const, noexcept));
     };
-    StrictMock<transform_mock_t> transform_mock;
+    StrictMock<mock_transform_t> mock_transform;
 
     struct transform_t
     {
-        transform_mock_t* mock;
+        mock_transform_t* mock;
         [[nodiscard]] auto apply(scalar_t output) const noexcept -> scalar_t { return mock->scalar(output); }
         [[nodiscard]] auto apply(jet_t output) const noexcept -> jet_t { return mock->jet(output); }
     };
@@ -60,13 +60,13 @@ struct output_positioned_curve_test_t : Test
 
     static_assert(is_curve<output_positioned_curve_t<transform_t, curve_t>, scalar_t>);
 
-    output_positioned_curve_t<transform_t, curve_t> sut{{&transform_mock}, {&curve_mock, {13.0, 17.0}}};
+    output_positioned_curve_t<transform_t, curve_t> sut{{&mock_transform}, {&mock_curve, {13.0, 17.0}}};
 };
 
 TEST_F(output_positioned_curve_test_t, positions_nested_scalar_output)
 {
-    EXPECT_CALL(curve_mock, scalar(3.0)).WillOnce(Return(5.0));
-    EXPECT_CALL(transform_mock, scalar(5.0)).WillOnce(Return(7.0));
+    EXPECT_CALL(mock_curve, scalar(3.0)).WillOnce(Return(5.0));
+    EXPECT_CALL(mock_transform, scalar(5.0)).WillOnce(Return(7.0));
     EXPECT_EQ(sut(3.0), 7.0);
 }
 
@@ -75,8 +75,8 @@ TEST_F(output_positioned_curve_test_t, positions_nested_jet_output)
     auto const input = jet_t{3.0, 5.0};
     auto const nested = jet_t{7.0, 11.0};
     auto const expected = jet_t{13.0, 17.0};
-    EXPECT_CALL(curve_mock, jet(input)).WillOnce(Return(nested));
-    EXPECT_CALL(transform_mock, jet(nested)).WillOnce(Return(expected));
+    EXPECT_CALL(mock_curve, jet(input)).WillOnce(Return(nested));
+    EXPECT_CALL(mock_transform, jet(nested)).WillOnce(Return(expected));
     EXPECT_EQ(sut(input), expected);
 }
 
@@ -88,7 +88,7 @@ TEST_F(output_positioned_curve_test_t, forwards_nested_domain)
 TEST_F(output_positioned_curve_test_t, forwards_nested_critical_points)
 {
     auto const points = std::vector<scalar_t>{2.0, 5.0};
-    EXPECT_CALL(curve_mock, critical_points()).WillOnce(Return(points));
+    EXPECT_CALL(mock_curve, critical_points()).WillOnce(Return(points));
     EXPECT_EQ(sut.critical_points(), points);
 }
 
