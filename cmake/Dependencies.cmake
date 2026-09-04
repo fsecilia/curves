a853c1777cbc8eb9917fef4e4f375f392dd7dba2
Frank Secilia <frank.secilia@gmail.com>
2026-09-04 10:54:34 -0400

    build: do not enable ccache from within cmake
    Unconditionally enabling cmake is a pessimization for everyone other
    than developers. Obs, distro maintainers and end users will tend to
    compile this once. This has been removed. For developers, set these
    as shell variables, or on the command line manually:
    
    ```
    export CMAKE_C_COMPILER_LAUNCHER=ccache
    export CMAKE_CXX_COMPILER_LAUNCHER=ccache
    export CMAKE_C_LINKER_LAUNCHER=ccache
    export CMAKE_CXX_LINKER_LAUNCHER=ccache
    ```

--- cmake/Dependencies.cmake
+++ cmake/Dependencies.cmake
@@ -16,15 +16,6 @@
 set(THREADS_PREFER_PTHREAD_FLAG True)
 find_package(Threads REQUIRED)
 
-# use ccache, if available
-find_program(ccache ccache)
-if (ccache)
-    set(CMAKE_C_COMPILER_LAUNCHER "${ccache}")
-    set(CMAKE_CXX_COMPILER_LAUNCHER "${ccache}")
-    set(CMAKE_C_LINKER_LAUNCHER "${ccache}")
-    set(CMAKE_CXX_LINKER_LAUNCHER "${ccache}")
-endif()
-
 # find gtest when testing is requested
 if (BUILD_TESTING)
     set(INSTALL_GTEST OFF)
